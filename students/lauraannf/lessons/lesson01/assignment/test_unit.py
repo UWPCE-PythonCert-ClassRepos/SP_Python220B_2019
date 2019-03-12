899f16f8458246606eb04b7f3298dae5a4dbc4cf
lauraannf <lauraanng@gmail.com>
2019-03-12 12:48:44 -0400

    test_unit updated

--- students/lauraannf/lessons/lesson01/assignment/test_unit.py
+++ students/lauraannf/lessons/lesson01/assignment/test_unit.py
@@ -15,7 +15,7 @@ from inventory_management.main import main_menu as main_menu
 from inventory_management.main import get_price as get_price, \
     add_new_item as add_new_item, item_info as item_info, \
     exit_program as exit_program
-import sys
+from io import StringIO
 
 
 class FurnitureTests(TestCase):
@@ -75,14 +75,12 @@ class MainTests(TestCase):
     def test_get_price(self):
         self.assertEqual(24, get_price(1))
 
-    def test_exit(self):
-        self.assertRaises(SystemExit)
-
     def test_add_new_item(self):
         furniture_input = ['100', 'description', 'rentalprice', 'y',
                            'material', 'size']
         electronic_input = ['200', 'description', 'rentalprice', 'n', 'y',
                             'brand', 'voltage']
+        other_input = ['300', 'description', 'rentalprice', 'n', 'n']
         with patch('builtins.input', side_effect=furniture_input):
             test_inventory = add_new_item()
             test_return_furniture = {'100': {'product_code': '100',
@@ -100,32 +98,45 @@ class MainTests(TestCase):
                                                'rental_price': 'rentalprice',
                                                'material': 'material',
                                                'size': 'size'},
-                                        '200': {'product_code': '200',
-                                                'description': 'description',
-                                                'market_price': 24,
-                                                'rental_price': 'rentalprice',
-                                                'brand': 'brand',
-                                                'voltage': 'voltage'}}
+                                       '200': {'product_code': '200',
+                                               'description': 'description',
+                                               'market_price': 24,
+                                               'rental_price': 'rentalprice',
+                                               'brand': 'brand',
+                                               'voltage': 'voltage'}}
             self.assertEqual(test_return_electronics, test_inventory)
+        with patch('builtins.input', side_effect=other_input):
+            test_inventory = add_new_item()
+            test_return_other = {'100': {'product_code': '100',
+                                         'description': 'description',
+                                         'market_price': 24,
+                                         'rental_price': 'rentalprice',
+                                         'material': 'material',
+                                         'size': 'size'},
+                                 '200': {'product_code': '200',
+                                         'description': 'description',
+                                         'market_price': 24,
+                                         'rental_price': 'rentalprice',
+                                         'brand': 'brand',
+                                         'voltage': 'voltage'},
+                                 '300': {'product_code': '300',
+                                         'description': 'description',
+                                         'market_price': 24,
+                                         'rental_price': 'rentalprice'}}
+            self.assertEqual(test_return_other, test_inventory)
 
-    def test_item_info(capsys):
+    def test_item_info(self):
         furniture_input = ['100', 'description', 'rentalprice', 'y',
                            'material', 'size']
         with patch('builtins.input', side_effect=furniture_input):
             add_new_item()
-        with patch('builtins.input', side_effect='100'):
-            item_info()
-            test_return_furniture = {'product_code:100 \n'
-                                     'description:d \n'
-                                     'market_price:24 \n'
-                                     'rental_price:e \n'
-                                     'material:a \n'
-                                     'size:e'}
-#            out = sys.stdout
-#            assert out == test_return_furniture
-
+#        with patch('builtins.input', side_effect='100'):
+#            item_test1 = item_info()
+#        self.assertEqual(item_test1, None)
         with patch('builtins.input', side_effect='500'):
-            item_info()
-            test_return_furniture = {'Item not found in inventory'}
-#            out = capsys.readouterr()
-#            assert out.out == test_return_furniture
+            item_test2 = item_info()
+        self.assertEqual(item_test2, None)
+
+    def test_exit(self):
+        with self.assertRaises(SystemExit):
+            exit_program()
