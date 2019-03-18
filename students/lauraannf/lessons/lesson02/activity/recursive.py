82ded059a7c74f0a7d0a8431f8f9793064b9fd44
lauraannf <lauraanng@gmail.com>
2019-03-17 22:13:50 -0400

    lesson 2 activity

--- students/lauraannf/lessons/lesson02/activity/recursive.py
+++ students/lauraannf/lessons/lesson02/activity/recursive.py
@@ -3,14 +3,17 @@ recursion for debuging
 """
 
 import sys
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 
 def my_fun(n):
+    logging.debug(n)
     if n == 2:
         return True
     return my_fun(n / 2)
 
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
+    n = 100
     print(my_fun(n))
