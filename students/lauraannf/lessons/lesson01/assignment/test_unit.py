36fb47606df948b5238302c03c89c5eaf7f790d7
lauraannf <lauraanng@gmail.com>
2019-03-19 14:07:37 -0400

    fixed test_unit.py based on recommendations

--- students/lauraannf/lessons/lesson01/assignment/test_unit.py
+++ students/lauraannf/lessons/lesson01/assignment/test_unit.py
@@ -1,27 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 12 13:53:15 2019
+Created on Tue Mar 19 13:44:20 2019
 
 @author: Laura.Fiorentino
 """
-
 from unittest import TestCase
 from unittest.mock import Mock, MagicMock, patch
-from inventory_management.market_prices import get_latest_price
-from inventory_management.furniture import Furniture
-from inventory_management.inventory import Inventory
-from inventory_management.electricappliances import ElectricAppliances
 from inventory_management.main import main_menu
-from inventory_management.main import get_price
-from inventory_management.main import add_new_item
-from inventory_management.main import item_info
-from inventory_management.main import exit_program
+import inventory_management
 
 
 class FurnitureTests(TestCase):
     def test_furniture(self):
-        furniture_test = Furniture('100', 'description', 'marketprice',
-                                   'rentalprice', 'material', 'size')
+        furniture_test = inventory_management. \
+            furniture.Furniture('100', 'description', 'marketprice',
+                                'rentalprice', 'material', 'size')
         furniture_test_2 = furniture_test.return_as_dictionary()
         self.assertEqual(furniture_test_2, {'product_code': '100',
                                             'description': 'description',
@@ -33,8 +26,9 @@ class FurnitureTests(TestCase):
 
 class ElectricTests(TestCase):
     def test_electric(self):
-        electric_test = ElectricAppliances('200', 'description', 'marketprice',
-                                           'rentalprice', 'brand', 'voltage')
+        electric_test = inventory_management.electricappliances. \
+            ElectricAppliances('200', 'description', 'marketprice',
+                               'rentalprice', 'brand', 'voltage')
         electric_test_2 = electric_test.return_as_dictionary()
         self.assertEqual(electric_test_2, {'product_code': '200',
                                            'description': 'description',
@@ -46,8 +40,9 @@ class ElectricTests(TestCase):
 
 class InventoryTests(TestCase):
     def test_inventory(self):
-        inventory_test = Inventory('300', 'description', 'marketprice',
-                                   'rentalprice')
+        inventory_test = inventory_management. \
+            inventory.Inventory('300', 'description', 'marketprice',
+                                'rentalprice')
         inventory_test_2 = inventory_test.return_as_dictionary()
         self.assertEqual(inventory_test_2, {'product_code': '300',
                                             'description': 'description',
@@ -57,35 +52,37 @@ class InventoryTests(TestCase):
 
 class PriceTests(TestCase):
     def test_getprice(self):
-        price = get_latest_price('1')
+        price = inventory_management.market_prices.get_latest_price('1')
         self.assertEqual(price, 24)
 
 
 class MainTests(TestCase):
     def test_main_menu_add(self):
         with patch('builtins.input', side_effect='1'):
-            self.assertEqual(main_menu(), add_new_item)
+            self.assertEqual(main_menu(),
+                             inventory_management.main.add_new_item)
 
     def test_main_menu_info(self):
         with patch('builtins.input', side_effect='2'):
-            self.assertEqual(main_menu(), item_info)
+            self.assertEqual(main_menu(), inventory_management.main.item_info)
 
     def test_main_menu_quit(self):
         with patch('builtins.input', side_effect='q'):
-            self.assertEqual(main_menu(), exit_program)
+            self.assertEqual(main_menu(),
+                             inventory_management.main.exit_program)
 
     def test_getprice(self):
         self.get_price = MagicMock(return_value=24)
-        self.assertEqual(24, get_price(1))
+        self.assertEqual(24, inventory_management.main.get_price(1))
 
     def test_add_new_item(self):
         furniture_input = ['100', 'description', 'rentalprice', 'y',
                            'material', 'size']
         electric_input = ['200', 'description', 'rentalprice', 'n', 'y',
-                            'brand', 'voltage']
+                          'brand', 'voltage']
         other_input = ['300', 'description', 'rentalprice', 'n', 'n']
         with patch('builtins.input', side_effect=furniture_input):
-            test_inventory = add_new_item()
+            test_inventory = inventory_management.main.add_new_item()
             test_return_furniture = {'100': {'product_code': '100',
                                              'description': 'description',
                                              'market_price': 24,
@@ -94,22 +91,22 @@ class MainTests(TestCase):
                                              'size': 'size'}}
             self.assertEqual(test_return_furniture, test_inventory)
         with patch('builtins.input', side_effect=electric_input):
-            test_inventory = add_new_item()
+            test_inventory = inventory_management.main.add_new_item()
             test_return_electrics = {'100': {'product_code': '100',
-                                               'description': 'description',
-                                               'market_price': 24,
-                                               'rental_price': 'rentalprice',
-                                               'material': 'material',
-                                               'size': 'size'},
-                                       '200': {'product_code': '200',
-                                               'description': 'description',
-                                               'market_price': 24,
-                                               'rental_price': 'rentalprice',
-                                               'brand': 'brand',
-                                               'voltage': 'voltage'}}
+                                             'description': 'description',
+                                             'market_price': 24,
+                                             'rental_price': 'rentalprice',
+                                             'material': 'material',
+                                             'size': 'size'},
+                                     '200': {'product_code': '200',
+                                             'description': 'description',
+                                             'market_price': 24,
+                                             'rental_price': 'rentalprice',
+                                             'brand': 'brand',
+                                             'voltage': 'voltage'}}
             self.assertEqual(test_return_electrics, test_inventory)
         with patch('builtins.input', side_effect=other_input):
-            test_inventory = add_new_item()
+            test_inventory = inventory_management.main.add_new_item()
             test_return_other = {'100': {'product_code': '100',
                                          'description': 'description',
                                          'market_price': 24,
@@ -135,12 +132,19 @@ class MainTests(TestCase):
                             'rental_price': 'rentalprice',
                             'material': 'material',
                             'size': 'size'}}
-        with patch('builtins.input', side_effect='100'):
-            test_info = item_info(FULL_INV)
-            self.assertEqual(FULL_INV, test_info)
+        with patch('builtins.input', return_value='100'):
+            test_info = {'product_code': '100',
+                         'description': 'description',
+                         'market_price': 24,
+                         'rental_price': 'rentalprice',
+                         'material': 'material',
+                         'size': 'size'}
+            self.assertEqual(inventory_management.main.item_info(FULL_INV),
+                             test_info)
         with patch('builtins.input', side_effect='500'):
-            self.assertEqual(item_info(FULL_INV), None)
+            self.assertEqual(inventory_management.main.item_info(FULL_INV),
+                             None)
 
     def test_exit(self):
         with self.assertRaises(SystemExit):
-            exit_program()
+            inventory_management.main.exit_program()
