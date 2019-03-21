3c77c7dbcea50dd3a1a6f8c20780e992011dfdb9
Vinodh Venugopalan <vvinodh@gmail.com>
2019-03-21 15:58:21 -0500

    message

--- students/vvinodh/Lesson1/test_unit.py
+++ students/vvinodh/Lesson1/test_unit.py
@@ -3,7 +3,7 @@
 from unittest import TestCase
 
 from inventory_management.inventory_class import Inventory
-
+from inventory_management.furniture_class import Furniture
 
 
 class InventoryTest(TestCase):
@@ -14,3 +14,13 @@ class InventoryTest(TestCase):
                      'market_price': 50, 'rental_price': 10}
         test_inv = Inventory(6789, 'Cars', 50, 10)
         self.assertEqual(test_dict, test_inv.return_as_dictionary())
+
+class FurnitureTest(TestCase):
+    """Tests Furniture Class"""
+    def test_furniture(self):
+        """Tests the creation of an instance for furniture object"""
+        test_dict = {'product_code': 6789, 'description': 'Horse',
+                     'market_price': 50, 'rental_price': 10,
+                     'material': 'Leather', 'size': 'Large'}
+        test_furn = Furniture(6789, 'Car', 50, 10, 'Leather', 'Large')
+        self.assertEqual(test_dict, test_furn.return_as_dictionary())
