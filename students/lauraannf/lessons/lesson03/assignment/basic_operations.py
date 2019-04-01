6b8616ab04aa74b28d1e2ca76d21652a4a7d5bf2
lauraannf <lauraanng@gmail.com>
2019-04-01 16:48:52 -0400

    started lesson 3

---
+++ students/lauraannf/lessons/lesson03/assignment/basic_operations.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 16:47:37 2019
+
+@author: Laura.Fiorentino
+"""
+
+
+from customer_model import *
---
+++ students/lauraannf/lessons/lesson03/assignment/customer_model.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 15:41:41 2019
+
+@author: Laura.Fiorentino
+
+Lesson3 Assignment
+"""
+
+
+from peewee import *
+
+
+DATABASE = SqliteDatabase('customers.db')
+DATABASE.connect()
+DATABASE.execute_sql('PRAGMA foreign_keys = ON;')  # needed for sqlite only
+
+
+class BaseModel(Model):
+    """ sets up model"""
+    class Meta:
+        """sets up model"""
+        database = DATABASE
+
+
+class Customer(BaseModel):
+    """
+        This class defines Customer, which maintains details of customers
+    """
+    customer_ID = CharField(primary_key=True, max_length=30)
+    first_name = CharField(max_length=30)
+    last_name = CharField(max_length=30)
+    home_address = CharField(max_length=100)
+    phone_number = CharField(max_length=12)
+    email_address = CharField(max_length=30)
+    status = CharField(max_length=8)
+    credit_limit = DecimalField(decimal_places=2)
+
+
+class Rentals(BaseModel):
+    rental_number = CharField(primary_key=True, max_length=10)
+    renter = ForeignKeyField(Customer, null = False)
