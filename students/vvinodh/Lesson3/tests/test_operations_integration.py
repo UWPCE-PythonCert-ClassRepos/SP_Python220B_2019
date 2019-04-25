481b8b7a9d0638d1a3d8c659be3fa63e695881ca
Vinodh Venugopalan <vvinodh@gmail.com>
2019-04-25 00:02:43 -0500

    message

--- students/vvinodh/Lesson3/tests/test_operations_integration.py
+++ students/vvinodh/Lesson3/tests/test_operations_integration.py
@@ -19,19 +19,19 @@ class TestOperations(unittest.TestCase):
     """Tests all the methods in basic_operations in one go"""
     def test_full_integration(self):
         """Integrates all methods into one test"""
-        basic_operations.add_customer('007', 'James', 'Bond', '007 Bond Street', 8885671000,
-                                      'bond@bond.com', 'active', 10000.00)
-        basic_operations.update_customer_credit('009', 30000)
-        basic_operations.update_status('009', 'inactive')
-        expected_customer = {'customer_id': '009', 'name': 'Blames', 'lastname': 'Gond',
-                             'home_address': '123 Rainier Street', 'phone_number': '8885671003',
-                             'email_address': 'gond@gond.com', 'status': 'inactive',
-                             'credit_limit': 30000.00}
-        actual_searched_customer = basic_operations.search_customer('004')
+        basic_operations.add_customer('010', 'James', 'Bond', '123 Turnberry Street', 8885671000,
+                                      'bond@bond.com', 'active', 1000.00)
+        basic_operations.update_customer_credit('010', 3000)
+        basic_operations.update_status('010', 'inactive')
+        expected_customer = {'customer_id': '010', 'name': 'James', 'lastname': 'Bond',
+                             'home_address': '007 Bond Street', 'phone_number': '8885671000',
+                             'email_address': 'bond@bond.com', 'status': 'inactive',
+                             'credit_limit': 3000.0}
+        actual_searched_customer = basic_operations.search_customer('010')
         self.assertEqual(actual_searched_customer, expected_customer)
         actual_customer_count = basic_operations.list_active_customers()
-        self.assertEqual(actual_customer_count, 0)
-        basic_operations.delete_customer('004')
+        self.assertEqual(actual_customer_count, 1)
+        basic_operations.delete_customer('010')
 
         try:
             DATABASE.connect()
@@ -49,7 +49,7 @@ class TestOperations(unittest.TestCase):
                                 'credit_limit': searched_customer.credit_limit}
 
         except Exception as ex:
-            LOGGER.info('Error finding customer 004')
+            LOGGER.info('Error finding customer 010')
             LOGGER.info(ex)
             deleted_customer = {}
 
