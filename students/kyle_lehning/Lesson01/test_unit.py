7dd20a8aa67c01461ccfc46741a4be8ccf57464f
Kyle Lehning <lehnink@gmail.com>
2019-04-29 20:22:34 -0700

    all unit tests but main complete

--- students/kyle_lehning/Lesson01/test_unit.py
+++ students/kyle_lehning/Lesson01/test_unit.py
@@ -10,8 +10,9 @@ from furniture_class import *
 from inventory_class import *
 from main import *
 from market_prices import *
-from unittest import TestCase
 import unittest
+from unittest import TestCase
+from unittest.mock import  MagicMock
 
 
 class ElectricAppliancesTest(TestCase):
@@ -68,6 +69,40 @@ class FurnitureTest(TestCase):
                                "market_price": "test market price", "rental_price": "test rental price",
                                "material": "test material", "size": "test size"}
         self.assertEqual(e.return_as_dictionary(), expected_dictionary)
+
+
+class InventoryTest(TestCase):
+
+    def test_inventory_init(self):
+        """
+        Tests that Inventory can be initiated
+        """
+        e = Inventory("test product code", "test description", "test market price", "test rental price")
+
+        self.assertEqual(e.product_code, "test product code")
+        self.assertEqual(e.description, "test description")
+        self.assertEqual(e.market_price, "test market price")
+        self.assertEqual(e.rental_price, "test rental price")
+
+    def test_inventory_return(self):
+        """
+        Tests Inventory return_as_dictionary
+        """
+        e = Inventory("test product code", "test description", "test market price", "test rental price")
+        expected_dictionary = {"product_code": "test product code", "description": "test description",
+                               "market_price": "test market price", "rental_price": "test rental price"}
+        self.assertEqual(e.return_as_dictionary(), expected_dictionary)
+
+
+class MarketPricesTest(TestCase):
+
+    def test_get_latest_price(self):
+        """
+        Tests that get_latest_price works from market_prices
+        """
+        self.assertEqual(market_prices.get_latest_price("test"), 24)
+        market_prices.get_latest_price = MagicMock(return_value=10)
+        self.assertEqual(market_prices.get_latest_price("test"), 10)
 
 
 if __name__ == '__main__':
