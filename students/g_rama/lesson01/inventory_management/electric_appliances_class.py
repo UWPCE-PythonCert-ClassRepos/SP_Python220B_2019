2c5172d02254b78955be4171b20b1cd7364dc1a1
guntur <ramakanth.gunturu@gmail.com>
2019-05-06 23:27:57 -0700

    Correcting the syntax per pep8 standard

---
+++ students/g_rama/lesson01/inventory_management/electric_appliances_class.py
@@ -0,0 +1,26 @@
+"""# Electric appliances class"""
+from inventory_class import Inventory
+
+
+class ElectricAppliances(Inventory):
+
+    """ElectricAppliances class"""
+
+    def __init__(self, product_code, description, market_price, rental_price, brand, voltage):
+        Inventory.__init__(self, product_code, description, market_price, rental_price)
+        # Creates common instance variables from the parent class
+
+        self.brand = brand
+        self.voltage = voltage
+
+    def return_as_dictionary(self):
+        """Function to return Inventory as dictionary"""
+        output_dict = dict()
+        output_dict['product_code'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['market_price'] = self.market_price
+        output_dict['rental_price'] = self.rental_price
+        output_dict['brand'] = self.brand
+        output_dict['voltage'] = self.voltage
+
+        return output_dict
