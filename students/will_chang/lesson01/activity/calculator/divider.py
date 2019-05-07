2a5b2ff4d3709f585dd75b5ed4c59beee264a97c
Will Chang <williamchang6@gmail.com>
2019-05-06 20:36:01 -0700

    adding activity files

---
+++ students/will_chang/lesson01/activity/calculator/divider.py
@@ -0,0 +1,12 @@
+"""
+This module provides a division operator
+"""
+
+
+class Divider(object):
+    """A class used to represent a Divider object"""
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """A method that returns the quotient of two operands"""
+        return operand_1/operand_2
+        
---
+++ students/will_chang/lesson01/activity/calculator/exceptions.py
@@ -0,0 +1,9 @@
+"""
+This module provides an InsuffucientOperands Exception
+"""
+
+
+class InsufficientOperands(Exception):
+    """A class used to represent an InsufficientOperands Exception"""
+    pass
+    
