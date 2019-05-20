79751b13e1dcb2cff29decdd08d68e262df1f15f
guntur <ramakanth.gunturu@gmail.com>
2019-05-19 18:47:56 -0700

    Adding the initial basic operations and test files

---
+++ students/g_rama/lesson03/src/basic_operations.py
@@ -0,0 +1,22 @@
+import logging
+import sqlite3
+
+
+def add_customer():
+    pass
+
+
+def search_customer():
+    pass
+
+
+def delete_customer():
+    pass
+
+
+def update_customer_credit():
+    pass
+
+
+def list_active_customers():
+    pass
---
+++ students/g_rama/lesson03/tests/test_basic_operations.py
@@ -0,0 +1,21 @@
+from src import basic_operaions
+
+
+def test_add_customer():
+    pass
+
+
+def test_search_customer():
+    pass
+
+
+def test_delete_customer():
+    pass
+
+
+def test_update_customer_credit():
+    pass
+
+
+def test_list_active_customers():
+    pass
