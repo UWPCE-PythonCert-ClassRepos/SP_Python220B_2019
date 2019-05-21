29cf25611a0c2cc7e78660695e05b64b83f47aa9
guntur <ramakanth.gunturu@gmail.com>
2019-05-20 19:47:55 -0700

    Fixed the duplicate code issue for inventory management system

--- students/g_rama/lesson01/inventory_management/furniture_class.py
+++ students/g_rama/lesson01/inventory_management/furniture_class.py
@@ -16,11 +16,7 @@ class Furniture(Inventory):  # pylint: disable=too-few-public-methods
 
     def return_as_dictionary(self):
         """Function to store furniture as a data dictionary"""
-        output_dict = dict()
-        output_dict['product_code'] = self.product_code
-        output_dict['description'] = self.description
-        output_dict['market_price'] = self.market_price
-        output_dict['rental_price'] = self.rental_price
+        output_dict = Inventory.return_as_dictionary()
         output_dict['material'] = self.material
         output_dict['size'] = self.size
 
