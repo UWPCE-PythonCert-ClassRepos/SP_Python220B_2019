1b1db0dd097dec3fd1c2f92ea417a7c97b9c223e
guntur <ramakanth.gunturu@gmail.com>
2019-06-01 13:23:02 -0700

    After adding the mongodb connections

--- students/g_rama/lesson05/src/database.py
+++ students/g_rama/lesson05/src/database.py
@@ -22,10 +22,6 @@ class MongoDBConnection():
         self.connection.close()
 
 
-directory_name = "/Users/guntur/PycharmProjects/uw/" \
-           "p220/SP_Python220B_2019/students/g_rama/lesson05/src/data"
-
-
 def import_data(directory_name, product_file, customer_file, rentals_file):
     """Import data for inventory management"""
 
@@ -34,22 +30,36 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
         # mongodb database; it all starts here
         db = mongo.connection.hpnorton
 
+        product_file_csv = os.path.join(directory_name, product_file)
+        customer_file_csv = os.path.join(directory_name, customer_file)
+        rentals_file_csv = os.path.join(directory_name, rentals_file)
+
         products = db["products"]
         customers = db["customers"]
         rentals = db["rentals"]
 
-        list_of_files = {}
-        for filename in os.listdir(path):
-            # if the element is a csv file then..
-            if filename[-4:] == ".csv":
-                list_of_files[filename] = path + "\\" + filename
-                print(list_of_files[filename])
-                with open(list_of_files[filename], encoding="utf8") as f:
-                    csv_f = csv.reader(f)
-                    for i, row in enumerate(csv_f):
-                        if i > 5 and len(row) > 1:
-                            print(row)
-                            db.insert({'F1': row[0], 'F2': row[1]})
+        with open(product_file_csv) as product:
+            products_csv = csv.DictReader(product)
+            for row in products_csv:
+                print(row)
+                products.insert_one(row)
+
+        with open(customer_file_csv) as product:
+            products_csv = csv.DictReader(product)
+            for row in products_csv:
+                print(row)
+                customers.insert_one(row)
+
+        with open(rentals_file_csv) as product:
+            products_csv = csv.DictReader(product)
+            for row in products_csv:
+                print(row)
+                rentals.insert_one(row)
+
+    with mongo:
+        db = mongo.connection.hpnorton
+        print("Printing the collection of DB")
+        print(db.list_collection_names())
 
 
 def show_available_products():
@@ -58,5 +68,12 @@ def show_available_products():
 
 
 def show_rentals(product_id):
-    """Display the users who rented a prouct"""
+    """Display the users who rented a product"""
     pass
+
+
+if __name__ == '__main__':
+    directory_name = "/Users/guntur/PycharmProjects/uw/" \
+               "p220/SP_Python220B_2019/students/g_rama/lesson05/src/data"
+    import_data(directory_name,"customers.csv","products.csv","rentals.csv")
+
--- students/g_rama/lesson05/tests/test_database.py
+++ students/g_rama/lesson05/tests/test_database.py
@@ -1,1 +1,20 @@
 """Testing the database.py"""
+import logging
+import unittest
+import sys
+sys.path.append('/Users/guntur/PycharmProjects/uw/p220/SP_Python220B_2019/'
+                'students/g_rama/lesson05/src/')
+import database
+
+
+class TestDatabase(unittest.TestCase):
+
+    def test_import_data(self, directory_name, product_file, customer_file, rentals_file):
+        """Testing """
+        database.import_data(directory_name, product_file, customer_file, rentals_file)
+
+    def test_show_available_products(self):
+        pass
+
+    def test_show_rentals(self):
+        pass
