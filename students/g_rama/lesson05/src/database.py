df43ea375af0a7514dd77c70d818e10f98ac9da5
guntur <ramakanth.gunturu@gmail.com>
2019-06-02 14:00:05 -0700

    Corrected to have the encoding to fix key error issue

--- students/g_rama/lesson05/src/database.py
+++ students/g_rama/lesson05/src/database.py
@@ -46,7 +46,12 @@ def import_data(directory_name, product_file, customer_file, rental_file):
         customers = db["customers"]
         rentals = db["rentals"]
 
-        with open(product_file_csv) as product:
+        with mongo:
+            db = mongo.connection.hpnorton
+            print("Printing the collection of DB")
+            print(db.list_collection_names())
+
+        with open(product_file_csv, encoding='utf-8-sig') as product:
             products_csv = csv.DictReader(product)
             for row in products_csv:
                 print(row)
@@ -55,7 +60,7 @@ def import_data(directory_name, product_file, customer_file, rental_file):
                 except pymongo.errors.DuplicateKeyError:
                     products_error += 1
 
-        with open(customer_file_csv) as customer:
+        with open(customer_file_csv, encoding='utf-8-sig') as customer:
             customers_csv = csv.DictReader(customer)
             for row in customers_csv:
                 print(row)
@@ -64,7 +69,7 @@ def import_data(directory_name, product_file, customer_file, rental_file):
                 except pymongo.errors.DuplicateKeyError:
                     customers_error += 1
 
-        with open(rental_file_csv) as rental:
+        with open(rental_file_csv, encoding='utf-8-sig') as rental:
             rentals_csv = csv.DictReader(rental)
             for row in rentals_csv:
                 print(row)
@@ -80,11 +85,6 @@ def import_data(directory_name, product_file, customer_file, rental_file):
         tuple1 = (products_count, customers_count, rentals_count)
         tuple2 = (products_error, customers_error, rentals_error)
         return tuple1, tuple2
-    #
-    # with mongo:
-    #     db = mongo.connection.hpnorton
-    #     print("Printing the collection of DB")
-    #     print(db.list_collection_names())
 
 
 def show_available_products():
@@ -92,20 +92,24 @@ def show_available_products():
     mongo = MongoDBConnection()
     with mongo:
         db = mongo.connection.hpnorton
-        products = db.products.find()
+        print(db.list_collection_names())
+        product_all = db.products.find()
+        print(product_all)
         print("NAME" + "    " + "Address" + "    " + "phone_number" + "   " + "email")
-        for product in products:
-            print(f'{product["name"]} {product["address"]} { product["phone_number"]} {product["email"]}\n')
+        for p in db.products.find():
+            print(f'{p["product_id"]},{p["description"]},'
+                  f'{p["product_type"]},{p["quantity_available"]}\n')
 
 
-def show_rentals():
+
+def show_rentals(product_id):
     """Display the users who rented a product"""
     mongo = MongoDBConnection()
     with mongo:
         db = mongo.connection.hpnorton
         # print(product_id)
         # print(type(product_id))
-        rentals = db.rentals.find()
+        rentals_all = db.rentals.find({'product_id': {'$eq': product_id}})
         # {'product_id': {'$in': [product_id]}}
         #type(rentals)
         #rentals = db.rentals.find()
@@ -114,7 +118,7 @@ def show_rentals():
         #     print("i am in")
         #     print(f'{rental["product_id"]} \n')
         #
-        for rental in rentals:
+        for rental in rentals_all:
             print(rental['user_id'])
 
 
@@ -124,16 +128,19 @@ def drop_collections():
     with mongo:
         db = mongo.connection.hpnorton
         db.products.drop()
+        print("deleted the products")
         db.customers.drop()
+        print("deleted the customers")
         db.rentals.drop()
+        print("deleted the rentals")
 
 
 if __name__ == '__main__':
     directory_name = "/Users/guntur/PycharmProjects/uw/" \
                "p220/SP_Python220B_2019/students/g_rama/lesson05/src/data"
-    import_data(directory_name, "customers.csv", "products.csv", "rentals.csv")
+    import_data(directory_name, "products.csv", "customers.csv", "rentals.csv")
     show_available_products()
 
-    show_rentals()
-    drop_collections()
+    #show_rentals("p101")
+    #drop_collections()
 
