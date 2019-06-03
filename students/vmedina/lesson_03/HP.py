5729f8b5fcbb89bdc991c0579ee8d61952e333d0
Victor Medina <vmedina1014@gmail.com>
2019-06-02 21:14:46 -0700

    added lesson_03

---
+++ students/vmedina/lesson_03/HP.py
@@ -0,0 +1,39 @@
+"""
+    Simple database example with Peewee ORM, sqlite and Python
+    Here we define the schema
+    Use logging for messages so they can be turned off
+
+"""
+import logging
+from peewee import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+database = SqliteDatabase('customers.db')
+logger.info('Created database customers.db')
+database.connect()
+logger.info('Connected to database')
+database.execute_sql('PRAGMA foreign_keys = ON;')
+logger.info('Setting up Models')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Customer(BaseModel):
+    """
+        This class defines Customer, which maintains details of customers
+        and their info.
+    """
+    customer_id = IntegerField(primary_key=True, null=False)
+    customer_name = CharField(max_length=30)
+    customer_last_name = CharField(max_length=30)
+    home_address = CharField(max_length=30)
+    phone_number = CharField(max_length=30)
+    email_address = CharField(max_length=30)
+    status = CharField(max_length=30)
+    credit_limit = IntegerField(default=200)
---
+++ students/vmedina/lesson_03/basic_operations.py
@@ -0,0 +1,114 @@
+"""
+Module that includes operations on the customers database
+"""
+# pylint: disable= W0401,W1202
+import logging
+from HP import Customer
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+def add_customer(customer_id, name, last_name, home_address, phone_number, email_address, status,
+                 credit_limit):
+    """
+    :param customer_id:
+    :type customer_id:
+    :param name:
+    :type name:
+    :param last_name:
+    :type last_name:
+    :param home_address:
+    :type home_address:
+    :param phone_number:
+    :type phone_number:
+    :param email_address:
+    :type email_address:
+    :param status:
+    :type status:
+    :param credit_limit:
+    :type credit_limit:
+    :return:
+    :rtype:
+    """
+    try:
+        new_customer = Customer.create(customer_id=customer_id,
+                                       customer_name=name,
+                                       customer_last_name=last_name,
+                                       home_address=home_address,
+                                       phone_number=phone_number,
+                                       email_address=email_address,
+                                       status=status,
+                                       credit_limit=int(credit_limit))
+        new_customer.save()
+        LOGGER.info('Added a new customer')
+
+    except Exception as error:
+        LOGGER.info('Customer with ID {} could not be created, check all inputs'
+                    .format(customer_id))
+        LOGGER.info(error)
+
+
+def search_customer(customer_id):
+    """
+
+    :param customer_id:
+    :type customer_id:
+    :return:
+    :rtype:
+    """
+    try:
+        customer = Customer.get(Customer.customer_id == customer_id)
+        LOGGER.info('Found customer with id {} '.format(customer_id))
+        customer_info = {'name': customer.customer_name,
+                         'last_name': customer.customer_last_name,
+                         'email_address': customer.email_address,
+                         'phone_number': customer.phone_number}
+        return customer_info
+    except Exception as error:
+        LOGGER.info(error)
+        return {}
+
+
+def delete_customer(customer_id):
+    """
+
+    :param customer_id:
+    :type customer_id:
+    :return:
+    :rtype:
+    """
+    LOGGER.info('Deleting customer with id {}'.format(customer_id))
+    customer = Customer.get(Customer.customer_id == customer_id)
+    try:
+        customer.delete_instance()
+    except Exception as error:
+        LOGGER.info('Customer with id {} not deleted'.format(customer_id))
+        LOGGER.info(error)
+
+
+def update_customer_credit(customer_id, credit_limit):
+    """
+
+    :param customer_id:
+    :type customer_id:
+    :param credit_limit:
+    :type credit_limit:
+    :return:
+    :rtype:
+    """
+
+    customer = Customer.get(Customer.customer_id == customer_id)
+    LOGGER.info('Credit limit before the change is {}'.format(customer.credit_limit))
+    customer.credit_limit = int(credit_limit)
+    customer.save()
+    LOGGER.info('New credit limit is {}'.format(customer.credit_limit))
+
+
+def list_active_customers():
+    """
+    :return:
+    :rtype:
+    """
+    query = Customer.select().where(Customer.status == True).count()
+    return query
