498f57338dd20cfc598adc729a3823872ac1afe8
Greg DeVore <greg.h.devore@gmail.com>
2019-07-05 11:42:12 -0700

    Fixed pylint and pep8 errors

--- students/gregdevore/lesson01/activity/calculator/multiplier.py
+++ students/gregdevore/lesson01/activity/calculator/multiplier.py
@@ -1,5 +1,30 @@
-class Multiplier(object):
+"""
+This module provides a multiplication operator
+"""
 
+
+class Multiplier(object):
+    """
+    Class for multiplying two numbers
+
+    Methods:
+
+        calc(operand_1, operand_2):
+            Static method, takes two operands as input and returns their
+            product
+
+    """
     @staticmethod
     def calc(operand_1, operand_2):
+        """
+        Method to multiply two numbers
+
+        Args:
+
+            operand_1, operand_2 (numeric):
+                Numbers to multiply
+
+        Returns the product of the two operands
+
+        """
         return operand_1*operand_2
