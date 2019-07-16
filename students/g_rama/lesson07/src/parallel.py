d08960868d0933bd9b5d001b1b1d7eefedd0e778
guntur <ramakanth.gunturu@gmail.com>
2019-07-16 15:12:57 -0700

    Added the thread pool

--- students/g_rama/lesson07/src/parallel.py
+++ students/g_rama/lesson07/src/parallel.py
@@ -5,12 +5,12 @@ import pymongo
 from pymongo import MongoClient
 from line_profiler import LineProfiler
 import atexit
-from threading import Thread
-from queue import Queue
-import time
+import multiprocessing
+from multiprocessing.pool import Pool
 profile = LineProfiler()
 atexit.register(profile.print_stats)
 # acquire_lock = threading.Lock()
+
 
 class MongoDBConnection:
     """MongoDB Connection"""
@@ -30,20 +30,24 @@ class MongoDBConnection:
 
 
 @profile
-def import_data(directory_name, product_file, customer_file, rental_file):
+def import_data(directory_name, customer_file, product_file, rental_file):
     """Import data for inventory management"""
 
-    # threads = []
-    products_count, products_error = import_generic(directory_name, product_file, "products")
-    #products_count, products_error = threads.append(Thread(target  =  import_generic(directory_name, product_file, "products")))
-    customers_count, customers_error = import_generic(directory_name, customer_file, "customers")
-    rentals_count, rentals_error = import_generic(directory_name, rental_file, "rentals")
-
-    tuple1 = (products_count, customers_count, rentals_count)
-    tuple2 = (products_error, customers_error, rentals_error)
-    print(tuple1)
-    print(tuple2)
-    return tuple1, tuple2
+    with multiprocessing.pool.Pool() as pool:
+        """Using apply_asyn to run two jobs parllely"""
+        cust = pool.apply_async(import_generic, (directory_name, customer_file, "customers"))
+        prod = pool.apply_async(import_generic, (directory_name, product_file, "products"))
+        customer_prior_imported_table_count, customer_imported_table_count, customer_after_imported_table_count = \
+            cust.get()
+        product_prior_imported_table_count, product_imported_table_count, product_after_imported_table_count = \
+            prod.get()
+        customer_tuple = (customer_prior_imported_table_count, customer_imported_table_count,
+                          customer_after_imported_table_count)
+        product_tuple = (product_prior_imported_table_count, product_imported_table_count,
+                         product_after_imported_table_count)
+        print(customer_tuple)
+        print(product_tuple)
+        return customer_tuple, product_tuple
 
 
 @profile
@@ -53,7 +57,7 @@ def import_generic(directory_name, import_file, imported_table):
         # mongodb database; it all starts here
         DB = mongo.connection.hpnorton
         imported_error = 0
-        imported_table_count = 0
+        #imported_table_count = 0
         try:
             import_file_csv = os.path.join(directory_name, import_file)
         except FileNotFoundError:
@@ -69,28 +73,16 @@ def import_generic(directory_name, import_file, imported_table):
                         imp_table.insert_one(row)
                     except pymongo.errors.DuplicateKeyError:
                         imported_error += 1
-                """ Python 3.7 supports coun_documents, python 3.7 has line_profiler compiling issues"""
-                # if imported_table == 'products':
-                #     imported_table_count = DB.products.count_documents({})
-                # elif imported_table == 'customers':
-                #     imported_table_count = DB.customers.count_documents({})
-                # elif imported_table == 'rentals':
-                #     imported_table_count = DB.rentals.count_documents({})
+
                 after_imported_table_count = DB[imported_table].count()
-                #
-                # if imported_table == 'products':
-                #     imported_table_count = DB.products.count()
-                # elif imported_table == 'customers':
-                #     imported_table_count = DB.customers.count()
-                # elif imported_table == 'rentals':
-                #     imported_table_count = DB.rentals.count()
+
         except FileNotFoundError:
             imported_error += 1
         imported_table_count = after_imported_table_count - prior_imported_table_count
         print(imported_table_count)
         print(imported_error)
 
-        return imported_table_count, imported_error
+        return prior_imported_table_count, imported_table_count, after_imported_table_count
 
 
 def show_available_products():
