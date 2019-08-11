d39298badcb11f6856f63e81d0ba8438c0f2c467
Greg DeVore <greg.h.devore@gmail.com>
2019-08-11 14:42:01 -0700

    Added integration test and updated all code for pylint conformity

--- students/gregdevore/lesson03/assignment/basic_operations.py
+++ students/gregdevore/lesson03/assignment/basic_operations.py
@@ -7,33 +7,67 @@ from peewee import OperationalError, IntegrityError, DoesNotExist, fn
 from customer_model import database, Customer
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-logger.info('One off program to build the class from the model in the database')
+LOGGER.info('One off program to build the class from the model in the database')
 database.create_tables([Customer])
 database.close()
 
-def add_customer(id, firstname, lastname, address, phone,email, status, credit_limit):
+def add_customer(customer_id, firstname, lastname, address, phone, email, status, credit_limit):
+    '''
+    Add customer to database, handle several peewee exceptions in the event
+    of an error.
+
+    Args:
+        customer_id (str):
+            Unique customer id (5 characters)
+        firstname (str):
+            Customer's first name
+        lastname (str):
+            Customer's last name
+        address (str):
+            Customer's address
+        phone (str):
+            Customer's phone number (format XXX-XXX-XXXX)
+        email (str):
+            Customer's email address
+        status (boolean):
+            Customer's status (active or not)
+        credit_limit (float):
+            Customer's credit limit in dollars
+
+    '''
     try:
         with database.transaction():
             new_customer = Customer.create(
-            id = id,
-            firstname = firstname,
-            lastname = lastname,
-            address = address,
-            phone = phone,
-            email = email,
-            status = status,
-            credit_limit = credit_limit)
+                id=customer_id,
+                firstname=firstname,
+                lastname=lastname,
+                address=address,
+                phone=phone,
+                email=email,
+                status=status,
+                credit_limit=credit_limit)
         new_customer.save()
-        logger.info('Database add successful')
-    except (OperationalError, IntegrityError, DoesNotExist) as e:
-        logger.info(f'Error creating entry for customer {id}, see error below')
-        logger.info(e)
+        LOGGER.info('Database add successful')
+    except (OperationalError, IntegrityError, DoesNotExist) as exc:
+        LOGGER.info(f'Error creating entry for customer {customer_id}, see error below')
+        LOGGER.info(exc)
 
-def search_customer(id):
+def search_customer(customer_id):
+    '''
+    Search for customer in database.
+
+    Args:
+        customer_id (str):
+            Unique customer ID (5 characters)
+
+    Returns:
+        Dictionary with customer information or empty dictionary if customer
+        not found.
+    '''
     try:
-        customer = Customer.get(Customer.id == id)
+        customer = Customer.get(Customer.id == customer_id)
         # Return dictionary of customer name, email, phone
         return {'firstname': customer.firstname, 'lastname': customer.lastname,
                 'email': customer.email, 'phone':customer.phone}
@@ -41,20 +75,42 @@ def search_customer(id):
         # Customer not found, return empty dictionary
         return {}
 
-def delete_customer(id):
+def delete_customer(customer_id):
+    '''
+    Delete customer from database.
+
+    Args:
+        customer_id (str):
+            Unique customer ID (5 characters)
+    '''
     # Retrieve and delete customer from database
-    customer = Customer.get(Customer.id == id)
+    customer = Customer.get(Customer.id == customer_id)
     customer.delete_instance()
 
-def update_customer_credit(id, credit_limit):
+def update_customer_credit(customer_id, credit_limit):
+    '''
+    Update customer credit limit. Raises value error if customer not found.
+
+    Args:
+        customer_id (str):
+            Unique customer ID (5 characters)
+        credit_limit (float):
+            New customer credit limit
+    '''
     try:
         # Retrieve customer and update credit limit
-        customer = Customer.get(Customer.id == id)
+        customer = Customer.get(Customer.id == customer_id)
         customer.credit_limit = credit_limit
         customer.save()
     except DoesNotExist:
-        raise ValueError(f'Customer with id {id} does not exist.')
+        raise ValueError(f'Customer with id {customer_id} does not exist.')
 
 def list_active_customers():
-    query = Customer.select(Customer, fn.SUM(Customer.status)).where(Customer.status == True)
+    '''
+    Return count of active customers in database.
+
+    Returns:
+        Count of active customers (int)
+    '''
+    query = Customer.select(Customer, fn.SUM(Customer.status)).where(Customer.status)
     return query.count()
--- students/gregdevore/lesson03/assignment/tests/test_unit.py
+++ students/gregdevore/lesson03/assignment/tests/test_unit.py
@@ -1,72 +1,147 @@
+'''
+Unit tests for basic_operations module (Customer database API)
+'''
+
 import sys
 # Add path to files
-sys.path.append('/Users/gdevore21/Documents/Certificate Programs/Python/PYTHON220/SP_Python220B_2019/students/gregdevore/lesson03/assignment')
+sys.path.append('/Users/gdevore21/Documents/Certificate Programs/Python/PYTHON220/ \
+SP_Python220B_2019/students/gregdevore/lesson03/assignment')
 
-from customer_model import database, Customer
-from unittest import TestCase, TestLoader
+from unittest import TestCase
 from peewee import DoesNotExist
+from customer_model import database, Customer
 import basic_operations
 
 class CustomerTests(TestCase):
+    '''
+    Test suite for unit testing the basic_operations module
 
+    Methods:
+        setUpClass(cls):
+            Class method, run before any tests, clears Customer database,
+            creates new customer
+
+        clear_database():
+            Static method, resets database before tests are run
+
+        add_customer_to_database(self):
+            Method called by multiple tests, adds customer if not in database
+
+        test_customer_add(self):
+            Test method to add customer to database
+
+        test_search_customer(self):
+            Test method to search for customer
+
+        test_search_fake_customer(self):
+            Tests case where customer being searched does not exist
+
+        test_delete_customer(self):
+            Tests method to delete customer from database
+
+        test_delete_fake_customer(self):
+            Tests case where customer being deleted does not exist
+
+        test_update_customer_credit(self):
+            Tests method to update customer credit, verifies new limit
+
+        test_update_fake_customer_credit(self):
+            Tests case where customer being updated does not exist
+
+        test_active_customers(self):
+            Tests method to return integer count of active customers
+    '''
     @classmethod
     def setUpClass(cls):
+        '''
+        Run before any tests. Clears Customer database, creates new customer
+        '''
         # Clear database for tests
-        cls.clearDatabase()
+        cls.clear_database()
         # Create new customers, visible by all test cases
         cls.new_customer = {'id':'00001', 'firstname':'Ron', 'lastname':'Swanson',
-        'address':'123 Fake Street', 'phone':'555-867-5309',
-        'email':'ronswanson@pawnee.gov', 'status':0, 'credit_limit':10000}
-        # Make sure tests run in order specified (subsequent tests rely on first)
-        TestLoader.sortTestMethodsUsing = None
+                            'address':'123 Fake Street', 'phone':'555-867-5309',
+                            'email':'ronswanson@pawnee.gov', 'status':0,
+                            'credit_limit':10000}
 
     @staticmethod
-    def clearDatabase():
+    def clear_database():
+        '''
+        Resets database before tests are run
+        '''
         # Set up empty table for testing
         database.drop_tables([Customer])
         database.create_tables([Customer])
 
     def add_customer_to_database(self):
+        '''
+        Method called by multiple tests, adds customer if not in database
+        '''
         # Add customer if not part of database (certain tests add/delete records)
         query = Customer.select().where(Customer.id == self.new_customer['id'])
         if not query.exists():
             # Add customer (used by multiple tests)
             basic_operations.add_customer(self.new_customer['id'],
-            self.new_customer['firstname'],self.new_customer['lastname'],
-            self.new_customer['address'], self.new_customer['phone'],
-            self.new_customer['email'], self.new_customer['status'],
-            self.new_customer['credit_limit'])
+                                          self.new_customer['firstname'],
+                                          self.new_customer['lastname'],
+                                          self.new_customer['address'],
+                                          self.new_customer['phone'],
+                                          self.new_customer['email'],
+                                          self.new_customer['status'],
+                                          self.new_customer['credit_limit'])
 
     def test_customer_add(self):
+        '''
+        Test method to add customer to database
+        '''
         # Add customer to database
         self.add_customer_to_database()
         # Retrieve customer to ensure all fields were added
         customer = Customer.get(Customer.id == self.new_customer['id'])
-        self.assertEqual(customer.id,self.new_customer['id'])
-        self.assertEqual(customer.firstname,self.new_customer['firstname'])
-        self.assertEqual(customer.lastname,self.new_customer['lastname'])
-        self.assertEqual(customer.address,self.new_customer['address'])
-        self.assertEqual(customer.phone,self.new_customer['phone'])
-        self.assertEqual(customer.email,self.new_customer['email'])
-        self.assertEqual(customer.status,self.new_customer['status'])
-        self.assertEqual(customer.credit_limit,self.new_customer['credit_limit'])
+        self.assertEqual(customer.id, self.new_customer['id'])
+        self.assertEqual(customer.firstname, self.new_customer['firstname'])
+        self.assertEqual(customer.lastname, self.new_customer['lastname'])
+        self.assertEqual(customer.address, self.new_customer['address'])
+        self.assertEqual(customer.phone, self.new_customer['phone'])
+        self.assertEqual(customer.email, self.new_customer['email'])
+        self.assertEqual(customer.status, self.new_customer['status'])
+        self.assertEqual(customer.credit_limit, self.new_customer['credit_limit'])
+
+        # Add customer again, should handle exception
+        basic_operations.add_customer(self.new_customer['id'],
+                                      self.new_customer['firstname'],
+                                      self.new_customer['lastname'],
+                                      self.new_customer['address'],
+                                      self.new_customer['phone'],
+                                      self.new_customer['email'],
+                                      self.new_customer['status'],
+                                      self.new_customer['credit_limit'])
 
     def test_search_customer(self):
+        '''
+        Test method to search for customer
+        '''
         # Add customer to database
         self.add_customer_to_database()
         # Define expected return value
         return_dict = {'firstname':'Ron', 'lastname':'Swanson',
-        'phone':'555-867-5309', 'email':'ronswanson@pawnee.gov'}
+                       'phone':'555-867-5309', 'email':'ronswanson@pawnee.gov'}
         # Search for customer in database
         customer_dict = basic_operations.search_customer(self.new_customer['id'])
         self.assertEqual(return_dict, customer_dict)
 
     def test_search_fake_customer(self):
+        '''
+        Tests case where customer being searched does not exist
+        '''
         # Search for nonexistant customer, should return empty dictionary
         fake_customer_dict = basic_operations.search_customer('99999')
         self.assertEqual({}, fake_customer_dict)
 
     def test_delete_customer(self):
+        '''
+        Tests method to delete customer from database
+        '''
         # Add customer to database
         self.add_customer_to_database()
         # Delete customer from database
@@ -76,24 +151,36 @@ class CustomerTests(TestCase):
             Customer.get(Customer.id == self.new_customer['id'])
 
     def test_delete_fake_customer(self):
+        '''
+        Tests case where customer being deleted does not exist
+        '''
         # Deleting a nonexistant customer should raise an error
         with self.assertRaises(DoesNotExist):
             basic_operations.delete_customer('99999')
 
     def test_update_customer_credit(self):
+        '''
+        Tests method to update customer credit, verifies new limit
+        '''
         self.add_customer_to_database()
         # Update credit limit
-        basic_operations.update_customer_credit(self.new_customer['id'],15000)
+        basic_operations.update_customer_credit(self.new_customer['id'], 15000)
         # Verify that credit limit has been updated
         updated_customer = Customer.get(Customer.id == self.new_customer['id'])
         self.assertEqual(updated_customer.credit_limit, 15000)
 
     def test_update_fake_customer_credit(self):
+        '''
+        Tests case where customer being updated does not exist
+        '''
         # Ensure that updating a fake customer's credit raises a value error
         with self.assertRaises(ValueError):
-            basic_operations.update_customer_credit('99999',5000)
+            basic_operations.update_customer_credit('99999', 5000)
 
     def test_active_customers(self):
+        '''
+        Tests method to return integer count of active customers
+        '''
         # Empty database should have no active customers
         num_active = basic_operations.list_active_customers()
         self.assertEqual(num_active, 0)
