f3a63f6ced5c1ff32c375832e2dd22ac87161b52
guntur <ramakanth.gunturu@gmail.com>
2019-08-12 01:53:45 -0700

    Images recursion

--- students/g_rama/lesson09/pngdiscover.py
+++ students/g_rama/lesson09/pngdiscover.py
@@ -1,17 +1,33 @@
+"""Recursion exercise to go to the bottom of th folders"""
 import os
 import sys
 
-png_root_dir = sys.argv[1]
+#png_root_dir = sys.argv[1]
 
-print('png_root_dir = ' + png_root_dir)
+# print('png_root_dir = ' + png_root_dir)
+#
+#
+# print('png_root_dir (absolute) = ' + os.path.abspath(png_root_dir))
+
+# for dir_name, sub_dirs, files in os.walk(PNG_ROOT_DIR):
+#     sub_dirs = [n for n in sub_dirs]
+#     images = sub_dirs + files
+#     images.sort()
+#     for image in images:
+#         print(image)
 
 
-print('png_root_dir (absolute) = ' + os.path.abspath(png_root_dir))
+def image_recursion(root_dir):
+    dirs = os.listdir(root_dir)
+    print(dirs)
+    images = []
+    for file in dirs:
+        images.append(file)
+        if os.path.isdir(os.path.join(file)) is True:
+            image_recursion(file)
+            print(images)
 
-for dir_name, sub_dirs, files in os.walk(png_root_dir):
-    sub_dirs = [n for n in sub_dirs]
-    images = sub_dirs + files
-    images.sort()
-    for image in images:
-        print(image)
 
+if __name__ == '__main__':
+    png_root_dir = sys.argv[1]
+    image_recursion(png_root_dir)
