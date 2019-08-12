32fef49ba8084fe7ae483030d92d72d93762feb2
guntur <ramakanth.gunturu@gmail.com>
2019-08-11 21:45:16 -0700

    Removed the ignore pylint errors

--- students/g_rama/lesson04/src/create_customerdb.py
+++ students/g_rama/lesson04/src/create_customerdb.py
@@ -1,9 +1,4 @@
 """Creates a customer database with peewee ORM, sqlite and python"""
-# pylint: disable=unused-wildcard-import,wildcard-import,too-many-arguments,wrong-import-position
-<<<<<<< HEAD
-=======
-from customer_model import Customer, DB, IntegrityError
->>>>>>> 4ac53ea01d4e497495a3c3acfbb6ca719fec7eaf
 import logging
 from customer_model import Customer, DB
 
