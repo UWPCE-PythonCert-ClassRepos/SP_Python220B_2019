0835c179f145792f3ca52bb06cb4c37053042f65
guntur <ramakanth.gunturu@gmail.com>
2019-08-14 00:37:48 -0700

    Recursion of directories

--- students/g_rama/lesson09/pngdiscover.py
+++ students/g_rama/lesson09/pngdiscover.py
@@ -19,15 +19,45 @@ import sys
 
 def image_recursion(root_dir):
     dirs = os.listdir(root_dir)
+    print("rmakanth")
     print(dirs)
+    #return dirs
     images = []
     for file in dirs:
+        print(file)
         images.append(file)
-        if os.path.isdir(os.path.join(file)) is True:
-            image_recursion(file)
-            print(images)
+        if os.path.isdir(os.path.join(root_dir, file)):
+            path_in = os.path.join(root_dir, file)
+            print(path_in)
+            dirs_in  = os.listdir(path_in)
+            images.append(dirs_in)
+            images_in = []
+            for file_in in dirs_in:
+                images_in.append(file)
+                #images.append(images_in)
+                if os.path.isdir(os.path.join(path_in, file_in)):
+                    print(os.path.join(path_in, file_in))
+                    inside_image = image_recursion(os.path.join(path_in, file_in))
+                    print(inside_image)
+                    images_in.append(inside_image)
+
+    return images
+
+
+
+
+           # image_recursion(os.path.join(root_dir, file))
+
+
+        # images.append(file)
+        # if os.path.isdir(file):
+        #     path = os.path.join(file, "")
+        #     print(path)
+        #     image_recursion(path)
+        #     print(images)
 
 
 if __name__ == '__main__':
     png_root_dir = sys.argv[1]
-    image_recursion(png_root_dir)
+    direc = image_recursion(png_root_dir)
+    print(direc)
