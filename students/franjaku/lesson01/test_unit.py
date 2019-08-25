70e6e7b018e4429dbe4b923cc4c16a34c1444b96
franjaku <kulyckyf@umich.edu>
2019-08-25 13:49:25 -0700

    added test files and wrote inventory class tests

---
+++ students/franjaku/lesson01/test_unit.py
@@ -0,0 +1,28 @@
+# test_unit.py
+"""Create unit tests for inventory management classes."""
+import unittest
+
+from inventory_management.inventory_class import Inventory
+
+
+class InventoryTest(unittest.TestCase):
+    """Inventory class tests."""
+
+    def test_init(self):
+        # test inventory item initializes and correct sets initial attributes/properties
+        inven = Inventory(1025, "test item", 80.00, 10.00)
+        self.assertEqual(inven.product_code, 1025)
+        self.assertEqual(inven.description, "test item")
+        self.assertEqual(inven.market_price, 80.00)
+        self.assertEqual(inven._rental_price, 10.00)
+
+    def test_dict_return(self):
+        # test that a dictionary of the item is returned and the fields are correct
+        inven = Inventory(1025, "test item", 80.00, 10.00)
+        inven_dict = inven.return_as_dictionary()
+
+        self.assertIsInstance(inven_dict, dict, "testing output is of type dictionary")
+        self.assertEqual(inven_dict["product_code"], 1025)
+        self.assertEqual(inven_dict["description"], "test item")
+        self.assertEqual(inven_dict["market_price"], 80.00)
+        self.assertEqual(inven_dict["rental_price"], 10.00)
