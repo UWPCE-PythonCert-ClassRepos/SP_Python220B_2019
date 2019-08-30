052e87d634883b89a0c93a3a348ac5ee92cd8eee
Greg DeVore <greg.h.devore@gmail.com>
2019-08-30 11:02:24 -0700

    Created import data method and associated tests

--- students/gregdevore/lesson05/assignment/database.py
+++ students/gregdevore/lesson05/assignment/database.py
@@ -47,48 +47,103 @@ def import_csv_to_json(csv_file):
             CSV file to read
 
     Returns:
-        JSON formatted string representation of CSV file contents.
-        Each string is a series of key/values pairs representing a row from
-        the csv file (key = csv column header, value = row value)
+        json_list (JSON):
+            JSON formatted string representation of CSV file contents.
+            Each string is a series of key/values pairs representing a row
+            from the csv file (key = csv column header, value = row value)
+        errors (int):
+            Number of errors encountered during CSV read
     '''
+    json_list = []
+    errors = 0
     try:
         LOGGER.info(f'Reading file \'{csv_file}\'')
         with open(csv_file) as f:
             json_list = [json.loads(json.dumps(row)) for row in csv.DictReader(f)]
     except FileNotFoundError:
         LOGGER.error(f'{csv_file} not found. Check file path and/or name.')
-        sys.exit()
+        errors += 1
     except IOError:
         LOGGER.error(f'Could not read {csv_file}, check file permissions.')
-        sys.exit()
-    return json_list
+        errors += 1
+    return json_list, errors
 
-def import_data(directory_name, product_file, customer_file, rentals_file):
+def add_json_to_mongodb(json_data, db_name):
     '''
-    This function takes a directory name three csv files as input, one with
-    product data, one with customer data and the third one with rentals data
-    and creates and populates a new MongoDB database with these data. It
-    returns 2 tuples: the first with a record count of the number of
-    products, customers and rentals added (in that order), the second with a
-    count of any errors that occurred, in the same order.
+    Method to add JSON formatted data to a mongo database
+
+    Args:
+        json_data (JSON):
+            A list of JSON formatted data
+        db_name (str):
+            Mongo database name
+
+    Returns:
+        collection_count (int):
+            Count of documents added to the database
+        error_count (int):
+            Count of errors generated during document add
     '''
-
-    product_json = import_csv_to_json(os.path.join(directory_name, product_file))
-    customer_json = import_csv_to_json(os.path.join(directory_name, customer_file))
-    rental_json = import_csv_to_json(os.path.join(directory_name, rentals_file))
-
-    print(product_json)
-    print(customer_json)
-    print(rental_json)
-
     # Create mongo database connection
     mongo = MongoDBConnection()
 
+    errors = 0
     try:
         with mongo:
-            pass
+            LOGGER.info('Creating mongo connection')
+            db = mongo.connection.HPNorton
+            LOGGER.info(f'Creating {db_name} database and adding records')
+            new_db = db[db_name]
+            new_db.insert_many(json_data)
     except ConnectionFailure as CF: # MongoDB connection issue
-        pass
+        LOGGER.error('Could not connect to MongoDB database.')
+        LOGGER.error(f'Error message: {CF}')
+        errors += 1
+
+    collection_count = new_db.estimated_document_count()
+
+    return collection_count, errors
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    '''
+    Method to import CSV data and add to a mongo database
+
+    Args:
+        directory_name (str):
+            Directory where CSV files are located
+            Pass empty string if files in current directory
+        product_file (str):
+            Name of CSV file containing product data
+        customer_file (str):
+            Name of CSV file containing customer data
+        rentals_file (str):
+            Name of CSV file containing rentals data
+
+    Returns:
+        counts (tuple):
+            Count of documents added for the product, customer, and rentals
+            database, in that order
+        errors (tuple):
+            Count of errors encountered while adding documents for the product,
+            customer, and rentals database, in that order
+    '''
+
+    # Convert csv files to JSON data
+    product_json, product_csv_errors = import_csv_to_json(os.path.join(directory_name, product_file))
+    customer_json, customer_errors = import_csv_to_json(os.path.join(directory_name, customer_file))
+    rentals_json, rentals_errors = import_csv_to_json(os.path.join(directory_name, rentals_file))
+
+    # Add JSON data to mongo database
+    product_count, product_db_errors = add_json_to_mongodb(product_json, 'product')
+    customer_count, customer_db_errors = add_json_to_mongodb(customer_json, 'customer')
+    rentals_count, rentals_db_errors = add_json_to_mongodb(rentals_json, 'rentals')
+
+    counts = (product_count, customer_count, rentals_count)
+    errors = (product_csv_errors + product_db_errors,
+              customer_errors + customer_db_errors,
+              rentals_errors + rentals_db_errors)
+
+    return counts, errors
 
 def show_available_products():
     '''
