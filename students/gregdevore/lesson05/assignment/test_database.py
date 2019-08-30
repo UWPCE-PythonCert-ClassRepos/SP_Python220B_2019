a55c80f2637f51a40bf895c165b35811ce4993c8
Greg DeVore <greg.h.devore@gmail.com>
2019-08-30 11:02:24 -0700

    Created import data method and associated tests

--- students/gregdevore/lesson05/assignment/test_database.py
+++ students/gregdevore/lesson05/assignment/test_database.py
@@ -3,20 +3,17 @@ Insert docstring
 '''
 
 from unittest import TestCase
-import database
+from database import *
 import os
+from pymongo import MongoClient
 
 class DBTests(TestCase):
     '''
     Test suite for testing the database module
     '''
-    def test_csv_to_json(self):
-        directory = 'data'
-        product_file = 'products.csv'
-        customer_file = 'customers.csv'
-        rentals_file = 'rentals.csv'
-
-        product_json_expected = [{'product_id': 'prd001', 'description': 'sofa',
+    @classmethod
+    def setUpClass(cls):
+        cls.product_json = [{'product_id': 'prd001', 'description': 'sofa',
          'product_type': 'livingroom', 'quantity_available': '4'},
          {'product_id': 'prd002', 'description': 'coffee table',
          'product_type': 'livingroom', 'quantity_available': '2'},
@@ -37,9 +34,7 @@ class DBTests(TestCase):
          {'product_id': 'prd010', 'description': 'queen box spring',
          'product_type': 'bedroom', 'quantity_available': '3'}]
 
-        product_json_actual = database.import_csv_to_json(os.path.join(directory,product_file))
-
-        customer_json_expected = [{'user_id': 'user001', 'name': 'Jake Peralta',
+        cls.customer_json = [{'user_id': 'user001', 'name': 'Jake Peralta',
         'address': '123 Precinct Lane', 'phone_number': '917-555-0001',
         'email': 'jperalta@brooklyn.pd'}, {'user_id': 'user002',
         'name': 'Rose Diaz', 'address': '123 Precinct Lane',
@@ -58,9 +53,7 @@ class DBTests(TestCase):
         'address': '123 Precinct Lane', 'phone_number': '917-555-0007',
         'email': 'glinetti@brooklyn.pd'}]
 
-        customer_json_actual = database.import_csv_to_json(os.path.join(directory,customer_file))
-
-        rentals_json_expected = [{'rental_id': 'rnt001', 'customer_id': 'user003',
+        cls.rentals_json = [{'rental_id': 'rnt001', 'customer_id': 'user003',
         'product_id': 'prd001'}, {'rental_id': 'rnt002', 'customer_id': 'user005',
         'product_id': 'prd005'}, {'rental_id': 'rnt003', 'customer_id': 'user002',
         'product_id': 'prd004'}, {'rental_id': 'rnt004', 'customer_id': 'user006',
@@ -68,8 +61,46 @@ class DBTests(TestCase):
         'product_id': 'prd010'}, {'rental_id': 'rnt006', 'customer_id': 'user001',
         'product_id': 'prd002'}]
 
-        rentals_json_actual = database.import_csv_to_json(os.path.join(directory,rentals_file))
+    @classmethod
+    def tearDown(cls):
+        '''
+        Drop mongo collections after each test to ensure a fresh start
+        '''
+        mongo = MongoDBConnection()
+        with mongo:
+            db = mongo.connection.HPNorton
+            db['product'].drop()
+            db['customer'].drop()
+            db['rentals'].drop()
 
-        self.assertEqual(product_json_expected, product_json_actual)
-        self.assertEqual(customer_json_expected, customer_json_actual)
-        self.assertEqual(rentals_json_expected, rentals_json_actual)
+    def test_csv_to_json(self):
+        directory = 'data'
+        product_file = 'products.csv'
+        customer_file = 'customers.csv'
+        rentals_file = 'rentals.csv'
+
+        product_json_actual, product_errors = import_csv_to_json(os.path.join(directory,product_file))
+        customer_json_actual, customer_errors = import_csv_to_json(os.path.join(directory,customer_file))
+        rentals_json_actual, rentals_errors = import_csv_to_json(os.path.join(directory,rentals_file))
+
+        self.assertEqual(self.product_json, product_json_actual)
+        self.assertEqual(self.customer_json, customer_json_actual)
+        self.assertEqual(self.rentals_json, rentals_json_actual)
+
+    def test_bad_csv_to_json(self):
+        return_data, error_count = import_csv_to_json('fake_file')
+        self.assertEqual(return_data,[])
+        self.assertEqual(error_count, 1)
+
+    def test_json_to_mongo(self):
+        product_counts = add_json_to_mongodb(self.product_json,'product')
+        customer_counts = add_json_to_mongodb(self.customer_json,'customer')
+        rentals_counts = add_json_to_mongodb(self.rentals_json,'rentals')
+        self.assertEqual(product_counts,(10,0))
+        self.assertEqual(customer_counts,(7,0))
+        self.assertEqual(rentals_counts,(6,0))
+
+    def test_import_data(self):
+        counts, errors = import_data('data', 'products.csv', 'customers.csv', 'rentals.csv')
+        self.assertEqual(counts,(10,7,6))
+        self.assertEqual(errors,(0,0,0))
