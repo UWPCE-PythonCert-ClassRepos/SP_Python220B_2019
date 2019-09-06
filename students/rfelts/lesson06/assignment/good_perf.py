16c037df42378d9abeafdf7fc81293819c9c7cad
rfelts <rfelts@adobe.com>
2019-09-06 16:01:01 -0700

    Adding the timing and optimized perf files

--- students/rfelts/lesson06/assignment/good_perf.py
+++ students/rfelts/lesson06/assignment/good_perf.py
@@ -1,21 +1,25 @@
-"""
-Improved performance module
-"""
+#!/usr/bin/env python3
+
+# Russell Felts
+# Assignment 6
+
+""" Improved performance module """
 
 import datetime
 import csv
 
 
 def analyze(filename):
+    """
+    Read over a csv file and gather a count for the number of times a
+    specific year and 'ao' is encountered
+    :param filename: csv file to read
+    :return: start date, end date, dictionary containing year count,
+    and integer containing the number of times 'ao' was found
+    """
     start = datetime.datetime.now()
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        new_ones = []
-        for row in reader:
-            lrow = list(row)
-            if lrow[5] > '00/00/2012':
-                new_ones.append((lrow[5], lrow[0]))
-
         year_count = {
             "2013": 0,
             "2014": 0,
@@ -24,40 +28,28 @@ def analyze(filename):
             "2017": 0,
             "2018": 0
         }
-
-        for new in new_ones:
-            if new[0][6:] == '2013':
-                year_count["2013"] += 1
-            if new[0][6:] == '2014':
-                year_count["2014"] += 1
-            if new[0][6:] == '2015':
-                year_count["2015"] += 1
-            if new[0][6:] == '2016':
-                year_count["2016"] += 1
-            if new[0][6:] == '2017':
-                year_count["2017"] += 1
-            if new[0][6:] == '2018':
-                year_count["2017"] += 1
-
-        print(year_count)
-
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-
         found = 0
-
-        for line in reader:
-            lrow = list(line)
-            if "ao" in line[6]:
+        for row in reader:
+            lrow = list(row)
+            # Update year count based on the year in the row
+            if lrow[5][-4:] in year_count:
+                year_count[lrow[5][-4:]] += 1
+            # Add 1 to the ao found count
+            if "ao" in lrow[6]:
                 found += 1
 
+        print(year_count)
         print(f"'ao' was found {found} times")
+
         end = datetime.datetime.now()
 
-    return (start, end, year_count, found)
+    return start, end, year_count, found
 
 
 def main():
+    """
+    Main function
+    """
     filename = "data/exercise.csv"
     analyze(filename)
 
