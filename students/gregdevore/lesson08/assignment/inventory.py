cedaf14947dac32e7e13b08b86a3dac20b96addd
Greg DeVore <greg.h.devore@gmail.com>
2019-09-18 21:35:41 -0700

    Created single_customer method with test

--- students/gregdevore/lesson08/assignment/inventory.py
+++ students/gregdevore/lesson08/assignment/inventory.py
@@ -2,6 +2,8 @@
 Module to replace existing inventory spreadsheet
 '''
 import os
+import csv
+from functools import partial
 
 def add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
     # Set flag to append/write based on existence of file
@@ -14,4 +16,12 @@ def add_furniture(invoice_file, customer_name, item_code, item_description, item
 
 def single_customer(customer_name, invoice_file):
     def add_rentals(rental_items):
-        pass
+        add_furniture_curried = partial(add_furniture, invoice_file=invoice_file, customer_name=customer_name)
+        with open(rental_items,'r') as rental_file:
+            csv_reader = csv.reader(rental_file)
+            for row in csv_reader:
+                item_code, item_description, item_monthly_price = row
+                add_furniture_curried(item_code=item_code,
+                                      item_description=item_description,
+                                      item_monthly_price=float(item_monthly_price))
+    return add_rentals
--- students/gregdevore/lesson08/assignment/test_inventory.py
+++ students/gregdevore/lesson08/assignment/test_inventory.py
@@ -5,7 +5,7 @@ Test suite for inventory.py module
 import os
 import csv
 from unittest import TestCase
-from inventory import add_furniture
+from inventory import add_furniture, single_customer
 
 class InventoryTests(TestCase):
     '''
@@ -13,23 +13,46 @@ class InventoryTests(TestCase):
     '''
     @classmethod
     def setUpClass(cls):
-        cls.test_file = 'test_furniture.csv'
+        cls.test_add_furnture_file = 'test_furniture.csv'
+        cls.test_single_customer_read_file = 'test_items.csv'
+        cls.test_single_customer_write_file = 'rented_items.csv'
+        cls.furniture_lines = [['Elisa Miles','LR04','Leather Sofa',25],
+                                ['Edward Data','KT78','Kitchen Table',10],
+                                ['Alex Gonzales','BR02','Queen Mattress',17]]
+        cls.single_customer_lines = [['Susan Wong','LR04','Leather Sofa',25],
+                                     ['Susan Wong','KT78','Kitchen Table',10],
+                                     ['Susan Wong','BR02','Queen Mattress',17]]
+        with open(cls.test_single_customer_read_file,'w') as test_csv:
+            test_csv.write('LR04,Leather Sofa,25.00\n')
+            test_csv.write('KT78,Kitchen Table,10.00\n')
+            test_csv.write('BR02,Queen Mattress,17.00')
 
     @classmethod
     def tearDownClass(cls):
-        os.remove(cls.test_file)
+        os.remove(cls.test_add_furnture_file)
+        os.remove(cls.test_single_customer_read_file)
+        os.remove(cls.test_single_customer_write_file)
 
     def test_add_furniture(self):
-        to_add = [['Elisa Miles','LR04','Leather Sofa',25],
-                  ['Edward Data','KT78','Kitchen Table',10],
-                  ['Alex Gonzales','BR02','Queen Mattress',17]]
-        for item in to_add:
-            add_furniture(self.test_file, *item)
+        for item in self.furniture_lines:
+            add_furniture(self.test_add_furnture_file, *item)
         # Read csv file to confirm correct write
-        with open(self.test_file, 'r') as csv_file:
+        with open(self.test_add_furnture_file, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             for i,row in enumerate(csv_reader):
                 # Convert final column (price) to int
                 row[-1] = int(float(row[-1]))
                 # Ensure row is equal to input data
-                self.assertEqual(to_add[i], row)
+                self.assertEqual(self.furniture_lines[i], row)
+
+    def test_single_customer(self):
+        create_invoice = single_customer("Susan Wong", self.test_single_customer_write_file)
+        create_invoice(self.test_single_customer_read_file)
+        # Read csv file to confirm correct write
+        with open(self.test_single_customer_write_file, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for i,row in enumerate(csv_reader):
+                # Convert final column (price) to int
+                row[-1] = int(float(row[-1]))
+                # Ensure row is equal to input data
+                self.assertEqual(self.single_customer_lines[i], row)
