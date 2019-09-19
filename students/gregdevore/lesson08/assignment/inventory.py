2d4867fad48d4e7f9ec2b64775a2d76cec675009
Greg DeVore <greg.h.devore@gmail.com>
2019-09-19 05:26:14 -0700

    Fixed all pylint warnings

--- students/gregdevore/lesson08/assignment/inventory.py
+++ students/gregdevore/lesson08/assignment/inventory.py
@@ -6,6 +6,21 @@ import csv
 from functools import partial
 
 def add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
+    '''
+    Add furntiture information to csv file
+
+    Args:
+        invoice_file (str):
+            Path to new or existing csv file
+        customer_name (str):
+            Customer name
+        item_code (str):
+            Code for item to add
+        item_description (str):
+            Description of item to add
+        item_monthly_price (int):
+            Price of item to add
+    '''
     # Set flag to append/write based on existence of file
     flag = 'a' if os.path.exists(invoice_file) else 'w'
     with open(invoice_file, flag) as csv_file:
@@ -15,9 +30,39 @@ def add_furniture(invoice_file, customer_name, item_code, item_description, item
                                                    item_monthly_price))
 
 def single_customer(customer_name, invoice_file):
+    '''
+    Add furntiture data from csv file for single customer to new or existing
+    csv file
+
+    Args:
+        customer_name (str):
+            Customer name
+        invoice_file (str):
+            New or existing csv file to add rental items to
+    '''
     def add_rentals(rental_items):
-        add_furniture_curried = partial(add_furniture, invoice_file=invoice_file, customer_name=customer_name)
-        with open(rental_items,'r') as rental_file:
+        '''
+        Add rental items from existing csv file to new or existing csv file.
+        Uses partial function to fix first two inputs of existing add_furniture
+        function
+
+        Args:
+            rental_items (str):
+                Path to csv file with furnture items to add
+
+        Returns:
+            add_rentals (function):
+                Function that takes the existing csv file as input and makes
+                use of currying to add the items to the csv file entered in the
+                closure function with the customer name provided
+        '''
+        # Use partial to fix first two arguments of add_furniture
+        add_furniture_curried = partial(add_furniture,
+                                        invoice_file=invoice_file,
+                                        customer_name=customer_name)
+        # Open existing csv file and write items to new or existing csv file
+        # Makes use of curried version of add_furniture
+        with open(rental_items, 'r') as rental_file:
             csv_reader = csv.reader(rental_file)
             for row in csv_reader:
                 item_code, item_description, item_monthly_price = row
--- students/gregdevore/lesson08/assignment/test_inventory.py
+++ students/gregdevore/lesson08/assignment/test_inventory.py
@@ -10,48 +10,77 @@ from inventory import add_furniture, single_customer
 class InventoryTests(TestCase):
     '''
     Test suite for inventory module
+
+    Methods:
+        setUpClass(cls):
+            Class method to run before any tests, defines csv file names, validation
+            data, and writes test csv file
+
+        tearDownClass(cls):
+            Class method to run after all tests, removes csv files created during
+            testing
+
+        test_add_furniture(self):
+            Method to test add_furniture method
+
+        test_single_customer(self):
+            Method to test single_customer method
     '''
     @classmethod
     def setUpClass(cls):
+        '''
+        Class method to run before any tests, defines csv file names, validation
+        data, and writes test csv file
+        '''
         cls.test_add_furnture_file = 'test_furniture.csv'
         cls.test_single_customer_read_file = 'test_items.csv'
         cls.test_single_customer_write_file = 'rented_items.csv'
-        cls.furniture_lines = [['Elisa Miles','LR04','Leather Sofa',25],
-                                ['Edward Data','KT78','Kitchen Table',10],
-                                ['Alex Gonzales','BR02','Queen Mattress',17]]
-        cls.single_customer_lines = [['Susan Wong','LR04','Leather Sofa',25],
-                                     ['Susan Wong','KT78','Kitchen Table',10],
-                                     ['Susan Wong','BR02','Queen Mattress',17]]
-        with open(cls.test_single_customer_read_file,'w') as test_csv:
+        cls.furniture_lines = [['Elisa Miles', 'LR04', 'Leather Sofa', 25],
+                               ['Edward Data', 'KT78', 'Kitchen Table', 10],
+                               ['Alex Gonzales', 'BR02', 'Queen Mattress', 17]]
+        cls.single_customer_lines = [['Susan Wong', 'LR04', 'Leather Sofa', 25],
+                                     ['Susan Wong', 'KT78', 'Kitchen Table', 10],
+                                     ['Susan Wong', 'BR02', 'Queen Mattress', 17]]
+        with open(cls.test_single_customer_read_file, 'w') as test_csv:
             test_csv.write('LR04,Leather Sofa,25.00\n')
             test_csv.write('KT78,Kitchen Table,10.00\n')
             test_csv.write('BR02,Queen Mattress,17.00')
 
     @classmethod
     def tearDownClass(cls):
+        '''
+        Class method to run after all tests, removes csv files created during
+        testing
+        '''
         os.remove(cls.test_add_furnture_file)
         os.remove(cls.test_single_customer_read_file)
         os.remove(cls.test_single_customer_write_file)
 
     def test_add_furniture(self):
+        '''
+        Method to test add_furniture method
+        '''
         for item in self.furniture_lines:
             add_furniture(self.test_add_furnture_file, *item)
         # Read csv file to confirm correct write
         with open(self.test_add_furnture_file, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
-            for i,row in enumerate(csv_reader):
+            for i, row in enumerate(csv_reader):
                 # Convert final column (price) to int
                 row[-1] = int(float(row[-1]))
                 # Ensure row is equal to input data
                 self.assertEqual(self.furniture_lines[i], row)
 
     def test_single_customer(self):
+        '''
+        Method to test single_customer method
+        '''
         create_invoice = single_customer("Susan Wong", self.test_single_customer_write_file)
         create_invoice(self.test_single_customer_read_file)
         # Read csv file to confirm correct write
         with open(self.test_single_customer_write_file, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
-            for i,row in enumerate(csv_reader):
+            for i, row in enumerate(csv_reader):
                 # Convert final column (price) to int
                 row[-1] = int(float(row[-1]))
                 # Ensure row is equal to input data
