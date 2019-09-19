6355784a075238527bc9a1c4f4046571e56da9de
Zach Thomson <thomsonzachary@gmail.com>
2019-09-18 20:15:21 -0700

    started evaluating the code using Pylint and fixing issues

---
+++ students/zach_thomson/lesson01/inventory_managment/inventory_class.py
@@ -0,0 +1,17 @@
+# Inventory class
+class Inventory:
+
+    def __init__(self, product_code, description, market_price, rental_price):
+        self.product_code = product_code
+        self.description = description
+        self.market_price = market_price
+        self.rental_price = rental_price
+
+    def return_as_dictionary(self):
+        output_dict = {}
+        output_dict['productCode'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['marketPrice'] = self.market_price
+        output_dict['rentalPrice'] = self.rental_price
+
+        return output_dict
