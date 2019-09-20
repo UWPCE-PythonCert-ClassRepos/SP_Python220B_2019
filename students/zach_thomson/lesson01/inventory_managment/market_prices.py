6816f0a8f0e33a0bd3bdf01d5be6f9bceffc5560
Zach Thomson <thomsonzachary@gmail.com>
2019-09-19 21:08:38 -0700

    removed misspelled folder

--- students/zach_thomson/lesson01/inventory_managment/market_prices.py
+++
@@ -1,6 +0,0 @@
-'''module to determine the market price'''
-
-def get_latest_price(item_code):
-    '''function that gets the latest price'''
-    return 24
-    # Raise an exception to force the user to Mock its output
