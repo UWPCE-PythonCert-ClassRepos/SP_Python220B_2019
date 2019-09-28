1238689d4960bfb42511f3e666f2d2d5c7911cee
Zach Thomson <thomsonzachary@gmail.com>
2019-09-28 15:11:29 -0700

    starting to figure out debug/logging messages to deal with negative rental days

--- students/zach_thomson/lesson02/assignment/code/charges_calc.py
+++ students/zach_thomson/lesson02/assignment/code/charges_calc.py
@@ -5,12 +5,26 @@ import argparse
 import json
 import datetime
 import math
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+#Create formatting for logging
+#log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+#formatter = logging.Formatter(log_format)
+
+#Create logging file
+#file_handler = logging.FileHandler(log_file)
+#file_handler.setFormatter(formatter)
+#logger = logging.getLogger()
+#logger.addHandler(file_handler)
+
+#charges_calc functions
 def parse_cmd_arguments():
     '''Setup for argparse'''
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--input', help='input JSON file', required=True)
     parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
+    parser.add_argument('-d', '--debug', help='debug level', required=True)
 
     return parser.parse_args()
 
@@ -31,7 +45,13 @@ def calculate_additional_fields(data):
             rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
             rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
             value['total_days'] = (rental_end - rental_start).days
+            logging.debug(value['total_days'])
+            if value['total_days'] < 0:
+                logging.warning('Negative day rental - skipping calcs for this entry')
+                break
             value['total_price'] = value['total_days'] * value['price_per_day']
+            logging.debug(value['total_price'])
+            #math domain error due to negative number from rental date issues
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             value['unit_cost'] = value['total_price'] / value['units_rented']
         except:
