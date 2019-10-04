dae62600f68de763a0fff0991d1b346f5045cdb1
Andrew Garcia <ajaggarcia@gmail.com>
2019-10-03 19:30:49 -0700

    Added Assignment01 Files

---
+++ students/andrew_garcia/lesson01-assignment/inventory_management/market_prices.py
@@ -0,0 +1,7 @@
+""" Module for getting item prices """
+
+
+def get_latest_price(item_code):
+    """ Gets the latest price for item """
+    return 24
+    # Raise an exception to force the user to Mock its output
