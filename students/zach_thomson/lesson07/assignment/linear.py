083a890e04ff6c6d79cb67e3b97607bf49df3c3f
Zach Thomson <thomsonzachary@gmail.com>
2019-11-10 12:14:00 -0800

    updated linear to return new tuples per assignment

--- students/zach_thomson/lesson07/assignment/linear.py
+++ students/zach_thomson/lesson07/assignment/linear.py
@@ -4,6 +4,7 @@ Linear vs parallelization lesson (linear module)
 import csv
 import os
 import logging
+import time
 from pymongo import MongoClient
 
 #logging setup
@@ -57,11 +58,6 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
     '''
     product_added = 0
     customer_added = 0
-    rentals_added = 0
-
-    product_errors = 0
-    customer_errors = 0
-    rental_errors = 0
 
     mongo = MongoDBConnection()
 
@@ -69,27 +65,32 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
         db = mongo.connection.media
 
         #Create product collection
+        p_time_start = time.time()
         product_db = db['product']
-        #only use drop for resetting database
-        #product_db.drop()
+        initial_product_count = product_db.count_documents({})
         try:
             with open(os.path.join(directory_name, product_file)) as csvfile:
                 product_reader = csv.DictReader(csvfile)
                 for row in product_reader:
-                    product_added += 1
                     new_product = {'product_id':row['product_id'],
                                    'description':row['description'],
                                    'product_type':row['product_type'],
                                    'quantity_available':row['quantity_available']}
-                    LOGGER.info('%s added to database', new_product['product_id'])
                     product_db.insert_one(new_product)
+                    product_added += 1
+            final_product_count = product_db.count_documents({})
+            p_time_end = time.time()
+            p_elapsed = (p_time_end - p_time_start)
+            LOGGER.info('Product info addition took %s', p_elapsed)
+            product_tuple = (product_added, initial_product_count,
+                             final_product_count, p_elapsed)
         except FileNotFoundError:
-            product_errors += 1
+            print('Product file not found')
 
         #Create customer collection
+        c_time_start = time.time()
         customer_db = db['customer']
-        #only use drop for resetting database
-        #customer_db.drop()
+        initial_cust_count = customer_db.count_documents({})
         try:
             with open(os.path.join(directory_name, customer_file)) as csvfile:
                 customer_reader = csv.DictReader(csvfile)
@@ -100,26 +101,35 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
                                     'address':row['address'],
                                     'phone_number':row['phone_number'],
                                     'email':row['email']}
-                    LOGGER.info('%s added to database', new_customer['user_id'])
                     customer_db.insert_one(new_customer)
+            final_cust_count = customer_db.count_documents({})
+            c_time_end = time.time()
+            c_elapsed = (c_time_end - c_time_start)
+            LOGGER.info('Customer info addition took %s', c_elapsed)
+            customer_tuple = (customer_added, initial_cust_count,
+                              final_cust_count, c_elapsed)
         except FileNotFoundError:
-            customer_errors += 1
+            print('Customer file not found')
 
         #Create rentals collection
         rentals_db = db['rentals']
-        #only use drop for resetting database
-        #rentals_db.drop()
+
         try:
             with open(os.path.join(directory_name, rentals_file)) as csvfile:
                 rental_reader = csv.DictReader(csvfile)
                 for row in rental_reader:
-                    rentals_added += 1
                     new_rental = {'user_id':row['user_id'],
                                   'product_id':row['product_id']}
-                    LOGGER.info('%s rental added to database', new_rental['product_id'])
                     rentals_db.insert_one(new_rental)
         except FileNotFoundError:
-            rental_errors += 1
+            print('Rental file not found')
 
-    return [(product_added, customer_added, rentals_added),
-            (product_errors, customer_errors, rental_errors)]
+    return [customer_tuple, product_tuple]
+
+if __name__ == '__main__':
+    START_TIME = time.time()
+    RESULTS = import_data('csv_files', 'product_file.csv',
+                          'customer_file.csv', 'rental_file.csv')
+    END_TIME = time.time()
+    print('total time is', END_TIME-START_TIME)
+    print(RESULTS)
