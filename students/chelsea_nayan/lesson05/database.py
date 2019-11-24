91d1675b7f0c71e26edd276ead09698959088d1b
Chelsea Nayan <chelseanayan@gmail.com>
2019-11-23 20:07:22 -0800

    Adding in progress work on Lesson05

---
+++ students/chelsea_nayan/lesson05/database.py
@@ -0,0 +1,158 @@
+'''Adds products in a csv file to a MongoDB database'''
+# lesson 05: Consuming APIs
+
+# Checklist:
+# Create a product db with attributes that reflect the contents of the csv file
+# Import all data in the csv files into your MongoDB implementation
+# Write queries to retrieve the product database
+# Write a query to integrate customer and product data
+# no errors from pylint
+# quantity_available of '0' is understood as 'not available'
+# Create a test_database.py
+
+# pylint: disable=too-many-statements
+# pylint: disable=invalid-name
+# pylint: disable=too-many-locals
+
+import logging
+import csv
+import os
+from pymongo import MongoClient
+# Set up logger
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+class MongoDBConnection():
+    '''MongoDBConnection'''
+
+    def __int__(self, host='127.0.0.1', port=27101):
+        '''Be sure to use the ip address, not the name for local windows'''
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    '''Populate new MongoDB database using the three csv file inputs'''
+    product_error, customer_error, rentals_error = 0, 0, 0
+    product_file_path = os.path.join(directory_name, product_file)
+    customer_file_path = os.path.join(directory_name, customer_file)
+    rentals_file_path = os.path.join(directory_name, rentals_file)
+
+    mongo = MongoDBConnection()
+    with mongo:
+        db = mongo.connection.hp_norton
+        products = db['products']
+        customers = db['customers']
+        rentals = db['rentals']
+
+    # Attempt to import product data file into MongoDB db
+    try:
+        with open(product_file_path, encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                add_product = {'_id': row['id'],
+                               'description': row['description'],
+                               'product_type': row['product_type'],
+                               'quantity_available': row['quantity_available']}
+                try:
+                    products.insert_single(add_product)
+                    LOGGER.info('Product added!')
+                except NameError:
+                    LOGGER.info('Error adding product to database')
+                    product_error += 1
+    except FileNotFoundError:
+        LOGGER.info('Product file not found.')
+        product_error += 1
+
+    # Attempt to import customer data into the MongoDB db
+    try:
+        with open(customer_file_path, encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                add_customer = {'_id': row['id'],
+                                'name': row['description'],
+                                'address': row['product_type'],
+                                'phone_number': row['quantity_available'],
+                                'email': row['email']}
+                try:
+                    customers.insert_single(add_customer)
+                    LOGGER.info('Customer added!')
+                except NameError:
+                    LOGGER.info('Error adding customer to database')
+                    customer_error += 1
+    except FileNotFoundError:
+        LOGGER.info('Customer file not found.')
+        customer_error += 1
+
+    # Attempt to import rentals file into the MongoDB db
+    try:
+        with open(rentals_file_path, encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                add_rentals = {'_id': row['id'],
+                               'product_id': row['product_id'],
+                               'user_id': row['user_id']}
+                try:
+                    rentals.insert_single(add_rentals)
+                    LOGGER.info('Rentals added!')
+                except NameError:
+                    LOGGER.info('Error adding rentals to database')
+                    rentals_error += 1
+    except FileNotFoundError:
+        LOGGER.info('Rentals file not found.')
+        rentals_error += 1
+
+    # Return the sum of each record type and the sum of each error in 2 tuples
+    record_count = (products.count_documents({}), customers.count_documents({}),
+                    rentals.count_documents({}))
+    fail_count = (product_error, customer_error, rentals_error)
+
+    return record_count, fail_count
+#
+def show_available_products():
+    '''Return a dictionary of products listed as available'''
+    mongo = MongoDBConnection()
+    available_products = {}
+    with mongo:
+        db = mongo.connection.hp_norton
+        for each in db.products.find({'quantity_available': {'$gt': '0'}}):
+            product_info = {'description': each['description'],
+                            'product_type': each['product_type'],
+                            'quantity_available': each['quantity_available']}
+            available_products[each['_id']] = product_info
+
+    return available_products
+
+def show_rentals(product_id):
+    '''Return a dictionary with info from users that have rented with the product id'''
+    mongo = MongoDBConnection()
+    rental_list = {}
+    with mongo:
+        db = mongo.connection.hp_norton
+        for each in db.rentals.find({'product_id': product_id}):
+            for pers in db.customers.find({'_id': each['user_id']}):
+                entry = {'name': pers['name'],
+                         'address': pers['address'],
+                         'phone_number': pers['phone_number'],
+                         'email': pers['email']}
+                rental_list[pers['_id']] = entry
+
+    return rental_list
+
+def clear_all():
+    '''Clears all database collections'''
+    mongo = MongoDBConnection()
+    with mongo:
+        db = mongo.connection.hp_norton
+        LOGGER.info('Dropping the databaaaaaase...')
+        db.products.drop()
+        db.customers.drop()
+        db.rentals.drop()
+        LOGGER.info('Cleared all databases!')
---
+++ students/chelsea_nayan/lesson05/test_database.py
@@ -0,0 +1,70 @@
+'''Testing database.py'''
+
+import unittest
+import logging
+from pymongo import errors as pyerror
+import database
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+PATH = ('C:\\Users\\chels\\SP_Python220B_2019\\students\\chelsea_nayan\\lesson05\\')
+
+class TestDatabase(unittest.TestCase):
+    '''test database.py'''
+
+    def test_import_data(self):
+        '''Test import_data function'''
+        database.clear_all()
+
+        # Test that the file not found error works for all three csv files
+        test_one = database.import_data(PATH, 'DNE_products.csv', 'DNE_customers.csv',
+                                        'DNE_rentals.csv')
+        test_two = database.import_data(PATH, 'products.csv', 'customers.csv', 'rentals.csv')
+        self.assertEqual(test_one, (0, 0, 0)) # File Not Found
+        self.assertEqual(test_two, (1, 1, 1)) # File exists
+
+    def test_show_available_products(self):
+        '''Test show_available_products function'''
+        database.clear_all()
+        database.import_data(PATH, 'products.csv', 'customers.csv', 'rentals.csv')
+
+        expected = {'p001': {'description': 'water bending scroll', 'product_type': 'artificat',
+                             'quantity_available': '10'},
+                    'p002': {'description': 'space sword', 'product_type': 'weaponry',
+                             'quantity_available': '1'},
+                    'p003': {'description': 'cactus juice', 'product_type': 'food',
+                             'quantity_available': '3'},
+                    'p004': {'description': 'meat jerky', 'product_type': 'food',
+                             'quantity_available': '15'},
+                    'p005': {'description': 'dual swords', 'product_type': 'weaponry',
+                             'quantity_available': '2'},
+                    'p006': {'description': 'blue mask', 'product_type': 'artifact',
+                             'quantity_available': '0'}}
+
+        dict_test = database.show_available_products()
+
+        self.assertEqual(expected, dict_test)
+        database.clear_all()
+
+    def test_show_rentals(self):
+        '''Test show_available_products function'''
+        database.clear_all()
+        database.import_data(PATH, 'products.csv', 'customers.csv', 'rentals.csv')
+
+        expected_1 = {'c004': {'name': 'Sokka', 'address': 'Southern Water Tribe',
+                               'phone_number': '1-206-444-4444', 'email': 'boomerang@gmail.com'},
+                      'c005': {'name': 'Toph', 'address': 'Southern Earth Kingdom',
+                               'phone_number': '1-206-555-5555', 'email': 'earth@gmail.com'}}
+
+        expected_2 = {'c006': {'name': 'Zuko', 'address': 'Fire Nation',
+                               'phone_number': '1-206-666-6666', 'email': 'pride@gmail.com'},
+                      'c001': {'name': 'Aang', 'address': 'Eastern Air Temple',
+                               'phone_number': '1-206-111-1111', 'email': 'avatar@gmail.com'}}
+
+        dict_test_1 = database.show_rentals('p004')
+        dict_test_2 = database.show_rentals('p006')
+
+        self.assertEqual(expected_1, dict_test_1)
+        self.assertEqual(expected_2, dict_test_2)
+
+        database.clear_all()
