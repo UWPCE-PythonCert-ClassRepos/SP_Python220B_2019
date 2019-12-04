41cf6405ecc964b768ddea9166a1e5001371b7a2
Zach Meves <zmeves@gmail.com>
2019-12-03 16:36:53 -0800

    Pylint grade 9.29 - will fix up to 10 while refactoring code to pass unit tests

--- students/zach_meves/lesson01/inventory_management/market_prices.py
+++ students/zach_meves/lesson01/inventory_management/market_prices.py
@@ -1,3 +1,14 @@
+"""
+Module for getting market values.
+"""
+
+
 def get_latest_price(item_code):
+    """
+    Get latest price for an item.
+
+    :param item_code: str, code of item to get price for
+    :return: float, price of item
+    """
     return 24
     # Raise an exception to force the user to Mock its output
