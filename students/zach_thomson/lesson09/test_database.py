9c24223311981389a95c34f38859e8ba700debb2
Zach Thomson <thomsonzachary@gmail.com>
2019-12-14 11:49:04 -0800

    updated tests to clear database

--- students/zach_thomson/lesson09/test_database.py
+++ students/zach_thomson/lesson09/test_database.py
@@ -2,7 +2,19 @@
 tests for the MongoDB database assignment
 '''
 from unittest import TestCase
-from database import import_data, show_available_products, show_rentals
+from database import import_data, show_available_products, show_rentals, MongoDBConnection
+
+def clear_db():
+    mongo = MongoDBConnection()
+
+    with mongo:
+        db = mongo.connection.media
+        product_db = db['product']
+        customer_db = db['customer']
+        rentals_db = db['rentals']
+        product_db.drop()
+        customer_db.drop()
+        rentals_db.drop()
 
 class MongoDBTest(TestCase):
     '''tests for basic operations'''
@@ -10,6 +22,7 @@ class MongoDBTest(TestCase):
     def test_import_data(self):
         '''tests csv files are imported correctly with
         correct tuples being returned'''
+        clear_db()
         test_import = import_data('csv_files', 'product_file.csv',
                                   'customer_file.csv', 'rental_file.csv')
         self.assertEqual(test_import, [(4, 2, 2), (0, 0, 0)])
@@ -17,6 +30,7 @@ class MongoDBTest(TestCase):
 
     def test_import_data_failure(self):
         '''tests exception handing on import data function'''
+        clear_db()
         test_import = import_data('csv_files', 'nofile1.csv',
                                   'nofile2.csv', 'nofile3.csv')
         self.assertEqual(test_import, [(0, 0, 0), (1, 1, 1)])
@@ -24,6 +38,7 @@ class MongoDBTest(TestCase):
 
     def test_show_available_products(self):
         '''tests show_available_products function'''
+        clear_db()
         import_data('csv_files', 'product_file.csv', 'customer_file.csv', 'rental_file.csv')
         actual_output = show_available_products()
         expected_dict = {'prd001':{'description':'60-inch TV stand',
@@ -37,6 +52,7 @@ class MongoDBTest(TestCase):
 
     def test_show_rentals(self):
         '''tests show_rentals function'''
+        clear_db()
         import_data('csv_files', 'product_file.csv', 'customer_file.csv', 'rental_file.csv')
         actual_output = show_rentals('prd002')
         expected_dict = {'user001':{'name':'Elisa Miles', 'address':'4490 Union Street',
