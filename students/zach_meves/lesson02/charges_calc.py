19993c526f7e0d3f593aa211fd8b05a46200fd7a
Zach Meves <zmeves@gmail.com>
2019-12-15 20:09:02 -0800

    Updated docstring

--- students/zach_meves/lesson02/charges_calc.py
+++ students/zach_meves/lesson02/charges_calc.py
@@ -15,12 +15,14 @@ The -d flag can be used to give a debug level from 0 to 3:
 - 2 : Errors and warnings
 - 3 : Errors, warnings, and debug statements
 
-Errors are issued when invalid data values are encountered
-Warnings are issued when required data is missing
+Errors are issued when invalid data values are encountered.
+Warnings are issued when required data is missing.
+Debug statements are issued if the computation or data retreival is
+successful.
 
 If any condition that causes an error or warning is encountered,
 the computation is abandoned and the program skips to the
-next item. 
+next item.
 """
 
 import argparse
@@ -28,22 +30,23 @@ import json
 import datetime
 import math
 import logging
+import sys
 
 # Set up logging
-log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
-formatter = logging.Formatter(log_format)
+LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+FORMATTER = logging.Formatter(LOG_FORMAT)
 
-log_file = datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
-file_handler = logging.FileHandler(log_file)
-file_handler.setFormatter(formatter)
+LOG_FILE = datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setFormatter(FORMATTER)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
+STREAM_HANDLER = logging.StreamHandler()
+STREAM_HANDLER.setFormatter(FORMATTER)
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(STREAM_HANDLER)
 
 VALID_OPTS = (0, 1, 2, 3)
 INVALID_KEY = "{}: No {} data present"
@@ -77,14 +80,68 @@ def load_rentals_file(filename):
     with open(filename) as file:
         try:
             # Attempt to load the file
-            logging.debug(f"Loading {filename}")
+            msg = f"Loading {filename}"
+            logging.debug(msg)
             data = json.load(file)
-        except Exception as error:
+        except (json.JSONDecodeError, FileNotFoundError):
             # Log an error message if unable to open file
-            logging.ERROR(f"Unable to load input file ({filename}).\n"
-                          f"Error raised: {error}")
-            exit(0)
+            msg = f"Unable to load input file ({filename})"
+            logging.error(msg)
+            sys.exit(0)
     return data
+
+
+def _compute_times(key, value):
+    """
+    Compute start and end times.
+
+    :param key: str, key of data
+    :param value: str, value of data
+    :returns: bool, True if successful
+    :returns: float, total rented days
+    """
+    # Attempt to get start rental date
+    try:
+        rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
+    except KeyError:
+        msg = INVALID_KEY.format(key, 'rental_start')
+        logging.warning(msg)
+        return False
+    except ValueError:
+        msg = INVALID_FORMAT.format(key, value['rental_start'])
+        logging.warning(msg)
+        return False
+    else:
+        msg = f"  - Start date = {rental_start}"
+        logging.debug(msg)
+
+    # Attempt to get end rental date
+    try:
+        rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+    except KeyError:
+        msg = INVALID_KEY.format(key, "rental_end")
+        logging.warning(msg)
+        return False
+    except ValueError:
+        msg = INVALID_FORMAT.format(key, value['rental_end'])
+        logging.warning(msg)
+        return False
+    else:
+        msg = f"  - End date = {rental_end}"
+        logging.debug(msg)
+
+    # Compute time of rental
+    total_days = (rental_end - rental_start).days
+    if total_days < 0:
+        msg = f"{key}: End date is before start date"
+        logging.error(msg)
+        return False
+
+    value['total_days'] = total_days
+    msg = f"  - total_days = {total_days}"
+    logging.debug(msg)
+
+    return True
 
 
 def calculate_additional_fields(data):
@@ -96,76 +153,57 @@ def calculate_additional_fields(data):
     """
     for key, value in data.items():
         # Store which key is being accessed
-        logging.debug(f"Current key: {key}")
-        
-        # Attempt to get start rental date
-        try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-        except KeyError:
-            logging.warning(INVALID_KEY.format(key, 'rental_start'))
-            continue  # to next item
-        except ValueError:
-            logging.warning(INVALID_FORMAT.format(key, value['rental_start']))
-            continue  # to next item
-        else:
-            logging.debug(f"  - Start date = {rental_start}")
-        
-        # Attempt to get end rental date
-        try:
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
-        except KeyError:
-            logging.warning(INVALID_KEY.format(key, "rental_end"))
-            continue  # to next item
-        except ValueError:
-            logging.warning(INVALID_FORMAT.format(key, value['rental_end']))
-            continue  # to next item
-        else:
-            logging.debug(f"  - End date = {rental_end}")
+        msg = f"Current key: {key}"
+        logging.debug(msg)
 
-        # Compute time of rental
-        total_days = (rental_end - rental_start).days
-        if total_days < 0:
-            logging.error(f"{key}: End date is before start date")
+        success = _compute_times(key, value)
+        if not success:
             continue
-        else:
-            value['total_days'] = total_days
-            logging.debug(f"  - total_days = {total_days}")
-        
+
         # Compute total price of rental
         try:
             ppd = value['price_per_day']
         except KeyError:
-            logging.warning(f"{key}: No price_per_day")
+            msg = f"{key}: No price_per_day"
+            logging.warning(msg)
             continue
         else:
-            logging.debug(f"  - price_per_day = {ppd}")
-        
+            msg = f"  - price_per_day = {ppd}"
+            logging.debug(msg)
+
         try:
-            value['total_price'] = total_days * ppd
+            value['total_price'] = value['total_days'] * ppd
         except ValueError:
-            logging.error(f"{key}: Invalid price_per_day")
+            msg = f"{key}: Invalid price_per_day"
+            logging.error(msg)
             continue
         else:
-            logging.debug(f"  - total_price ={value['total_price']}")
-        
+            msg = f"  - total_price ={value['total_price']}"
+            logging.debug(msg)
+
         # Compute square-root of total price
         try:
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
         except ValueError:
-            logging.error(f"{key}: Invalid total_price, unable to take square root")
+            msg = f"{key}: Invalid total_price, unable to take square root"
+            logging.error(msg)
         else:
-            logging.debug(f"  - sqrt_total_price = {value['sqrt_total_price']}")
-        
+            msg = f"  - sqrt_total_price = {value['sqrt_total_price']}"
+            logging.debug(msg)
+
         # Compute unit cost of rental
         try:
             value['unit_cost'] = value['total_price'] / value['units_rented']
         except KeyError:
-            logging.warning(f"{key}: No units_rented")
+            msg = f"{key}: No units_rented"
+            logging.warning(msg)
         except ValueError:
-            logging.error(f"{key}: Invalid units_rented")
+            msg = f"{key}: Invalid units_rented"
+            logging.error(msg)
         else:
-            logging.debug(f"  - units_rented = {value['units_rented']}")
-        
+            msg = f"  - units_rented = {value['units_rented']}"
+            logging.debug(msg)
+
     return data
 
 
@@ -176,36 +214,40 @@ def save_to_json(filename, data):
     :param filename: str, name of file to save to
     :param data: dict, data to save
     """
-    logging.debug(f"Dumping JSON data to {filename}")
+    msg = f"Dumping JSON data to {filename}"
+    logging.debug(msg)
     with open(filename, 'w') as file:
         json.dump(data, file)
 
 
 if __name__ == "__main__":
-    args = parse_cmd_arguments()
+    ARGS = parse_cmd_arguments()
 
     # Set logging level based on -d input
-    if args.d not in VALID_OPTS:
+    if ARGS.d not in VALID_OPTS:
         raise ValueError("Invalid debug flag")
 
-    d = args.d
-    if d == 0:
-        file_handler.setLevel(100)
-        stream_handler.setLevel(100)
+    D = ARGS.d
+    if D == 0:
+        FILE_HANDLER.setLevel(100)
+        STREAM_HANDLER.setLevel(100)
     else:
-        file_handler.setLevel(logging.ERROR)
-        stream_handler.setLevel(logging.ERROR)
-        if d >= 2:
-            file_handler.setLevel(logging.WARNING)
-            stream_handler.setLevel(logging.WARNING)
-        if d == 3:
-            stream_handler.setLevel(logging.DEBUG)
+        FILE_HANDLER.setLevel(logging.ERROR)
+        STREAM_HANDLER.setLevel(logging.ERROR)
+        if D >= 2:
+            FILE_HANDLER.setLevel(logging.WARNING)
+            STREAM_HANDLER.setLevel(logging.WARNING)
+        if D == 3:
+            STREAM_HANDLER.setLevel(logging.DEBUG)
 
     # Log commands
-    logging.debug(f"Input file: {args.input}")
-    logging.debug(f"Output file: {args.output}")
-    logging.debug(f"Debug level: {d}")
+    MSG = f"Input file: {ARGS.input}"
+    logging.debug(MSG)
+    MSG = f"Output file: {ARGS.output}"
+    logging.debug(MSG)
+    MSG = f"Debug level: {D}"
+    logging.debug(MSG)
 
-    data = load_rentals_file(args.input)
-    data = calculate_additional_fields(data)
-    save_to_json(args.output, data)
+    DATA = load_rentals_file(ARGS.input)
+    DATA = calculate_additional_fields(DATA)
+    save_to_json(ARGS.output, DATA)
