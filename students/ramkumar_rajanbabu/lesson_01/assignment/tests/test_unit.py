7bb69436e04b0d435d0c2ec8a25e9989fb671c20
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2019-12-26 11:41:06 -0800

    started on test_unit file

--- students/ramkumar_rajanbabu/lesson_01/assignment/tests/test_unit.py
+++ students/ramkumar_rajanbabu/lesson_01/assignment/tests/test_unit.py
@@ -1,2 +1,37 @@
-#Inventory management unit tests
+"""Module for unit tests for inventory management"""
 
+
+import io
+from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import inventory_management.main as Main
+import inventory_management.market_prices as MarketPrices
+from inventory_management.inventory_class import Inventory
+from inventory_management.electric_appliances_class import ElectricAppliances
+from inventory_management.furniture_class import Furniture
+
+class MarketPriceTests(TestCase):
+    """Unit tests for MarketPrices module"""
+    
+    
+class InventoryTests(TestCase):
+    """Unit tests for Inventory module"""
+    
+    
+class Furniture(TestCase):
+    """Unit tests for Furniture module"""
+    
+    def setUp(self):
+        """Sets up"""
+        self.furniture = Furniture(123, "Bed", 500, 20, "Cotton", "Small")
+
+class ElectricAppliances(TestCase):
+    """Unit tests for ElectricAppliances module"""
+    
+    
+    
+class MainTests(TestCase):
+    """Unit tests for Main module"""
+    
