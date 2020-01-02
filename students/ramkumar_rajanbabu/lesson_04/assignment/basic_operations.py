0d34c5b3b02a04fae4347d8ed0a5f548d390f40d
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-02 11:37:57 -0800

    updated assignment 4 with updates from A3

--- students/ramkumar_rajanbabu/lesson_04/assignment/basic_operations.py
+++ students/ramkumar_rajanbabu/lesson_04/assignment/basic_operations.py
@@ -25,75 +25,96 @@ LOGGER.addHandler(FILE_HANDLER) # Add file handler to logger
 LOGGER.addHandler(CONSOLE_HANDLER) # Add console handler to logger
 
 
+
+def setup_database():
+    """Setup database"""
+    cm.DATABASE.drop_tables([cm.Customer])
+    LOGGER.info("Cleared the database")
+    cm.DATABASE.create_tables([cm.Customer])
+    LOGGER.info("Created a table in database")
+
+
+def teardown_database():
+    """Close database"""
+    cm.DATABASE.close()
+    LOGGER.info("Closed database")
+
+
 def add_customer(customer_id, first_name, last_name, home_address,
                  phone_number, email_address, status, credit_limit):
     """Add a new customer to the database"""
     with cm.DATABASE.transaction():
-        LOGGER.info(f"In process of adding customer {customer_id}")
-        new_customer = cm.Customer.create(customer_id=customer_id,
-                                          first_name=first_name,
-                                          last_name=last_name,
-                                          home_address=home_address,
-                                          phone_number=phone_number,
-                                          email_address=email_address,
-                                          status=status,
-                                          credit_limit=credit_limit)
-        new_customer.save()
-        LOGGER.info(f"Added a new customer {first_name} {last_name}")
+        try:
+            new_customer = cm.Customer.create(customer_id=customer_id,
+                                              first_name=first_name,
+                                              last_name=last_name,
+                                              home_address=home_address,
+                                              phone_number=phone_number,
+                                              email_address=email_address,
+                                              status=status,
+                                              credit_limit=credit_limit)
+            new_customer.save()
+            LOGGER.info(f"Added customer #{customer_id}")
+        except pw.IntegrityError:
+            LOGGER.info(f"Unique constraint failed for customer {customer_id}")
+            raise pw.IntegrityError
 
 
 def search_customer(customer_id):
     """Return a dictionary with customer information"""
     with cm.DATABASE.transaction():
         try:
-            LOGGER.info(f"Searching for customer {customer_id}")
             a_customer = cm.Customer.get(
                 cm.Customer.customer_id == customer_id)
             a_customer = {'first_name': a_customer.first_name,
                           'last_name': a_customer.last_name,
                           'email_address': a_customer.email_address,
                           'phone_number': a_customer.phone_number}
-            LOGGER.info(f"Found customer {customer_id}")
+            LOGGER.info(f"Found customer #{customer_id}")
             return a_customer
         except pw.DoesNotExist:
-            LOGGER.warning("Customer not found")
-            return {}
+            LOGGER.info(f"Customer #{customer_id} not found")
+            a_customer = {}
+            return a_customer
 
 
 def delete_customer(customer_id):
     """Delete a customer from the database"""
     with cm.DATABASE.transaction():
-        if cm.Customer.get(cm.Customer.customer_id == customer_id):
-            LOGGER.info(f"Found customer {customer_id}")
+        try:
+            LOGGER.info(f"Searching for customer #{customer_id}")
             a_customer = cm.Customer.get(
                 cm.Customer.customer_id == customer_id)
             a_customer.delete_instance()
-            LOGGER.info(f"Deleted customer {customer_id}")
-        else:
-            LOGGER.warning("Customer not found")
-            raise pw.DoesNotExist
+            a_customer.save()
+            LOGGER.info("Deleted customer")
+        except pw.DoesNotExist:
+            LOGGER.info(f"Customer #{customer_id} not found")
+            raise ValueError
 
 
 def update_customer_credit(customer_id, credit_limit):
     """Search an existing customer and update their credit limit"""
     with cm.DATABASE.transaction():
         try:
-            LOGGER.info("Updating credit limit")
             a_customer = cm.Customer.get(
                 cm.Customer.customer_id == customer_id)
             a_customer.credit_limit = credit_limit
             a_customer.save()
-            LOGGER.info(f"Updated credit limit to {credit_limit}")
+            LOGGER.info(f"Updating credit limit to ${credit_limit}")
         except pw.DoesNotExist:
-            LOGGER.warning("Error updating credit limit")
+            LOGGER.info(f"Customer {customer_id} not found")
             raise ValueError
 
 
 def list_active_customers():
     """Return an integer with the number of active customers"""
     with cm.DATABASE.transaction():
-        LOGGER.info("Obtaining number of active customers")
         active_customers = cm.Customer.select().where(
             cm.Customer.status == "Active").count()
         LOGGER.info(f"Count of active customers: {active_customers}")
         return active_customers
+
+if __name__ == "__main__":
+    setup_database()
+    teardown_database()
--- students/ramkumar_rajanbabu/lesson_04/assignment/test_unit.py
+++ students/ramkumar_rajanbabu/lesson_04/assignment/test_unit.py
@@ -3,35 +3,39 @@
 import unittest
 from unittest import TestCase
 
-import peewee as pw
 import customer_model as cm
 import basic_operations as bo
 
 
-def setup_database():
-    """Setup for database"""
-    cm.DATABASE.drop_tables([cm.Customer])
-    cm.DATABASE.create_tables([cm.Customer])
-
 class BasicOperationsTest(TestCase):
     """Testing basic operations"""
+
+    @classmethod
+    def setUp(cls):
+        """Setup database"""
+        cm.DATABASE.drop_tables([cm.Customer])
+        cm.DATABASE.create_tables([cm.Customer])
+
+    @classmethod
+    def tearDown(cls):
+        """Close database"""
+        cm.DATABASE.close()
+
     def test_add_customer(self):
         """Test adding a new customer"""
         bo.add_customer('100', 'Peter', 'Parker',
                         '135 W. 50th Street, New York City, NY 10011',
                         '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
+                        True, 1000)
         bo.add_customer('200', 'Iron', 'Man',
                         '17801 International Blvd, Seattle, WA 98101',
                         '206-787-5388', 'iron.man@gmail.com',
                         True, 5000)
-
         bo.add_customer('300', 'Ramkumar', 'Rajanbabu',
                         '7525 166th Ave NE, Redmond, WA 98052',
                         '425-556-2900', 'ram.kumar@gmail.com',
-                        False, 7078.25)
+                        False, 7078)
 
-        # Checking expected results
         a_customer_1 = cm.Customer.get(cm.Customer.customer_id == '100')
         self.assertEqual(a_customer_1.customer_id, '100')
         self.assertEqual(a_customer_1.phone_number, '212-576-4000')
@@ -46,91 +50,82 @@ class BasicOperationsTest(TestCase):
         a_customer_3 = cm.Customer.get(cm.Customer.customer_id == '300')
         self.assertEqual(a_customer_3.customer_id, '300')
         self.assertEqual(a_customer_3.status, False)
-        self.assertEqual(a_customer_3.credit_limit, 7078.25)
+        self.assertEqual(a_customer_3.credit_limit, 7078)
 
     def test_search_customer(self):
         """Test searching for a customer"""
         bo.add_customer('100', 'Peter', 'Parker',
                         '135 W. 50th Street, New York City, NY 10011',
                         '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
+                        True, 1000)
 
-        a_customer = bo.search_customer('100')
-        a_customer_dict = {'first_name': 'Peter',
-                           'last_name': 'Parker',
-                           'email_address': 'peter.parker@marvel.com',
-                           'phone_number': '212-576-4000'}
-        self.assertEqual(a_customer, a_customer_dict)
+        actual = bo.search_customer('100')
+        expected = {'first_name': 'Peter', 'last_name': 'Parker',
+                    'email_address': 'peter.parker@marvel.com',
+                    'phone_number': '212-576-4000'}
+        self.assertEqual(actual, expected)
 
     def test_search_customer_fail(self):
         """Test searching for a customer (fail)"""
-        bo.add_customer('100', 'Peter', 'Parker',
-                        '135 W. 50th Street, New York City, NY 10011',
-                        '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
-
-        a_customer = bo.search_customer('100')
-        a_customer_dict = {}
-        self.assertEqual(a_customer, a_customer_dict)
+        actual = bo.search_customer('100')  # Not in table
+        expected = {}
+        self.assertEqual(actual, expected)
 
     def test_delete_customer(self):
         """Test deleting a customer"""
         bo.add_customer('100', 'Peter', 'Parker',
                         '135 W. 50th Street, New York City, NY 10011',
                         '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
+                        True, 1000)
 
         bo.delete_customer('100')
-        a_customer_dict = {}
-        self.assertEqual(bo.search_customer('100'), a_customer_dict)
+        actual = bo.search_customer('100')
+        expected = {}
+        self.assertEqual(actual, expected)
 
     def test_delete_customer_fail(self):
         """Test deleting a customer (fail)"""
-        bo.add_customer('100', 'Peter', 'Parker',
-                        '135 W. 50th Street, New York City, NY 10011',
-                        '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
-
-        with self.assertRaises(pw.DoesNotExist):  # Specific exception raised
-            bo.delete_customer('100')
+        with self.assertRaises(ValueError):
+            bo.delete_customer('100')  # Not in table
 
     def test_update_customer_credit(self):
         """Test updating customer credit limit"""
         bo.add_customer('100', 'Peter', 'Parker',
                         '135 W. 50th Street, New York City, NY 10011',
                         '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
-        
-        bo.update_customer_credit('100', 2000.51)
+                        True, 1000)
+
+        bo.update_customer_credit('100', 2000)
         a_customer = cm.Customer.get(cm.Customer.customer_id == '100')
-        self.assertEqual(a_customer.credit_limit, 2000.51)
+        actual = a_customer.credit_limit
+        expected = 2000
+        self.assertEqual(actual, expected)
 
     def test_update_customer_credit_fail(self):
         """Test updating customer credit limit (fail)"""
-        bo.add_customer('100', 'Peter', 'Parker',
-                        '135 W. 50th Street, New York City, NY 10011',
-                        '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
-
-        with self.assertRaises(pw.DoesNotExist):  # Specific exception raised
-            bo.delete_customer('100')
+        with self.assertRaises(ValueError):
+            bo.update_customer_credit('100', 2000)  # Not in table
 
     def test_list_active_customers(self):
         """Test listing active customers"""
-        bo.add_customer('100', 'Peter', 'Parker',
-                        '135 W. 50th Street, New York City, NY 10011',
-                        '212-576-4000', 'peter.parker@marvel.com',
-                        True, 1000.10)
-        bo.add_customer('200', 'Iron', 'Man',
-                        '17801 International Blvd, Seattle, WA 98101',
-                        '206-787-5388', 'iron.man@gmail.com',
-                        True, 5000)
-        bo.add_customer('300', 'Ramkumar', 'Rajanbabu',
-                        '7525 166th Ave NE, Redmond, WA 98052',
-                        '425-556-2900', 'ram.kumar@gmail.com',
-                        False, 7078.25)
+        bo.add_customer("100", "Peter", "Parker",
+                        "135 W. 50th Street, New York City, NY 10011",
+                        "212-576-4000", "peter.parker@marvel.com",
+                        True, "1000.10")
 
+        bo.add_customer("200", "Iron", "Man",
+                        "17801 International Blvd, Seattle, WA 98101",
+                        "206-787-5388", "iron.man@gmail.com",
+                        True, "5000")
+
+        bo.add_customer("300", "Ramkumar", "Rajanbabu",
+                        "7525 166th Ave NE, Remond, WA 98052",
+                        "425-556-2900", "ram.kumar@gmail.com",
+                        False, "7078.25")
+
+        # Only 2 active customers (2 True)
         self.assertEqual(bo.list_active_customers(), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
