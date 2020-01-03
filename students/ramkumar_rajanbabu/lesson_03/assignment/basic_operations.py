a034a09c189b8cb4790072a8ea593b859775be5b
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-03 13:34:30 -0800

    Fixed coverage issue,coverage above 90%

--- students/ramkumar_rajanbabu/lesson_03/assignment/basic_operations.py
+++ students/ramkumar_rajanbabu/lesson_03/assignment/basic_operations.py
@@ -27,11 +27,9 @@ def add_customer(customer_id, first_name, last_name, home_address,
                                               credit_limit=credit_limit)
             # .save() will write the data to the database
             new_customer.save()
-            msg = f"Added customer #{customer_id}"
-            LOGGER.info(msg)
+            LOGGER.info("Added customer [%s]", customer_id)
         except pw.IntegrityError:
-            msg = f"Unique constraint failed for customer {customer_id}"
-            LOGGER.info(msg)
+            LOGGER.error("Customer [%s] not added to database!", customer_id)
             raise pw.IntegrityError
 
 
@@ -47,12 +45,10 @@ def search_customer(customer_id):
                           'last_name': a_customer.last_name,
                           'email_address': a_customer.email_address,
                           'phone_number': a_customer.phone_number}
-            msg = f"Found customer #{customer_id}"
-            LOGGER.info(msg)
+            LOGGER.info("Found customer [%s]", customer_id)
             return a_customer
         except pw.DoesNotExist:
-            msg = f"Customer #{customer_id} not found"
-            LOGGER.info(msg)
+            LOGGER.warning("Customer [%s] not in database!", customer_id)
             a_customer = {}
             return a_customer  # Empty dictionary if no customer was found
 
@@ -61,8 +57,7 @@ def delete_customer(customer_id):
     """Delete a customer from the database"""
     with cm.DATABASE.transaction():
         try:
-            msg = f"Searching for customer #{customer_id}"
-            LOGGER.info(msg)
+            LOGGER.info("Searching for customer [%s]", customer_id)
             a_customer = cm.Customer.get(
                 cm.Customer.customer_id == customer_id)
             # .delete_instance() will delete the record
@@ -70,8 +65,7 @@ def delete_customer(customer_id):
             a_customer.save()
             LOGGER.info("Deleted customer")
         except pw.DoesNotExist:
-            msg = f"Customer #{customer_id} not found"
-            LOGGER.info(msg)
+            LOGGER.warning("Customer [%s] not in database!", customer_id)
             raise ValueError
 
 
@@ -83,11 +77,11 @@ def update_customer_credit(customer_id, credit_limit):
                 cm.Customer.customer_id == customer_id)
             a_customer.credit_limit = credit_limit
             a_customer.save()
-            msg = f"Updating credit limit to ${credit_limit}"
-            LOGGER.info(msg)
+            LOGGER.info("Updating customer [%s] credit limit to $%s",
+                        customer_id, credit_limit)
         except pw.DoesNotExist:
-            msg = f"Customer {customer_id} not found"
-            LOGGER.info(msg)
+            LOGGER.warning("Error updating credit limit for customer [%s]!",
+                           customer_id)
             raise ValueError
 
 
@@ -97,10 +91,10 @@ def list_active_customers():
         # .select() has a .where() method to specify criteria for searching
         active_customers = cm.Customer.select().where(
             cm.Customer.status == "Active").count()
-        msg = f"Count of active customers: {active_customers}"
-        LOGGER.info(msg)
+        LOGGER.info("Active customers: %s", active_customers)
         return active_customers
 
 
 if __name__ == "__main__":
+    cm.DATABASE.drop_tables([cm.Customer])
     cm.DATABASE.create_tables([cm.Customer])
