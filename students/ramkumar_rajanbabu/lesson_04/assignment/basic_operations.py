fe393303aee4ca4cadf4a0b82f3546587e2ff6d0
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-02 22:12:43 -0800

    updated a4 with right log formats

--- students/ramkumar_rajanbabu/lesson_04/assignment/basic_operations.py
+++ students/ramkumar_rajanbabu/lesson_04/assignment/basic_operations.py
@@ -1,7 +1,6 @@
 """Module for basic operations"""
 
 # pylint: disable=too-many-arguments
-# pylint: disable=logging-format-interpolation
 
 import logging
 import peewee as pw
@@ -25,94 +24,77 @@ LOGGER.addHandler(FILE_HANDLER) # Add file handler to logger
 LOGGER.addHandler(CONSOLE_HANDLER) # Add console handler to logger
 
 
-
-def setup_database():
-    """Setup database"""
-    cm.DATABASE.drop_tables([cm.Customer])
-    LOGGER.info("Cleared the database")
-    cm.DATABASE.create_tables([cm.Customer])
-    LOGGER.info("Created a table in database")
-
-
-def teardown_database():
-    """Close database"""
-    cm.DATABASE.close()
-    LOGGER.info("Closed database")
-
-
 def add_customer(customer_id, first_name, last_name, home_address,
                  phone_number, email_address, status, credit_limit):
     """Add a new customer to the database"""
-    with cm.DATABASE.transaction():
-        try:
-            cus = cm.Customer.create(customer_id=customer_id,
-                                     first_name=first_name, 
-                                     last_name=last_name, 
-                                     home_address=home_address, 
-                                     phone_number=phone_number, 
-                                     email_address=email_address, 
-                                     status=status, 
-                                     credit_limit=credit_limit)
-            cus.save()
-            LOGGER.info(f"Added customer #{customer_id}")
-        except pw.IntegrityError:
-            LOGGER.info(f"Unique constraint failed for customer {customer_id}")
-            raise pw.IntegrityError
+    try:
+        cus = cm.Customer.create(customer_id=customer_id,
+                                 first_name=first_name,
+                                 last_name=last_name,
+                                 home_address=home_address,
+                                 phone_number=phone_number,
+                                 email_address=email_address,
+                                 status=status,
+                                 credit_limit=credit_limit)
+        cus.save()
+        LOGGER.info("Added customer [%s]", customer_id)
+    except pw.IntegrityError:
+        LOGGER.error("Customer [%s] not added to database!", customer_id)
+        raise pw.IntegrityError
 
 
 def search_customer(customer_id):
     """Return a dictionary with customer information"""
-    with cm.DATABASE.transaction():
-        try:
-            cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
-            cus = {'first_name': cus.first_name,
-                   'last_name': cus.last_name,
-                   'email_address': cus.email_address,
-                   'phone_number': cus.phone_number}
-            LOGGER.info(f"Found customer #{customer_id}")
-            return cus
-        except pw.DoesNotExist:
-            LOGGER.info(f"Customer #{customer_id} not found")
-            cus = {}
-            return cus
+    try:
+        cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
+        cus = {'first_name': cus.first_name,
+               'last_name': cus.last_name,
+               'email_address': cus.email_address,
+               'phone_number': cus.phone_number}
+        LOGGER.info("Found customer [%s]", customer_id)
+        return cus
+    except pw.DoesNotExist:
+        LOGGER.warning("Customer [%s] not in database!", customer_id)
+        cus = dict()
+        return cus
 
 
 def delete_customer(customer_id):
     """Delete a customer from the database"""
-    with cm.DATABASE.transaction():
-        try:
-            LOGGER.info(f"Searching for customer #{customer_id}")
-            cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
-            cus.delete_instance()
-            cus.save()
-            LOGGER.info("Deleted customer")
-        except pw.DoesNotExist:
-            LOGGER.info(f"Customer #{customer_id} not found")
-            raise ValueError
+    try:
+        LOGGER.info("Searching for customer [%s]", customer_id)
+        cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
+        cus.delete_instance()
+        cus.save()
+        LOGGER.info("Deleted customer")
+    except pw.DoesNotExist:
+        LOGGER.warning("Customer [%s] not in database!", customer_id)
+        raise ValueError
 
 
 def update_customer_credit(customer_id, credit_limit):
     """Search an existing customer and update their credit limit"""
-    with cm.DATABASE.transaction():
-        try:
-            cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
-            cus.credit_limit = credit_limit
-            cus.save()
-            LOGGER.info(f"Updating credit limit to ${credit_limit}")
-        except pw.DoesNotExist:
-            LOGGER.info(f"Customer {customer_id} not found")
-            raise ValueError
+    try:
+        cus = cm.Customer.get(cm.Customer.customer_id == customer_id)
+        cus.credit_limit = credit_limit
+        cus.save()
+        LOGGER.info("Updating customer [%s] credit limit to $%s",
+                    customer_id, credit_limit)
+    except pw.DoesNotExist:
+        LOGGER.warning("Error updating credit limit for customer [%s]!",
+                       customer_id)
+        raise ValueError
 
 
 def list_active_customers():
     """Return an integer with the number of active customers"""
-    with cm.DATABASE.transaction():
-        active = cm.Customer.select().where(cm.Customer.status).count()
-        LOGGER.info(f"Active customers: {active}")
-        return active
+    active = cm.Customer.select().where(cm.Customer.status).count()
+    LOGGER.info("Active customers: %s", active)
+    return active
 
 if __name__ == "__main__":
-    setup_database()
+    cm.DATABASE.drop_tables([cm.Customer])
+    cm.DATABASE.create_tables([cm.Customer])
     add_customer(100, 'Peter', 'Parker',
                  '135 W. 50th Street, New York City, NY 10011',
                  '212-576-4000', 'peter.parker@marvel.com', True, 1000)
@@ -127,5 +109,4 @@ if __name__ == "__main__":
     list_active_customers()
     delete_customer(200)
     list_active_customers()
-    #delete_customer(200)
-    teardown_database()
+    cm.DATABASE.close()
