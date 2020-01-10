0bb5e3bc7984e147566f400f6424aff5afd605b0
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-09 22:18:21 -0800

    Finished A5

--- students/ramkumar_rajanbabu/lesson_05/assignment/database.py
+++ students/ramkumar_rajanbabu/lesson_05/assignment/database.py
@@ -1,5 +1,8 @@
 """Module for database"""
 
+# pylint: disable=too-many-locals, too-many-statements
+
+
 import csv
 import os
 import logging
@@ -7,6 +10,7 @@ from pymongo import MongoClient
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
+
 
 class MongoDBConnection():
     """Connect to MongoDB (Code from part 5)"""
@@ -17,12 +21,12 @@ class MongoDBConnection():
         self.host = host
         self.port = port
         self.connection = None
-    
+
     def __enter__(self):
         """Enter connection"""
         self.connection = MongoClient(self.host, self.port)
         return self
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Exit connection"""
         self.connection.close()
@@ -32,37 +36,42 @@ def import_data(directory_name, product_file, customer_file, rental_file):
     """
     Create and populate a new MongoDB database with
     three csv files
-    
+
     Args:
         product_file: csv file with product data
         customer_file: csv file with customer data
         rental_file: csv file with rental data
-            
+
     Returns:
-        total_count: 
+        total_count: record counts and fail counts
     """
     LOGGER.info("Creating add counts")
     product_count, customer_count, rental_count = 0, 0, 0
-    
+
     LOGGER.info("Creating error counts")
     product_error, customer_error, rental_error = 0, 0, 0
-    
+
     LOGGER.info("Creating file paths for files")
     product_file_path = os.path.join(directory_name, product_file)
     customer_file_path = os.path.join(directory_name, customer_file)
     rental_file_path = os.path.join(directory_name, rental_file)
 
     mongo = MongoDBConnection()
-    
+
     with mongo:
         LOGGER.info("Creating mongo database")
-        db = mongo.connection.media
-        
+        database = mongo.connection.media
+
+        database.products.drop()
+        database.customers.drop()
+        database.rental.drop()
+        LOGGER.info("Cleared all databases")
+
         LOGGER.info("Creating collections in database")
-        products = db["products"]
-        customers = db["customers"]
-        rentals = db["rental"]
-        
+        products = database["products"]
+        customers = database["customers"]
+        rentals = database["rentals"]
+
         try:
             LOGGER.info("Converting product csv file to dictionary")
             with open(product_file_path, encoding="utf-8-sig") as csv_file:
@@ -70,8 +79,9 @@ def import_data(directory_name, product_file, customer_file, rental_file):
                 for item in reader:
                     csv_item = {"product_id": item["product_id"],
                                 "description": item["description"],
-                                "product_type": item["product_type"], 
-                                "quantity_available": item["quantity_available"]}
+                                "product_type": item["product_type"],
+                                "quantity_available":
+                                item["quantity_available"]}
                     products.insert_one(csv_item)
                     product_count += 1
                     LOGGER.info("Added product")
@@ -86,7 +96,7 @@ def import_data(directory_name, product_file, customer_file, rental_file):
                 for item in reader:
                     csv_item = {"user_id": item["user_id"],
                                 "name": item["name"],
-                                "address": item["address"], 
+                                "address": item["address"],
                                 "phone_number": item["phone_number"],
                                 "email": item["email"]}
                     customers.insert_one(csv_item)
@@ -95,7 +105,7 @@ def import_data(directory_name, product_file, customer_file, rental_file):
         except FileNotFoundError:
             LOGGER.info("File not found!")
             customer_error += 1
-            
+
         try:
             LOGGER.info("Converting rental csv file to dictionary")
             with open(rental_file_path, encoding="utf-8-sig") as csv_file:
@@ -103,29 +113,25 @@ def import_data(directory_name, product_file, customer_file, rental_file):
                 for item in reader:
                     csv_item = {"rental_id": item["rental_id"],
                                 "product_id": item["product_id"],
-                                "user_id": item["user_id"],
-                                "name": item["name"],
-                                "address": item["address"], 
-                                "phone_number": item["phone_number"],
-                                "email": item["email"]}
+                                "user_id": item["user_id"]}
                     rentals.insert_one(csv_item)
                     rental_count += 1
                     LOGGER.info("Added rental")
         except FileNotFoundError:
             LOGGER.info("File not found!")
             rental_error += 1
-        
+
         LOGGER.info("Counting adds and errors")
         record_count = (product_count, customer_count, rental_count)
         fail_count = (product_error, customer_error, rental_error)
         total_count = record_count, fail_count
         return total_count
-    
+
 
 def show_available_products():
-    """Return dictionary of products listed as product_id, description, 
+    """Return dictionary of products listed as product_id, description,
     product_type, quantity_available
-    
+
     Returns:
         product_dict: dictionary of product information
     """
@@ -134,26 +140,25 @@ def show_available_products():
 
     with mongo:
         LOGGER.info("Connecting to mongo database")
-        db = mongo.connection.media
-        
+        database = mongo.connection.media
+
         LOGGER.info("Finding products")
-        available_products = db.products.find()
-        for product in available_products:
+        for product in database.products.find():
             product_info = {"description": product["description"],
                             "product_type": product["product_type"],
-                            "quantity_available": product["quantity_available"]}
+                            "quantity_available":
+                            product["quantity_available"]}
             product_dict[product["product_id"]] = product_info
     return product_dict
-        
 
 
 def show_rentals(product_id):
     """Return dictionary with user information from users that have
     rented products matching product_id
-    
+
     Args:
         product_id: product id number
-    
+
     Returns:
         rental_dict: dictionary of rental information
     """
@@ -162,45 +167,17 @@ def show_rentals(product_id):
 
     with mongo:
         LOGGER.info("Connecting to mongo database")
-        db = mongo.connection.media
-        
-        renters = db.rentals.find({"product_id": product_id})
-        for renter in renters:
-            customer = db.customers.find_ones({"user_id": renter["user_id"]})
-            rental_dict[renter["user_id"]] = {"name": customer["name"],
-                       "rental_id": customer["rental_id"], 
-                       "address": customer["address"], 
-                       "phone_number": customer["phone_number"], 
-                       "email": customer["email"]}
+        database = mongo.connection.media
+
+        LOGGER.info("Finding rentals")
+        rentals = database.rentals.find({"product_id":
+                                         product_id}).sort("user_id")
+        rental_list = [rental["user_id"] for rental in rentals]
+        customers = database.customers.find({"user_id": {'$in': rental_list}})
+        rental_dict = {person["user_id"]: {"name": person["name"],
+                                           "address": person["address"],
+                                           "phone_number":
+                                           person["phone_number"],
+                                           "email": person["email"]}
+                       for person in customers}
     return rental_dict
-
-
-def clear_database():
-	"""Clears all database collections"""
-	mongo = MongoDBConnection()
-
-	with mongo:
-		db = mongo.connection.media
-
-		db.products.drop()
-		db.customers.drop()
-		db.rental.drop()
-		LOGGER.info("Cleared all databases")
-
-def main():
-    clear_database()
-    import_data("C:/Users/kumar/Documents/GitHub/SP_Python220B_2019/" \
-                "students/ramkumar_rajanbabu/lesson_05/assignment",
-                "products.csv", "customers.csv", "rentals.csv")
-
-def print_mdb_collection(collection_name):
-    """ Generic collection printer """
-    for doc in collection_name.find():
-        print(doc)
-"""
-if __name__ == "__main__":
-    main()
-    #print_mdb_collection(products)
-    show_available()
-    show_rental("325")
-"""
--- students/ramkumar_rajanbabu/lesson_05/assignment/test_database.py
+++ students/ramkumar_rajanbabu/lesson_05/assignment/test_database.py
@@ -7,47 +7,51 @@ import database as db
 PATH = "C:/Users/kumar/Documents/GitHub/SP_Python220B_2019/" \
        "students/ramkumar_rajanbabu/lesson_05/assignment"
 
+
 class TestDatabase(TestCase):
-    """"""
+    """Test Database"""
     def test_import_data(self):
         """Testing import data"""
         actual = db.import_data(PATH, "products.csv", "customers.csv",
                                 "rentals.csv")
-        expected = ((4,3,4), (0,0,0))
-        #Count = 4 products rows, 3 customers rows, 4 rentals rows
-        #Error = 0, 0, 0 
+        expected = ((4, 3, 4), (0, 0, 0))
         self.assertEqual(actual, expected)
-        
+
     def test_show_available_products(self):
         """Tetsing showing available products"""
         actual = db
         actual.import_data(PATH, "products.csv", "customers.csv",
                            "rentals.csv")
-        expected = {"51": {"description": "TV", "product_type": "Electric", 
-                         "quantity_available": "4"},
-                    "999": {"description": "Couch", "product_type": "Furniture", 
-                         "quantity_available": "15"},
-                    "325": {"description": "Laptop", "product_type": "Electric", 
-                         "quantity_available": "112"},
-                    "223": {"description": "Table", "product_type": "Furniture", 
-                         "quantity_available": "25"}}
+        expected = {"51": {"description": "TV",
+                           "product_type": "Electric",
+                           "quantity_available": "4"},
+                    "999": {"description": "Couch",
+                            "product_type": "Furniture",
+                            "quantity_available": "15"},
+                    "325": {"description": "Laptop",
+                            "product_type": "Electric",
+                            "quantity_available": "112"},
+                    "223": {"description": "Table",
+                            "product_type": "Furniture",
+                            "quantity_available": "25"}}
         self.assertEqual(actual.show_available_products(), expected)
-        
+
     def test_show_rentals(self):
         """Testing show rentals"""
         actual = db
         actual.import_data(PATH, "products.csv", "customers.csv",
                            "rentals.csv")
-        expected = {"200": {"name": "Iron Man", 
-                            "address": "17801 International Blvd, Seattle, WA 98101",
+        expected = {"200": {"name": "Iron Man",
+                            "address":
+                            "17801 International Blvd, Seattle, WA 98101",
                             "phone_number": "206-787-5388",
                             "email": "iron.man@gmail.com"},
-                    "300": {"name": "Ramkumar Rajanbabu", 
+                    "300": {"name": "Ramkumar Rajanbabu",
                             "address": "7525 166th Ave NE, Redmond, WA 98052",
                             "phone_number": "425-556-2900",
                             "email": "ram.kumar@gmail.com"}}
         self.assertEqual(actual.show_rentals("999"), expected)
-        
-        
+
+
 if __name__ == "__main__":
     unittest.main()
