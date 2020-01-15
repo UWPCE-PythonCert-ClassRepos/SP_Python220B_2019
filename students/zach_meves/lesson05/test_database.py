ff74e0fd830aa3933600b60c9e812a4167a5f892
Zach Meves <zmeves@gmail.com>
2020-01-15 13:18:26 -0800

    Updating test cases

--- students/zach_meves/lesson05/test_database.py
+++ students/zach_meves/lesson05/test_database.py
@@ -11,9 +11,20 @@ import database
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(TEST_DIR, 'data')
 
+# Test CSV files
 CUSTOMERS = os.path.join(DATA_DIR, 'customers.csv')
 PRODUCTS = os.path.join(DATA_DIR, 'products.csv')
 RENTALS = os.path.join(DATA_DIR, 'rentals.csv')
+
+# Test CSV field names
+USER_ID, USER_NAME = "user_id", "name"
+USER_ADDRESS, USER_EMAIL = "address", "email"
+USER_PHONE = "phone_number"
+
+PROD_ID, PROD_DESC = "product_id", "description"
+PROD_TYPE, PROD_QTY = "product_type", "quantity"
+
+RENT_QTY = "quantity_rented"
 
 
 class TestCsvIO(unittest.TestCase):
@@ -72,9 +83,34 @@ class TestDatabase(unittest.TestCase):
         """Set up test case"""
 
         # The read_csv method is tested above
-        self.customer_data = database.read_csv(CUSTOMERS)
-        self.product_data = database.read_csv(PRODUCTS)
-        self.rental_data = database.read_csv(RENTALS)
+        self.customer_data = database.read_csv(CUSTOMERS, keyed=True)
+        self.product_data = database.read_csv(PRODUCTS, keyed=True)
+        self.rental_data = database.read_csv(RENTALS, keyed=False)
 
+        self.customer_keys = list(self.customer_data.keys())
+        self.product_keys = list(self.product_data.keys())
 
+        self.products_remaining = dict(zip(self.product_keys,
+                                           (self.product_data[k][PROD_QTY] for
+                                            k in self.product_keys)))
 
+        for rental in self.rental_data:  # Subtract rented quantities from stock
+            prod, qty = rental[PROD_ID], rental[RENT_QTY]
+            self.products_remaining[prod] = self.products_remaining[prod] - qty
+
+        self.client = pymongo.MongoClient()
+
+    def test_import_data(self):
+        """Test database.import_data"""
+
+        pass
+
+    def test_show_available_products(self):
+        """Test database.show_available_products"""
+
+        pass
+
+    def test_show_rentals(self):
+        """Test database.show_rentals"""
+
+    pass
