7c8938fca1d7c1d20854fc6817a4f614769758c1
Zach Meves <zmeves@gmail.com>
2020-01-15 13:03:54 -0800

    Created CSV I/O test cases

--- students/zach_meves/lesson05/database.py
+++ students/zach_meves/lesson05/database.py
@@ -11,6 +11,39 @@ import pymongo
 import csv
 
 
+def read_csv(file, keyed=False):
+    """
+    Read a CSV file and return the data as a list of dictionaries.
+
+    :param file: str, file to read
+    :param keyed: bool, True to return a dictionary keyed on the first-column
+    values, False to return a list of dicts
+    :return: list or dict
+    """
+
+    with open(file) as f:
+        reader = csv.reader(f)
+        header = [_.strip() for _ in next(reader)]
+
+        output = []
+        for line in reader:
+            line_values = [_.strip() for _ in line]
+            # Check if need to convert to floats or ints
+            for i in range(len(line_values)):
+                try:
+                    line_values[i] = float(line_values[i])
+                except ValueError:
+                    pass
+
+            output.append(dict(zip(header, line_values)))
+
+    if keyed:  # Convert to a single dictionary keyed with the first column
+        key = header[0]
+        return dict(zip((entry[key] for entry in output), output))
+
+    return output
+
+
 def import_data(directory, products, customers, rentals):
     """
     Create and populate a new MongoDB instance with data from
--- students/zach_meves/lesson05/test_database.py
+++ students/zach_meves/lesson05/test_database.py
@@ -4,9 +4,77 @@ Tests for database.py.
 
 import unittest
 import pymongo
+import os
+import csv
+import database
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(TEST_DIR, 'data')
+
+CUSTOMERS = os.path.join(DATA_DIR, 'customers.csv')
+PRODUCTS = os.path.join(DATA_DIR, 'products.csv')
+RENTALS = os.path.join(DATA_DIR, 'rentals.csv')
+
+
+class TestCsvIO(unittest.TestCase):
+    """Test reading/writing of CSV files"""
+
+    def setUp(self) -> None:
+        """Set up test case"""
+
+        self.header = ('A', 'b', 'Cd')
+        self.entries = ((1, 'a', 'd'),
+                        (2, 'b', 'e'),
+                        (3, 'c', 'f'))
+
+        self.data_list = [dict(zip(self.header, entry)) for entry in self.entries]
+        self.data_dict = dict(zip((entry[0] for entry in self.entries), self.data_list))
+
+        self.filename = "test_file.csv"
+
+        # Export CSV
+        with open(self.filename, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(self.header)
+            writer.writerows(self.entries)
+
+    def test_read_list(self):
+        """Test reading CSV with list output"""
+
+        # Read the file and assert that the dictionary output
+        # matches self.data_list
+        data = database.read_csv(self.filename)
+
+        self.assertEqual(self.data_list, data)
+
+    def test_read_dict(self):
+        """Test reading CSV with dict output"""
+
+        # Read the file and assert that the dictionary output
+        # matches self.data_dict
+        data = database.read_csv(self.filename, keyed=True)
+
+        self.assertEqual(self.data_dict, data)
+
+    def tearDown(self) -> None:
+        """Remove test file"""
+
+        try:
+            os.remove(self.filename)
+        except FileNotFoundError:
+            pass
 
 
 class TestDatabase(unittest.TestCase):
     """Test cases for database"""
 
-    pass
+    def setUp(self) -> None:
+        """Set up test case"""
+
+        # The read_csv method is tested above
+        self.customer_data = database.read_csv(CUSTOMERS)
+        self.product_data = database.read_csv(PRODUCTS)
+        self.rental_data = database.read_csv(RENTALS)
+
+
+
