7e8f8473ff8af8f519a245410cc44d456d261852
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-15 17:08:45 -0800

    Finsihed A6

--- students/ramkumar_rajanbabu/lesson_06/assignment/good_perf.py
+++ students/ramkumar_rajanbabu/lesson_06/assignment/good_perf.py
@@ -6,48 +6,39 @@ import csv
 
 def analyze(filename):
     """Return year of occurrencesa and count for ao"""
-    start = datetime.datetime.now()
+    start = datetime.datetime.now()  # Track start time
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        new_ones = []
-        for row in reader:
-            lrow = list(row)
-            if lrow[5] > '00/00/2012':
-                new_ones.append((lrow[5], lrow[0]))
 
-        year_count = {"2013": 0, "2014": 0, "2015": 0, "2016": 0, "2017": 0,
+        year_count = {"2013": 0,
+                      "2014": 0,
+                      "2015": 0,
+                      "2016": 0,
+                      "2017": 0,
                       "2018": 0}
 
-        for new in new_ones:
-            if new[0][6:] == '2013':
+        ao_count = 0
+        for row in reader:
+            if row[5][6:] == '2013':
                 year_count["2013"] += 1
-            if new[0][6:] == '2014':
+            if row[5][6:] == '2014':
                 year_count["2014"] += 1
-            if new[0][6:] == '2015':
+            if row[5][6:] == '2015':
                 year_count["2015"] += 1
-            if new[0][6:] == '2016':
+            if row[5][6:] == '2016':
                 year_count["2016"] += 1
-            if new[0][6:] == '2017':
+            if row[5][6:] == '2017':
                 year_count["2017"] += 1
-            if new[0][6:] == '2018':
-                year_count["2017"] += 1
+            if row[5][6:] == '2018':
+                year_count["2018"] += 1
+            if row[6] == "ao":
+                ao_count += 1
 
         print(year_count)
+        print(f"'ao' was found {ao_count} times")
 
-    with open(filename) as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-
-        found = 0
-
-        for line in reader:
-            lrow = list(line)
-            if "ao" in line[6]:
-                found += 1
-
-        print(f"'ao' was found {found} times")
-        end = datetime.datetime.now()
-
-    return (start, end, year_count, found)
+        end = datetime.datetime.now()  # Track end time
+    return (start, end, year_count, ao_count)
 
 
 if __name__ == "__main__":
