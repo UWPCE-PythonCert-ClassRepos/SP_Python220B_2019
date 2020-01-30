8954ff9af18fbfb455df3e605aadad29176e9d1a
Sean Hodges <sthodges@gmail.com>
2020-01-30 14:43:38 -0800

    Base peewee model

---
+++ students/shodges/lesson03/customer_model.py
@@ -0,0 +1,20 @@
+from peewee import *
+import logging
+
+customer_db = SqliteDatabase('customers.db')
+customer_db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = customer_db
+
+class Customer(BaseModel):
+    """This class defines the Customer DB schema."""
+    customer_id = DecimalField(primary_key = True)
+    first_name = CharField(max_length = 30)
+    last_name = CharField(max_length = 30)
+    home_address = CharField(max_length = 100)
+    phone_number = DecimalField(max_digits = 10)
+    email_address = CharField(max_length = 40)
+    is_active = BooleanField(default = True)
+    credit_limit = DecimalField(max_digits = 7, decimal_places = 2)
