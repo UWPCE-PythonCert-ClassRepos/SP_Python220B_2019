75d3064c0f8e0f41bb57e64a3c6d1d459591e97d
Sean Hodges <sthodges@gmail.com>
2020-01-31 14:25:04 -0800

    Add add_customer()

--- students/shodges/lesson03/basic_operations.py
+++ students/shodges/lesson03/basic_operations.py
@@ -3,4 +3,9 @@ import logging
 
 customer_db.create_tables([Customer])
 
+def add_customer(**kwargs):
+    with customer_db.transaction():
+        new_customer = Customer.create(**kwargs)
+        new_customer.save()
+
 customer_db.close()
