a5cfac6297e0c8d51fdc4a895bbcb9db52effc59
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-01-30 21:41:54 -0800

    Finished scripts a7

--- students/ramkumar_rajanbabu/lesson_07/assignment/linear.py
+++ students/ramkumar_rajanbabu/lesson_07/assignment/linear.py
@@ -34,13 +34,13 @@ def import_products_data(directory_name, product_file):
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         products = database["products"]
-        p_initial = products.count_documents({})
         product_file_path = os.path.join(directory_name, product_file)
 
+        p_initial = products.count()
         with open(product_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
                 p_info = {"product_id": row[0],
                           "description": row[1],
@@ -48,7 +48,7 @@ def import_products_data(directory_name, product_file):
                           "quantity_available": row[3]}
                 products.insert_one(p_info)
                 p_added += 1
-        p_final = products.count_documents({})
+        p_final = products.count()
         mod_time = time.time() - p_start
         p_tup = (p_added, p_initial, p_final, mod_time)
     return p_tup
@@ -61,13 +61,13 @@ def import_customers_data(directory_name, customer_file):
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         customers = database["customers"]
-        c_initial = customers.count_documents({})
         customer_file_path = os.path.join(directory_name, customer_file)
 
+        c_initial = customers.count()
         with open(customer_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
                 c_info = {"customer_id": row[0],
                           "name": row[1],
@@ -76,7 +76,7 @@ def import_customers_data(directory_name, customer_file):
                           "email": row[4]}
                 customers.insert_one(c_info)
                 c_added += 1
-        c_final = customers.count_documents({})
+        c_final = customers.count()
         mod_time = time.time() - c_start
         c_tup = (c_added, c_initial, c_final, mod_time)
     return c_tup
@@ -89,26 +89,27 @@ def import_rentals_data(directory_name, rental_file):
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         rentals = database["rentals"]
-        r_initial = rentals.count_documents({})
         rental_file_path = os.path.join(directory_name, rental_file)
 
+        r_initial = rentals.count()
         with open(rental_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
                 r_info = {"rental_id": row[0],
                           "product_id": row[1],
                           "customer_id": row[2]}
                 rentals.insert_one(r_info)
                 r_added += 1
-        r_final = rentals.count_documents({})
+        r_final = rentals.count()
         mod_time = time.time() - r_start
         r_tup = (r_added, r_initial, r_final, mod_time)
     return r_tup
 
 
 if __name__ == "__main__":
+    print("Linear Results:")
     START_TIME = time.time()
     PRODUCTS = import_products_data("sample_csv_files", "products.csv")
     CUSTOMERS = import_customers_data("sample_csv_files", "customers.csv")
--- students/ramkumar_rajanbabu/lesson_07/assignment/parallel.py
+++ students/ramkumar_rajanbabu/lesson_07/assignment/parallel.py
@@ -27,20 +27,20 @@ class MongoDBConnection():
         self.connection.close()
 
 
-def import_products_data(directory_name, product_file):
+def import_products_data(directory_name, product_file, queue):
     """Import data to products database"""
     p_start = time.time()
     p_added = 0
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         products = database["products"]
-        p_initial = products.count_documents({})
         product_file_path = os.path.join(directory_name, product_file)
 
+        p_initial = products.count()
         with open(product_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
                 p_info = {"product_id": row[0],
                           "description": row[1],
@@ -48,64 +48,65 @@ def import_products_data(directory_name, product_file):
                           "quantity_available": row[3]}
                 products.insert_one(p_info)
                 p_added += 1
-        p_final = products.count_documents({})
+        p_final = products.count()
         mod_time = time.time() - p_start
-    Queue.put((p_added, p_initial, p_final, mod_time))
+    queue.put((p_added, p_initial, p_final, mod_time))
 
 
-def import_customers_data(directory_name, customer_file):
+def import_customers_data(directory_name, customer_file, queue):
     """Import data to customers database"""
     c_start = time.time()
     c_added = 0
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         customers = database["customers"]
-        c_initial = customers.count_documents({})
         customer_file_path = os.path.join(directory_name, customer_file)
 
+        c_initial = customers.count()
         with open(customer_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
-                c_info = {"rental_id": row[0],
-                          "product_id": row[1],
-                          "customer_id": row[2]}
+                c_info = {"customer_id": row[0],
+                          "name": row[1],
+                          "address": row[2],
+                          "phone_number": row[3],
+                          "email": row[4]}
                 customers.insert_one(c_info)
                 c_added += 1
-        c_final = customers.count_documents({})
+        c_final = customers.count()
         mod_time = time.time() - c_start
-    Queue.put((c_added, c_initial, c_final, mod_time))
+    queue.put((c_added, c_initial, c_final, mod_time))
 
 
-def import_rentals_data(directory_name, rental_file):
+def import_rentals_data(directory_name, rental_file, queue):
     """Import data to rentals database"""
     r_start = time.time()
     r_added = 0
 
     mongo = MongoDBConnection()
     with mongo:
-        database = mongo.connection.store
+        database = mongo.connection.media
         rentals = database["rentals"]
-        r_initial = rentals.count_documents({})
         rental_file_path = os.path.join(directory_name, rental_file)
 
+        r_initial = rentals.count()
         with open(rental_file_path, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = csv.reader(csv_file)
             for row in reader:
-                r_info = {"customer_id": row[0],
-                          "name": row[1],
-                          "address": row[2],
-                          "phone_number": row[3],
-                          "email": row[4]}
+                r_info = {"rental_id": row[0],
+                          "product_id": row[1],
+                          "customer_id": row[2]}
                 rentals.insert_one(r_info)
                 r_added += 1
-        r_final = rentals.count_documents({})
+        r_final = rentals.count()
         mod_time = time.time() - r_start
-    Queue.put((r_added, r_initial, r_final, mod_time))
+    queue.put((r_added, r_initial, r_final, mod_time))
 
 
 if __name__ == "__main__":
+    print("Parallel Results:")
     START_TIME = time.time()
     MY_QUEUE = Queue()
     THREAD_1 = Thread(target=import_products_data,
