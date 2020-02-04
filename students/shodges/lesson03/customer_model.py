a172c1c4c6bdbc389adbf83002f54322e8254bb6
Sean Hodges <sthodges@gmail.com>
2020-02-04 15:53:41 -0800

    Remove whitespaces making pylint sad

--- students/shodges/lesson03/customer_model.py
+++ students/shodges/lesson03/customer_model.py
@@ -9,11 +9,11 @@ class BaseModel(Model):
 
 class Customer(BaseModel):
     """This class defines the Customer DB schema."""
-    customer_id = DecimalField(primary_key = True)
-    first_name = CharField(max_length = 30)
-    last_name = CharField(max_length = 30)
-    home_address = CharField(max_length = 100)
-    phone_number = DecimalField(max_digits = 10)
-    email_address = CharField(max_length = 40)
-    is_active = BooleanField(default = True)
-    credit_limit = DecimalField(max_digits = 7, decimal_places = 2)
+    customer_id = DecimalField(primary_key=True)
+    first_name = CharField(max_length=30)
+    last_name = CharField(max_length=30)
+    home_address = CharField(max_length=100)
+    phone_number = DecimalField(max_digits=10)
+    email_address = CharField(max_length=40)
+    is_active = BooleanField(default=True)
+    credit_limit = DecimalField(max_digits=7, decimal_places=2)
