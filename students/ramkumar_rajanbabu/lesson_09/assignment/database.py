6b9eacadb6a2ebee6f979173ec446a3a71e6faea
Ram Rajanbabu <ramr@alleninstitute.org>
2020-02-03 16:56:32 -0800

    cleaning up database file

--- students/ramkumar_rajanbabu/lesson_09/assignment/database.py
+++ students/ramkumar_rajanbabu/lesson_09/assignment/database.py
@@ -1,20 +1,12 @@
 """Module for database"""
 
-# pylint: disable=too-many-locals, too-many-statements
-
-
 import csv
-import os
-import logging
+from os import path
 from pymongo import MongoClient
-
-logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger(__name__)
 
 
 class MongoDBConnection():
     """Connect to MongoDB (Code from part 5)"""
-    LOGGER.info("Connecting to MongoDB")
 
     def __init__(self, host='127.0.0.1', port=27017):
         """Initialize"""
@@ -32,6 +24,14 @@ class MongoDBConnection():
         self.connection.close()
 
 
+def clear_database(mongo):
+    """Clear the database"""
+    with mongo:
+        mongo.products.drop()
+        mongo.customers.drop()
+        mongo.rental.drop()
+
+
 def import_data(directory_name, product_file, customer_file, rental_file):
     """
     Create and populate a new MongoDB database with
@@ -45,86 +45,65 @@ def import_data(directory_name, product_file, customer_file, rental_file):
     Returns:
         total_count: record counts and fail counts
     """
-    LOGGER.info("Creating add counts")
     product_count, customer_count, rental_count = 0, 0, 0
-
-    LOGGER.info("Creating error counts")
     product_error, customer_error, rental_error = 0, 0, 0
 
-    LOGGER.info("Creating file paths for files")
-    product_file_path = os.path.join(directory_name, product_file)
-    customer_file_path = os.path.join(directory_name, customer_file)
-    rental_file_path = os.path.join(directory_name, rental_file)
+    product_file_path = path.join(directory_name, product_file)
+    customer_file_path = path.join(directory_name, customer_file)
+    rental_file_path = path.join(directory_name, rental_file)
 
     mongo = MongoDBConnection()
-
     with mongo:
-        LOGGER.info("Creating mongo database")
         database = mongo.connection.media
+        clear_database()
 
-        database.products.drop()
-        database.customers.drop()
-        database.rental.drop()
-        LOGGER.info("Cleared all databases")
-
-        LOGGER.info("Creating collections in database")
         products = database["products"]
         customers = database["customers"]
         rentals = database["rentals"]
 
         try:
-            LOGGER.info("Converting product csv file to dictionary")
             with open(product_file_path, encoding="utf-8-sig") as csv_file:
-                reader = csv.DictReader(csv_file)
-                for item in reader:
-                    csv_item = {"product_id": item["product_id"],
-                                "description": item["description"],
-                                "product_type": item["product_type"],
-                                "quantity_available":
-                                item["quantity_available"]}
-                    products.insert_one(csv_item)
+                product_reader = csv.reader(csv_file)
+                for row in product_reader:
+                    product_info = {"product_id": row[0],
+                                    "description": row[1],
+                                    "product_type": row[2],
+                                    "quantity_available": row[3]}
+                    products.insert_one(product_info)
                     product_count += 1
-                    LOGGER.info("Added product")
         except FileNotFoundError:
-            LOGGER.info("File not found!")
             product_error += 1
 
         try:
-            LOGGER.info("Converting customer csv file to dictionary")
             with open(customer_file_path, encoding="utf-8-sig") as csv_file:
-                reader = csv.DictReader(csv_file)
-                for item in reader:
-                    csv_item = {"user_id": item["user_id"],
-                                "name": item["name"],
-                                "address": item["address"],
-                                "phone_number": item["phone_number"],
-                                "email": item["email"]}
-                    customers.insert_one(csv_item)
+                customer_reader = csv.reader(csv_file)
+                for row in customer_reader:
+                    customer_info = {"customer_id": row[0],
+                                     "name": row[1],
+                                     "address": row[2],
+                                     "phone_number": row[3],
+                                     "email": row[4]}
+                    customers.insert_one(customer_info)
                     customer_count += 1
-                    LOGGER.info("Added customer")
         except FileNotFoundError:
-            LOGGER.info("File not found!")
             customer_error += 1
 
         try:
-            LOGGER.info("Converting rental csv file to dictionary")
             with open(rental_file_path, encoding="utf-8-sig") as csv_file:
-                reader = csv.DictReader(csv_file)
-                for item in reader:
-                    csv_item = {"rental_id": item["rental_id"],
-                                "product_id": item["product_id"],
-                                "user_id": item["user_id"]}
-                    rentals.insert_one(csv_item)
+                rental_reader = csv.DictReader(csv_file)
+                for row in rental_reader:
+                    rental_info = {"rental_id": row[0],
+                                   "product_id": row[1],
+                                   "customer_id": row[2]}
+                    rentals.insert_one(rental_info)
                     rental_count += 1
-                    LOGGER.info("Added rental")
         except FileNotFoundError:
-            LOGGER.info("File not found!")
             rental_error += 1
 
-        LOGGER.info("Counting adds and errors")
         record_count = (product_count, customer_count, rental_count)
         fail_count = (product_error, customer_error, rental_error)
         total_count = record_count, fail_count
+
         return total_count
 
 
@@ -139,16 +118,15 @@ def show_available_products():
     product_dict = dict()
 
     with mongo:
-        LOGGER.info("Connecting to mongo database")
         database = mongo.connection.media
 
-        LOGGER.info("Finding products")
         for product in database.products.find():
             product_info = {"description": product["description"],
                             "product_type": product["product_type"],
                             "quantity_available":
                             product["quantity_available"]}
             product_dict[product["product_id"]] = product_info
+
     return product_dict
 
 
@@ -166,18 +144,16 @@ def show_rentals(product_id):
     rental_dict = dict()
 
     with mongo:
-        LOGGER.info("Connecting to mongo database")
         database = mongo.connection.media
 
-        LOGGER.info("Finding rentals")
         rentals = database.rentals.find({"product_id":
-                                         product_id}).sort("user_id")
-        rental_list = [rental["user_id"] for rental in rentals]
-        customers = database.customers.find({"user_id": {'$in': rental_list}})
-        rental_dict = {person["user_id"]: {"name": person["name"],
-                                           "address": person["address"],
-                                           "phone_number":
-                                           person["phone_number"],
-                                           "email": person["email"]}
+                                         product_id}).sort("customer_id")
+        rental_list = [rental["customer_id"] for rental in rentals]
+        customers = database.customers.find({"customer_id": {'$in': rental_list}})
+        rental_dict = {person["customer_id"]: {"name": person["name"],
+                                               "address": person["address"],
+                                               "phone_number": person["phone_number"],
+                                               "email": person["email"]}
                        for person in customers}
+
     return rental_dict
