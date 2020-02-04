5845438b0f4db41ce84cfe1825ada6ca2808d80c
Sean Hodges <sthodges@gmail.com>
2020-02-03 22:17:53 -0800

    Fix up some test cases

--- students/shodges/lesson03/test_unit.py
+++ students/shodges/lesson03/test_unit.py
@@ -17,10 +17,6 @@ class BaseDbTest(TestCase):
 
         self.assertIsInstance(basic_operations.customer_db, peewee.SqliteDatabase)
 
-        # Set a var to track the beginning record count so we can test this later
-        global begin_record_count
-        begin_record_count = basic_operations.list_active_customers()
-
     def test_add_record(self):
         """Test that a record is successfully added when fields are correctly specified."""
         cust_1 = {'customer_id': 1, 'first_name':'John', 'last_name':'Doe',
@@ -33,24 +29,16 @@ class BaseDbTest(TestCase):
                   'email_address':'bruce@notbatman.com', 'is_active':True,
                   'credit_limit':9999999.00}
 
-        cust_3 = {'customer_id': 3, 'first_name':'Failure2', 'last_name':'Add',
-                  'home_address': '2 Fail St', 'phone_number':9999999999,
-                  'email_address':'fail@testcases.com', 'is_active':True,
-                  'credit_limit':99999999.00}
+        cust_3 = {'customer_id': 3}
 
-        cust_4 = {'customer_id': 4}
+        begin_record_count = basic_operations.list_active_customers()
 
         self.assertEqual(basic_operations.add_customer(**cust_1), True)
 
         self.assertEqual(basic_operations.add_customer(**cust_2), True)
 
-        with self.assertRaises(ValueError): # cust_3 has too many digits in the credit_limit
-            basic_operations.add_customer(**cust_3)
+        self.assertEqual(basic_operations.add_customer(**cust_3), False)
 
-        with self.assertRaises(ValueError): # cust_4 is missing required fields
-            basic_operations.add_customer(**cust_4)
-
-        global begin_record_count
         self.assertEqual(basic_operations.list_active_customers(), (begin_record_count + 2))
 
         self.assertEqual(basic_operations.search_customer(2), cust_2)
@@ -63,8 +51,8 @@ class BaseDbTest(TestCase):
                     'email_address':'bruce@notbatman.com', 'is_active':True,
                     'credit_limit':1000.00}
 
-        with self.assertRaises(ValueError): # customer_id 3 doesn't exist
-            basic_operations.update_customer_credit(3, 1000.00)
+        # This will fail
+        self.assertEqual(basic_operations.update_customer_credit(3, 1000.00), False)
 
         self.assertEqual(basic_operations.update_customer_credit(2, 1000.00), True)
 
@@ -73,10 +61,9 @@ class BaseDbTest(TestCase):
     def test_delete_records(self):
         """Test that a record is deleted if it exists."""
 
-        with self.assertRaises(ValueError): # customer_id 3 wasn't added successfully
-            basic_operations.delete_customer(3)
-
         self.assertEqual(basic_operations.delete_customer(1), True)
         self.assertEqual(basic_operations.delete_customer(2), True)
+
+        self.assertEqual(basic_operations.delete_customer(3), False)
 
         self.assertEqual(basic_operations.list_active_customers(), 0)
