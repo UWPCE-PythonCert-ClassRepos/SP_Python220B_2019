9f1d8adb9cbb8066633fb67df81fa3fb8a217969
Sean Hodges <sthodges@gmail.com>
2020-02-05 10:01:33 -0800

    Refactor tests to expect search_customer() to return dict

--- students/shodges/lesson03/test_unit.py
+++ students/shodges/lesson03/test_unit.py
@@ -51,10 +51,11 @@ class BaseDbTest(TestCase):
 
         self.assertEqual(basic_operations.list_active_customers(), (begin_record_count + 2))
 
-        self.assertEqual(basic_operations.search_customer(2).home_address, cust_2['home_address'])
+        self.assertEqual(basic_operations.search_customer(2)['home_address'],
+                         cust_2['home_address'])
 
         # The duplicate is True -- so if there was no overwrite, this will pass
-        self.assertEqual(basic_operations.search_customer(4).is_active, False)
+        self.assertEqual(basic_operations.search_customer(4)['is_active'], False)
 
     def test_2_update_record(self):
         """Test that a record is updated when the credit_limit is <=7 digits."""
@@ -62,9 +63,9 @@ class BaseDbTest(TestCase):
         # This will fail
         self.assertEqual(basic_operations.update_customer_credit(3, 1000.00), False)
 
-        self.assertEqual(basic_operations.search_customer(2).credit_limit, 9999999.00)
+        self.assertEqual(basic_operations.search_customer(2)['credit_limit'], 9999999.00)
         self.assertEqual(basic_operations.update_customer_credit(2, 1000.00), True)
-        self.assertEqual(basic_operations.search_customer(2).credit_limit, 1000.00)
+        self.assertEqual(basic_operations.search_customer(2)['credit_limit'], 1000.00)
 
     def test_3_delete_records(self):
         """Test that a record is deleted if it exists."""
