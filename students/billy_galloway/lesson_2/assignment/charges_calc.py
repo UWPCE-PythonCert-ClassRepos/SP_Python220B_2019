4a684924a5aa866c917aa511d06dbfc08a0927cb
Billy Galloway <b.galloway@f5.com>
2020-02-07 17:57:52 -0800

    refresh assignment

--- students/billy_galloway/lesson_2/assignment/charges_calc.py
+++ students/billy_galloway/lesson_2/assignment/charges_calc.py
@@ -6,6 +6,7 @@ import json
 import datetime
 import math
 import logging
+import sys
 
 
 logger = logging.getLogger(__name__)
@@ -17,10 +18,8 @@ formatter = logging.Formatter(LOG_FORMAT)
 log_file = datetime.datetime.now().strftime("%Y-%m-%d")+'.log'
 file_handler = logging.FileHandler(log_file)
 file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
 
 def logging_level(log_level):
-    print("log level: ", log_level)
     levels = {
         0: logging.NOTSET,
         1: logging.ERROR,
@@ -35,7 +34,7 @@ def parse_cmd_arguments():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--input', help='input JSON file', required=True)
     parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
-    parser.add_argument('-d', '--debug', type=int, help='logging verbosity', required=True)
+    parser.add_argument('-d', '--debug', action='count', help='logging verbosity', required=False)
 
     return parser.parse_args()
 
@@ -61,27 +60,43 @@ def calculate_additional_fields(data, debug=None):
             rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
             logger.info(f"rental end {value['rental_end']}")
             
+            # ensure that total days is always positive
+            value['total_days'] = abs((rental_end - rental_start).days)
+            logger.debug(f"total days: {value['total_days']}")
+
             # added check to catch this issue of having the start data
             # after the end date, logged the reason, and continued
             if rental_end < rental_start:
-                logger.warning(f"rental end is before rental start")
-                logger.info(f"skipping total days")
+                logger.error(f"rental end is before rental start date")
+                logger.error(f"skipping calculations: total_price, sqrt_total_price, unit_cost")
+                rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
+                
+                # correct the start and end dates
+                switch_start = value['rental_end']
+                switch_end = value['rental_start']
+
+                value['rental_end'] = switch_end
+                value['rental_start'] = switch_start
+
+                value['total_price'] = value['total_days'] * value['price_per_day']
+                print(value['total_price'])
+                value['sqrt_total_price'] = math.sqrt(value['total_price'])
+                continue
+            # Prevent evaluation from breaking when no units rented
+            if value['units_rented'] < 1:
+                logger.debug(f"value cost: {value['unit_cost']} no units rented {value['units_rented']}")
+                value['unit_cost'] = 0
                 continue
 
-            value['total_days'] = (rental_start - rental_end).days
-
             value['total_price'] = value['total_days'] * value['price_per_day']
-
-            if value['total_price'] < 0: 
-                logger.info(f"total price equals a negative value{value['total_price']}")
-
+            logger.debug(f"total price: {value['total_price']}")
 
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
-            logger.info(f"square root price {value['sqrt_total_price']}")
+            logger.debug(f"square root price {value['sqrt_total_price']}")
+            
             
             value['unit_cost'] = value['total_price'] / value['units_rented']
-            logger.info(f"value cost: {value['unit_cost']} = {value['total_price']} / {value['units_rented']}")
-        
+
         except ValueError as value_error:
             logger.error(f"Value error caught {value_error}")
 
@@ -91,6 +106,7 @@ def calculate_additional_fields(data, debug=None):
 def save_to_json(filename, data, debug=None):
     with open(filename, 'w') as file:
         json.dump(data, file)
+
 
 if __name__ == "__main__":
     
@@ -102,8 +118,15 @@ if __name__ == "__main__":
             # integer to the logging_level 
             # method
             levels = logging_level(args.debug)
-            logger.setLevel(levels)
-
+            if levels == 10:
+                logger.setLevel(levels)
+                console_handler = logging.StreamHandler(sys.stdout)
+                console_handler.setFormatter(formatter)
+                logger.addHandler(console_handler)
+            else:
+                logger.setLevel(levels)
+                logger.addHandler(file_handler)
+                
         except KeyError:
             # If the debug value does not match
             # a key in the log level dict then exit
