40c65bb12adb234da84b7f21d3558cfcced9189c
Sean Hodges <sthodges@gmail.com>
2020-02-08 22:25:10 -0800

    Convert CSV's to dicts

--- students/shodges/lesson05/database.py
+++ students/shodges/lesson05/database.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from pathlib import Path
+import csv
 
 class DBConnection():
     """
@@ -25,3 +27,17 @@ class DBConnection():
         Close the connection when exiting the context manager.
         """
         self.connection.close()
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    """
+    Import data from specified CSV's into the database.
+    """
+    data_directory = Path(directory_name)
+    with open(data_directory/product_file, mode='r') as product_input:
+        product_list = {row[0]:row[1] for row in csv.reader(product_input)}
+
+    with open(data_directory/customer_file, mode='r') as customer_input:
+        customer_list = {row[0]:row[1] for row in csv.reader(customer_input)}
+
+    with open(data_directory/rentals_file, mode='r') as rentals_input:
+        rentals_list = {row[0]:row[1] for row in csv.reader(rentals_input)}
