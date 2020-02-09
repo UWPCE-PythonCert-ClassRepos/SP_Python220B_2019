77e5a56ed3d26cdffff3cecf8940081ba4401ef7
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-02-08 21:21:16 -0500

    updated pylint recommendations

--- students/eric_grandeo/lesson01/inventory_management/electric_appliances_class.py
+++ students/eric_grandeo/lesson01/inventory_management/electric_appliances_class.py
@@ -5,7 +5,7 @@ Classes:
 """
 from inventory_class import inventory
 
-class ElectricAppliances(inventory):
+class ElectricAppliances(inventory): # pylint: disable=too-few-public-methods
     """
     This is a class for Electric applicances.
     Attributes:
@@ -16,20 +16,19 @@ class ElectricAppliances(inventory):
         brand: name brand.
         voltage: voltage.
     """
-    
     def __init__(self, brand, voltage):
         self.brand = brand
         self.voltage = voltage
         super().__init__(self)
 
     def returnAsDictionary(self):
-        outputDict = {}
-        outputDict['productCode'] = self.productCode
-        outputDict['description'] = self.description
-        outputDict['marketPrice'] = self.marketPrice
-        outputDict['rentalPrice'] = self.rentalPrice
-        outputDict['brand'] = self.brand
-        outputDict['voltage'] = self.voltage
+        output_dict = {}
+        output_dict['productCode'] = self.productCode
+        output_dict['description'] = self.description
+        output_dict['marketPrice'] = self.marketPrice
+        output_dict['rentalPrice'] = self.rentalPrice
+        output_dict['brand'] = self.brand
+        output_dict['voltage'] = self.voltage
 
-        return outputDict
+        return output_dict
     
