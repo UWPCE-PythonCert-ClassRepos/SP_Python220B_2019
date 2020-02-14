75738e6c12a03cb17afdd9efd33ffd9f3c929f54
Alex Filson <alexdfilson@gmail.com>
2020-02-14 11:22:06 -0800

    Pulled assertEqual out of context manager.

--- students/al_headstrong/lesson08/assignment/test_inventory.py
+++ students/al_headstrong/lesson08/assignment/test_inventory.py
@@ -13,28 +13,25 @@ class InventoryTest(TestCase):
     def setUp(self):
         """Establish class data structures."""
         self.test_invoice_file = 'test_invoice.csv'
-        self.test_furniture_list = [["Elisa Miles", "LR04", "Leather Sofa", 25],
-                                    ["Edward Data", "KT78", "Kitchen Table", 10]]
-        self.test_single_list = [["Susan Wong", "LR04", "Leather Sofa", 25],
-                                 ["Susan Wong", "KT78", "Kitchen Table", 10],
-                                 ["Susan Wong", "BR02", "Queen Mattress", 17]]
+        self.test_furniture_list = ["Elisa Miles", "LR04", "Leather Sofa", "25"]
+        self.test_single_list = ["Susan Wong", "LR04", "Leather Sofa", "25"]
 
     def test_add_furniture(self):
         """Test add_furniture function."""
         open(self.test_invoice_file, 'w').close()
-        inventory.add_furniture("rented_items.csv", "Elisa Miles", "LR04", "Leather Sofa", 25)
-        inventory.add_furniture("rented_items.csv", "Edward Data", "KT78", "Kitchen Table", 10)
-        with open(self.test_invoice_file) as file:
+        inventory.add_furniture(self.test_invoice_file, "Elisa Miles", "LR04", "Leather Sofa", 25)
+        inventory.add_furniture(self.test_invoice_file, "Edward Data", "KT78", "Kitchen Table", 10)
+        with open(self.test_invoice_file, 'r') as file:
             contents = csv.reader(file)
-            for j, k in zip(contents, self.test_furniture_list):
-                self.assertEqual(j, k)
+            check_row = next(contents)
+        self.assertEqual(self.test_furniture_list, check_row)
 
     def test_single_customer(self):
         """Test single_customer function."""
         open(self.test_invoice_file, 'w').close()
-        create_invoice = inventory.single_customer("Susan Wong", "rented_items.csv")
+        create_invoice = inventory.single_customer("Susan Wong", self.test_invoice_file)
         create_invoice("test_items.csv")
-        with open(self.test_invoice_file) as file:
+        with open(self.test_invoice_file, 'r') as file:
             contents = csv.reader(file)
-            for j, k in zip(contents, self.test_single_list):
-                self.assertEqual(j, k)
+            check_row = next(contents)
+        self.assertEqual(self.test_single_list, check_row)
