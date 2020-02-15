02f0b57314ad8bc4d979c7896c94e9f81051e8c1
Alexander Boone <alboone@uw.edu>
2020-02-15 10:52:05 -0800

    Replaced my serial num gen. with uuid package and it was much faster.

--- students/alexander_boone/lesson06/assignment/data/generate_new_data.py
+++ students/alexander_boone/lesson06/assignment/data/generate_new_data.py
@@ -4,6 +4,7 @@ in the format shown in data/exercise.csv
 '''
 import datetime
 import random
+from uuid import uuid4
 from string import ascii_letters, digits, ascii_lowercase
 
 
@@ -12,18 +13,18 @@ SERIAL_LENGTHS = [8, 4, 4, 4, 12]
 AO = ['ao', '']
 
 # Generate Serial Number
-def random_alphanum_string(stringlength):
-    '''Generate random alphanumeric string'''
-    letters_and_numbers = ascii_lowercase + digits
-    return ''.join(random.choice(letters_and_numbers)
-                   for i in range(stringlength))
-
-def serial_num_generator(serial_lengths_list=SERIAL_LENGTHS):
-    '''
-    Generate random alphanumeric serial number
-    given the input list of section lengths.
-    '''
-    return '-'.join([random_alphanum_string(x) for x in serial_lengths_list])
+#def random_alphanum_string(stringlength):
+#    '''Generate random alphanumeric string'''
+#    letters_and_numbers = ascii_lowercase + digits
+#    return ''.join(random.choice(letters_and_numbers)
+#                   for i in range(stringlength))
+#
+#def serial_num_generator(serial_lengths_list=SERIAL_LENGTHS):
+#    '''
+#    Generate random alphanumeric serial number
+#    given the input list of section lengths.
+#    '''
+#    return '-'.join([random_alphanum_string(x) for x in serial_lengths_list])
 
 # Generate date
 def generate_random_date():
@@ -43,10 +44,9 @@ def generate_random_date():
 with open('new_exercise_data.csv', 'w') as csvfile:
     middle_nums = [0, 1, 2, 3]
     for i in range(1000000):
-        serial_num = [serial_num_generator()]
         middle_nums = [str(i+1), str(i+2), str(i+3), str(i+4)]
         write_list = [
-            serial_num_generator(),
+            str(uuid4()),
             ','.join(middle_nums),
             generate_random_date(),
             random.choice(AO)
