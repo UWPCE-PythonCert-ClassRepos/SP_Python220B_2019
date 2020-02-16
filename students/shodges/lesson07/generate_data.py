bcf778a1ec89af76a21347a91c26757abaa1d393
Sean Hodges <sthodges@gmail.com>
2020-02-16 13:21:09 -0800

    Add generate_data() functionality for products

--- students/shodges/lesson07/generate_data.py
+++ students/shodges/lesson07/generate_data.py
@@ -10,6 +10,9 @@ def generate_data(filename, target_count, data_type):
     Generate additional random data in filename.  target_count provides a declarative end-state
     for the count of records in the CSV.  data_type should be either: customers, products, or
     rentals.
+
+    Note that this makes heavy use of the assignment operator inside comprehensions to reuse
+    randomly generated fields in later fields so it requires Python 3.8!
     """
     with open(filename, 'r') as csvfile:
         # Evaluate whether the data file has a newline at the end of the file
@@ -45,14 +48,33 @@ def generate_data(filename, target_count, data_type):
                             'napple.com', 'oreoz.com', 'pinternet.com', 'quorum.com']
 
             rows = [['cust_' + str(i),
-                     fn:=random.choice(first_names) + " " +
-                     ln:=random.choice(last_names),
-                     str(random.randint(1,99999)) + ' ' + random.choice(street_names),
-                     random.randint(1000000000,9999999999),
+                     (fn:=random.choice(first_names)) + " " +
+                     (ln:=random.choice(last_names)),
+                     str(random.randint(1, 99999)) + ' ' + random.choice(street_names),
+                     random.randint(1000000000, 9999999999),
                      fn.lower() + "." + ln.lower() + "@" + random.choice(email_domain)]
                     for i in range(current_count + 1, target_count + 1)]
+        elif data_type == 'products':
+            colors = ['Blue', 'Cyan', 'Green', 'Mint', 'Indigo', 'Violet', 'Purple', 'Hot Pink',
+                      'Maroon', 'Red', 'Yellow', 'Orange', 'Burgundy', 'Brown', 'Gray']
+            product_type = ['Furniture', 'ElectricAppliance', 'Other']
+            products = {'Furniture': ['Sofa', 'Recliner', 'Dining Chair', 'Coffee Table',
+                                      'Dining Table', 'Barstool', 'Curio Cabinet', 'China Cabinet',
+                                      'End Table', 'Bed', 'Nightstand', 'Dresser', 'Armoire'],
+                        'ElectricAppliance': ['Oven', 'Refrigerator', 'Washer', 'Dryer',
+                                              'Dishwasher', 'Microwave'],
+                        'Other': ['Floor Lamp', 'Table Lamp', 'Vase', 'Picture Frame', 'Bowl',
+                                  'Dinner Plate', 'Salad Plate', 'Serving Dish', 'Fake Grass',
+                                  'Fake Flowers']}
+
+            rows = [['prod_' + str(i),
+                     random.choice(colors) + ' ' +
+                     random.choice(products[(pt:=random.choice(product_type))]),
+                     pt,
+                     random.randint(100, 999)]
+                    for i in range(current_count + 1, target_count + 1)]
             # Add lines in the same format
-            writer.writerows(rows)
+        writer.writerows(rows)
 
 if __name__ == "__main__":
     generate_data('data/exercise.csv', 1000000)
