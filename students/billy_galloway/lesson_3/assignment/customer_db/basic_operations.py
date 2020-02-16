a31e59a9aced1e9eb6e9738dbce878db62ddaaac
brgalloway <digital.locust@gmail.com>
2020-02-15 19:50:34 -0800

    updating assignment

--- students/billy_galloway/lesson_3/assignment/customer_db/basic_operations.py
+++ students/billy_galloway/lesson_3/assignment/customer_db/basic_operations.py
@@ -86,24 +86,7 @@ def update_customer_credit(customer_id, credit_limit):
         raise ValueError(f'{customer.customer_id} not found in database')
 
 def list_active_customers():
-    try:
-        active_customers = Customer.select().where(Customer.status).count()
-        active_cust_name = Customer.select().where(Customer.status)
+    active_customers = Customer.select().where(Customer.status).count()
+    logger.info(f'active customer count is {active_customers}')
 
-        logger.info(f'active customer count is {active_customers}')
-    except:
-        pass
     return active_customers
-# add_customer(customers[0][CUST_ID], customers[0][NAME], customers[0][LAST_NAME],
-#              customers[0][HOME_ADDRESS], customers[0][EMAIL_ADDRESS],
-#              customers[0][PHONE], customers[0][STATUS], customers[0][CREDIT_LIMIT])
-
-# add_customer(customers[1][CUST_ID], customers[1][NAME], customers[1][LAST_NAME],
-#              customers[1][HOME_ADDRESS], customers[1][EMAIL_ADDRESS],
-#              customers[1][PHONE], customers[1][STATUS], customers[1][CREDIT_LIMIT])
-
-# add_customer(customers[2][CUST_ID], customers[2][NAME], customers[2][LAST_NAME],
-#              customers[2][HOME_ADDRESS], customers[2][EMAIL_ADDRESS],
-#              customers[2][PHONE], customers[2][STATUS], customers[2][CREDIT_LIMIT])
-
-print(list_active_customers())
--- students/billy_galloway/lesson_3/assignment/tests/test_unit.py
+++ students/billy_galloway/lesson_3/assignment/tests/test_unit.py
@@ -49,4 +49,16 @@ class DatabaseTests(TestCase):
         '''
         pass
 
-    
+    # add_customer(customers[0][CUST_ID], customers[0][NAME], customers[0][LAST_NAME],
+#              customers[0][HOME_ADDRESS], customers[0][EMAIL_ADDRESS],
+#              customers[0][PHONE], customers[0][STATUS], customers[0][CREDIT_LIMIT])
+
+# add_customer(customers[1][CUST_ID], customers[1][NAME], customers[1][LAST_NAME],
+#              customers[1][HOME_ADDRESS], customers[1][EMAIL_ADDRESS],
+#              customers[1][PHONE], customers[1][STATUS], customers[1][CREDIT_LIMIT])
+
+# add_customer(customers[2][CUST_ID], customers[2][NAME], customers[2][LAST_NAME],
+#              customers[2][HOME_ADDRESS], customers[2][EMAIL_ADDRESS],
+#              customers[2][PHONE], customers[2][STATUS], customers[2][CREDIT_LIMIT])
+
+print(list_active_customers())
