b0a173631651fee2dc92384bba0c2c53a8fb664b
Sean Hodges <sthodges@gmail.com>
2020-02-18 00:22:43 -0800

    Format with 2 significant digits

--- students/shodges/lesson08/inventory.py
+++ students/shodges/lesson08/inventory.py
@@ -12,7 +12,7 @@ def add_furniture(invoice_file, customer_name, item_code, item_description, item
     """
     with open(invoice_file, 'a') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='"')
-        writer.writerow([customer_name, item_code, item_description, item_monthly_price])
+        writer.writerow([customer_name, item_code, item_description, '{:.2f}'.format(item_monthly_price)])
 
 
 #def single_customer(customer_name, invoice_file):
