d830a8d234ef6e6059d929b11038805344f6c9f3
mattcasari <mjcasari@gmail.com>
2020-02-18 13:21:53 -0800

    bumping iterator up to 1 million.
    Was previously running 100000, now up to 1 million.  Also
    truncated the datetime function

--- students/mattcasari/lesson06/assignment/create_data.py
+++ students/mattcasari/lesson06/assignment/create_data.py
@@ -16,8 +16,7 @@ def create_random_data():
     """
     idx=1
     while True:
-        r_date = dt.fromtimestamp(randrange(DATETIME_MIN, DATETIME_MAX))
-        r_date = r_date.strftime("%m/%d/%Y")
+        r_date = dt.fromtimestamp(randrange(DATETIME_MIN, DATETIME_MAX)).strftime("%m/%d/%Y")
         data = f"{str(uuid4())},{idx},{idx+1},{idx+2},{idx+3},{r_date},{choice(['ao',''])}\n"
         yield data
         idx += 1
@@ -30,7 +29,7 @@ def main():
     """
     data_gen = create_random_data()
     with open('exercise.csv', 'w+') as csv_file:
-        [csv_file.write(next(data_gen)) for x in range(100000)]
+        [csv_file.write(next(data_gen)) for x in range(1000000)]
 
 
 if __name__ == "__main__":
