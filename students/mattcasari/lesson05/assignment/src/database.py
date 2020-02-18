97bfe9265f3d6fb595b5f1bdb0330f43cc9988aa
mattcasari <mjcasari@gmail.com>
2020-02-17 19:04:56 -0800

    Most of lesson 05 complete.

--- students/mattcasari/lesson05/assignment/src/database.py
+++ students/mattcasari/lesson05/assignment/src/database.py
@@ -3,13 +3,37 @@
 # pylint: disable=unused-wildcard-import
 # pylint: disable=wildcard-import
 # pylint: disable=too-many-arguments
-# pylint: disable=broad-except
 
-import pymongo
+
+from pymongo import MongoClient
 import csv
 import logging
 from pathlib import Path
 
+#
+PRODUCT_NAME_KEYS = [
+    "product_id",
+    "description",
+    "market_price",
+    "rental_price",
+    "material",
+    "size",
+    "brand",
+    "voltage",
+]
+CUSTOMER_NAME_KEYS = [
+    "customer_id",
+    "name",
+    "last_name",
+    "address",
+    "phone_number",
+    "email_address",
+    "status",
+    "credit_limit",
+]
+RENTAL_NAME_KEYS = ["product_id", "description", "market_price", "rental_price"]
+
+# LOGGER SETUP
 LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
 LOG_FILE = "db.log"
 FORMATTER = logging.Formatter(LOG_FORMAT)
@@ -27,37 +51,98 @@ LOGGER.setLevel(logging.DEBUG)
 LOGGER.addHandler(FILE_HANDLER)
 LOGGER.addHandler(CONSOLE_HANDLER)
 
-def error_counter():
+
+class MongoDBConnection:
+    """MongoDB Connection"""
+
+    def __init__(self, host="127.0.0.1", port=27017):
+        """ be sure to use the ip address not name for local windows"""
+        LOGGER.info("Creating DB Connection: host=%s, port=%d", host, port)
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+def _counter(value=0):
     """ Error counter generator """
     cnt = 0
     while True:
         yield cnt
         cnt = cnt + 1
 
-def import_product_csv(directory_name, product_file):
+
+def populate_database(db_object, collection_name, data):
+    LOGGER.info("Populating database collection %s", collection_name)
+
+    record_cnt = 0
+    error_cnt = _counter()
+
+    if collection_name == "products":
+        collection_keys = PRODUCT_NAME_KEYS
+    elif collection_name == "customers":
+        collection_keys = CUSTOMER_NAME_KEYS
+    elif collection_name == "rentals":
+        collection_keys = RENTAL_NAME_KEYS
+
+    if len(data) > 1:
+        db_object[collection_name].insert_many(data)
+        record_cnt = len(data)
+    else:
+        db_object[collection_name].insert_one(data[0])
+        record_cnt = 1
+    LOGGER.debug(f"Record length is {record_cnt}")
+    return (record_cnt, next(error_cnt))
+
+
+def import_csv(directory_name, file_name):
     """ Import Product CSV File
     Import Product CSV File from given path and filename and returns
     a dictionary of the data.
 
     Args:
         directory_name: Path where CSV file lives (from root directory)
-        product_file: File name of product CSV file
+        file_name: File name of product CSV file
     Returns:
         dict: Dictionary of product data from CSV
     """
-    directory_name = Path('./'+directory_name)
-    file_path = directory_name / product_file
+    LOGGER.info("Importing CSV")
+    directory_name = Path("./" + directory_name)
+    file_field = file_name + ".csv"
+    file_path = directory_name / file_field
     LOGGER.info("Importing Product CSV file: %s", file_path)
-    product_dict = {}
-    with open(file_path, "r") as csvfile:
-        csv_data = csv.reader(csvfile, delimiter=',')
-        
-        for idx, row in enumerate(csv_data):
-            print(row)
-            if idx == 0:
-                csv_header = row
-            else:
-                product_dict.update(_product_file_parser(csv_header, row))
+    error_cnt = 0
+
+    return_data = []
+    try:
+        with open(file_path, "r") as csvfile:
+            csv_data = csv.reader(csvfile, delimiter=",")
+
+            for idx, row in enumerate(csv_data):
+                LOGGER.debug(f"ICSV Header: {row}")
+                if idx == 0:
+                    header = row
+                    LOGGER.debug(f"{file_name}")
+                    LOGGER.debug(f"Validating headers: {header}")
+                    _validate_headers(header, file_name)
+                else:
+                    LOGGER.debug(f"ICSV Data: {row}")
+                    # product_dict = {}
+                    temp_product = _file_parser(row, header)
+                    return_data.append(temp_product)
+    except IndexError:
+        error_cnt = 1
+    return (error_cnt, return_data)
+
+
+
+
 
 def import_data(directory_name, product_file, customer_file, rentals_file):
     """ 
@@ -77,26 +162,139 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
         IOError: Invalid File provided
         IndexError: Mismatched data and header length in file
     """
-    directory_name = Path(directory_name)
     record_count = [0, 0, 0]
+    error_count = [0, 0, 0]
+    mongo = MongoDBConnection()
+
+    collections = ["products", "customers", "rentals"]
+    with mongo:
+        db = mongo.connection.media
+
+        for idx, collection in enumerate(collections):
+            LOGGER.info("Importing rental file name: %s", collection)
+            data = []
+            e_cnt = 0
+            r_cnt = 0
+            [e_cnt, data] = import_csv(directory_name, product_file)
+            LOGGER.debug(f"Data length is {len(data)}")
+            error_count[idx] += e_cnt
+            [r_cnt, e_cnt] = populate_database(db, collection, data)
+            LOGGER.debug(f"rcnt={r_cnt}, ecnt={e_cnt}")
+            error_count[idx] += e_cnt
+            record_count[idx] += r_cnt
+
+    LOGGER.info("Products-> Record Count=%d, Error Count=%d", record_count[0], error_count[0])
+    LOGGER.info("Customers-> Record Count=%d, Error Count=%d", record_count[1], error_count[1])
+    LOGGER.info("Rentals-> Record Count=%d, Error Count=%d", record_count[2], error_count[2])
     
-    try:
-        pass
-        # with open(directory_name / product_file) as csvfile:
-        #     csv_header = csv.reader(csvfile, delimiter=',')
-    except IOError:
-        LOGGER.error('Invalid product file name %s', product_file)
-    except IndexError:
-        LOGGER.error('Mismatched data and header length')
-        LOGGER.error('Header: %s', csv_header)
-        LOGGER.error('Data:%s', csv_data)
-        
+    return (record_count, error_count)
 
 
-def _product_file_parser(header, p_data):
+def _validate_headers(headers, collection_name):
+    """ Validate the file headers in CSV file against expected collection name keys"""
+    if collection_name == "products":
+        collection_key = PRODUCT_NAME_KEYS
+    elif collection_name == "customers":
+        collection_key = CUSTOMER_NAME_KEYS
+    elif collection_name == "rentals":
+        collection_key = RENTAL_NAME_KEYS
+    LOGGER.debug(f"Provided Headers: {headers}")
+    LOGGER.debug(f"Collection Keys: {collection_key}")
+    for h in headers:
+        LOGGER.debug(h)
+        if h not in collection_key:
+            LOGGER.error(f"{h} not valid header key")
+            raise ValueError(f"{h} not in collection {collection_name} expected keys")
+
+
+def _file_parser(data, headers):
     """ Parse a line of the product file """
-    if len(header) != len(p_data):
-        raise IndexError('Data is not the same length as header')
-    d_vals = dict(zip(header,p_data))
-    LOGGER.info('Created file data: %s', d_vals)
+
+    if len(headers) != len(data):
+        LOGGER.error(
+            "Header length is %d, but data length is %d", len(headers), len(data)
+        )
+        raise IndexError("Data is not same length as header")
+    else:
+        d_vals = dict(zip(headers, data))
+
+    LOGGER.info("Created file data: %s", d_vals)
     return d_vals
+
+
+def _drop_collections(db_obj):
+    """ Drop/remove all collections from database """
+    db_obj.products.drop()
+    # db_obj.customer.drop()
+    db_obj.rentals.drop()
+
+
+# def import_product(directory_name, product_name):
+#     LOGGER.info("Importing product file %s", product_file)
+#     error_cnt = _counter()
+#     try:
+#         data = import_csv(directory_name / product_file)
+#     except IOError:
+#         LOGGER.error("Invalid product file name: %s", product_file)
+#         p_error_cnt()
+#     except IndexError:
+#         p_error_cnt()
+#         LOGGER.error("Mismatched data and header length")
+#         LOGGER.error("Header: %s", csv_header)
+#         LOGGER.error("Data:%s", csv_data)
+
+#     LOGGER.info("Creating Product Database")
+#     # try:
+
+#     # except:
+
+#     LOGGER.info("Populating Product Database")
+
+#     return (data, error_cnt)
+
+# def create_collections(db_object):
+#     LOGGER.info("Creating collection")
+
+#     prod = db_object["products"]
+#     customer = db_object["customers"]
+#     rentals = db_object["rentals"]
+
+#     [print(x) for x in db_object.list_collection_names()]
+
+
+# def import_collection(directory_name, file_name, db_obj, collection_name):
+#     """ Import Collection
+
+#     Imports the selected collection and returns record count and
+#     error count
+
+#     Args:
+#         directory_name: Path where csv files live
+#         file_name: CSV file with collection information (line 1 must be header!)
+#         collectin_name: Name of database collection to create/update
+#     Returns:
+#         record_count: # of entries added in collection
+#         error_count: # of errors occured with collection
+#     Raises:
+#         TBD
+#     """
+#     error_cnt = 0
+#     record_cnt = 0
+
+#     # Create Collection
+#     collection = db_obj[collection_name]
+
+#     # Import
+#     try:
+#         data = import_csv(directory_name, file_name)
+#     except FileNotFoundError:
+#         next(errors())
+
+#     # Update DB
+#     try:
+#         pass
+#     except:
+#         next(error_cnt())
+
+#     # Return
+#     return (record_cnt, error_cnt)
--- students/mattcasari/lesson05/assignment/tests/test_database.py
+++ students/mattcasari/lesson05/assignment/tests/test_database.py
@@ -6,63 +6,98 @@
 
 import logging
 import io
-from unittest import TestCase
+from unittest import TestCase, TestLoader
 
 from src import database as db
 
+TestLoader.sortTestMethodsUsing = None
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 LOGGER.info("START: test_database")
+LOGGER.setLevel(logging.INFO)
 
-EXPECTED_PRODUCT_DICT = {
-    "V0032-100":{
-        'product_id':"V0032-100", 
-        'descriptin':"Spice Harvester", 
-        'market_price':50000000.00, 
-        'rental_price':3000.00, 
-        'brand':"Spice World", 
-        'voltage': '1kV'
+EXPECTED_PRODUCT_DICT = [
+    {
+        "product_id": "V0032-100",
+        "description": "Spice Harvester",
+        "market_price": "50000000.00",
+        "rental_price": "3000.00",
+        "brand": "Spice World",
+        "voltage": "1kV",
+        "material": "",
+        "size": "",
     },
-    "T0072-401":{
-        'product_id':"T0072-401", 
-        'description':"Thumper", 
-        'market_price':2000.00, 
-        'rental_price':49.95, 
-        'brand':"Sandy Tools", 
-        'voltage':12, 
+    {
+        "product_id": "T0072-401",
+        "description": "Thumper",
+        "market_price": "2000.00",
+        "rental_price": "49.95",
+        "brand": "Sandy Tools",
+        "voltage": "12",
+        "material": "",
+        "size": "",
     },
-    "F1001-223":{
-        'product_id':"F1001-223", 
-        'description':"Harkonnen Chair", 
-        'market_price':820.00, 
-        'rental_price':12.40,
-        'material':"bone", 
-        'size':"large"
+    {
+        "product_id": "F1001-223",
+        "description": "Harkonnen Chair",
+        "market_price": "820.00",
+        "rental_price": "12.40",
+        "material": "bone",
+        "size": "large",
+        "brand": "",
+        "voltage": "",
     },
-    "F1001-052":{
-        'product_id':"F1001-052", 
-        'descriptinon':"Chair dog",  
-        'market_price':4000.00, 
-        'rental_price':70.00, 
-        'material':"bio", 
-        'size':"medium"
+    {
+        "product_id": "F1001-052",
+        "description": "Chair dog",
+        "market_price": "4000.00",
+        "rental_price": "70.00",
+        "material": "bio",
+        "size": "medium",
+        "brand": "",
+        "voltage": "",
+    },
+]
+
+EXPECTED_CUSTOMERS_DICT = [
+    {
+        "customer_id": "C1950991",
+        "name": "Paul",
+        "last_name": "Atreides",
+        "address": "Arrakis",
+        "phone_number": "888-778-6668",
+        "email_address": "thewormisspice@dune.net",
+        "status": "True",
+        "credit_limit": "999999.99",
     }
-}
+]
+
+EXPECTED_RENTALS_DICT = [
+    {
+        "product_id": "T0072-401",
+        "description": "Thumper",
+        "market_price": "2000.00",
+        "rental_price": "49.95",
+    }
+]
+
 
 class GeneratorTests(TestCase):
     """ Tests for MongoDB database functions """
+
     def setUp(self):
         """ Database Setup """
         pass
+
     def tearDown(self):
         """ Database teardown """
         pass
 
     def test_error_counter(self):
-        """ Test the generator error_counter """
+        """ Test the generator _counter """
         # Given
-        err = db.error_counter()
+        err = db._counter()
 
         # When
         next(err)
@@ -73,100 +108,236 @@ class GeneratorTests(TestCase):
         self.assertEqual(3, next(err))
 
     def test_error_counter_for_multiple_errors(self):
-        """ Test the generator error_counter for multiple errors """
+        """ Test the generator _counter for multiple errors """
         # Given
-        err1 = db.error_counter()
-        err2 = db.error_counter()
-        err3 = db.error_counter()
+        err1 = db._counter()
+        err2 = db._counter()
+        err3 = db._counter()
 
         # When
-        for i in range(0,50):
+        for i in range(0, 50):
             next(err1)
-        for i in range(0,300):
+        for i in range(0, 300):
             next(err2)
-        for i in range(0,7000):
+        for i in range(0, 7000):
             next(err3)
 
         # Then
         self.assertEqual(50, next(err1))
         self.assertEqual(300, next(err2))
         self.assertEqual(7000, next(err3))
-    
-class ImportFilesTests(TestCase):
-    """ Tests the functions responsible for importing data files (CSVs)"""
-    def setUp(self):
-        """ Set-up prior to running test"""
-        pass
-    def tearDown(self):
-        """ Teardown after each test"""
-        pass
-    def test_import_product_csv(self):
-        """Test the _import_product_csv function"""
-        # Given
-        directory_name = "csv_files"
-        product_file = "products.csv"
 
-        # When
-
-        # Then
-        result_dict = db.import_product_csv(directory_name, product_file)
-
-        self.assertDictEqual(EXPECTED_PRODUCT_DICT, result_dict)
 
 class ParserTests(TestCase):
     """ Tests for MongoDB database functions """
+
     def setUp(self):
         """ Database Setup """
         pass
+
     def tearDown(self):
         """ Database teardown """
         pass
-    
-    def test_product_file_parser_with_valid_data(self):
+
+    def test_file_parser_with_valid_data(self):
         """ Test product_file_parser with valid data """
         # Given
-        header_val = ['A', 'B', 'C', 'D']
-        data_val = [1, 2, 3, 4]
-
+        data = ["ID001", "Voice Modulator", "9999.99", "129.50"]
+        headers = ["product_id", "description", "market_price", "rental_price"]
         # When
-        expected_data = {'A':1, 'B':2, 'C':3, 'D':4}
-        
+        expected_data = {
+            "product_id": "ID001",
+            "description": "Voice Modulator",
+            "market_price": "9999.99",
+            "rental_price": "129.50",
+        }
+
         # Then
-        actual_data = db._product_file_parser(header_val, data_val)
+        actual_data = db._file_parser(data, headers)
         LOGGER.info(actual_data)
         self.assertEqual(expected_data, actual_data)
 
     def test_product_file_parser_fails_with_invalid_data(self):
         """ Test product_file_parser with invalid data """
         # Given
-        header_val = ['X', 'Y', 'Z']
         data_val = [5, 6, 7, 8]
-
+        headers = [
+            "product_id",
+            "description",
+            "market_price",
+            "rental_price",
+            "material",
+            "size",
+            "brand",
+            "voltage",
+        ]
         # When
 
         # Then
         with self.assertRaises(IndexError):
-            db._product_file_parser(header_val, data_val)
+            db._file_parser(data_val, "products")
+
+    def test_validate_headers(self):
+        """ Test if headers provided are valid with _validate_headers method"""
+        # Given
+        headers = [
+            "product_id",
+            "description",
+            "market_price",
+            "rental_price",
+            "material",
+            "size",
+            "brand",
+            "voltage",
+        ]
+
+        # When
+
+        # Then
+        db._validate_headers(headers, "products")
+
+    def test_validate_headers_has_bad_header_name(self):
+        """ Test _validate_headers with a bad header value"""
+        # Given
+        headers = ["product_id", "description", "mkt_value", "rental_value"]
+        # When
+
+        # Then
+        with self.assertRaises(ValueError):
+            db._validate_headers(headers, "rentals")
+
+
+class ImportFilesTests(TestCase):
+    """ Tests the functions responsible for importing data files (CSVs)"""
+
+    def setUp(self):
+        """ Set-up prior to running test"""
+        pass
+
+    def tearDown(self):
+        """ Teardown after each test"""
+        pass
+
+    def test_import_csv(self):
+        """Test the _import_csv function"""
+        # Given
+        directory_name = "csv_files"
+        product_file = "products"
+
+        # When
+
+        # Then
+        [error_cnt, result_dict] = db.import_csv(directory_name, product_file)
+        LOGGER.debug(f"Test Results: {result_dict}")
+        self.assertEqual(len(EXPECTED_PRODUCT_DICT), len(result_dict))
+        self.assertDictEqual(EXPECTED_PRODUCT_DICT[0], result_dict[0])
+
+    def test_import_customer_csv(self):
+        """Test the _import_csv function for customers"""
+        # Given
+        directory_name = "csv_files"
+        product_file = "customers"
+
+        # When
+
+        # Then
+        [error_cnt, result_dict] = db.import_csv(directory_name, product_file)
+        self.assertDictEqual(EXPECTED_CUSTOMERS_DICT[0], result_dict[0])
+
+    def test_import_rental_csv(self):
+        """Test the _import_csv function for rentalss"""
+        # Given
+        directory_name = "csv_files"
+        product_file = "rentals"
+
+        # When
+
+        # Then
+        [error_cnt, result_dict] = db.import_csv(directory_name, product_file)
+        self.assertDictEqual(EXPECTED_RENTALS_DICT[0], result_dict[0])
 
 
 class DatabaseTest(TestCase):
     """ Tests for MongoDB database functions """
+
     def setUp(self):
         """ Database Setup """
         pass
+
     def tearDown(self):
         """ Database teardown """
         pass
-    def test_import_data_should_increment_error_count_for_bad_product_file(self):
-        """ Tests the import_data function when bad product_file is given """
+
+    def test_populate_database(self):
+        """ Test the populate_database method"""
         # Given
-        directory_name = './'
-        product_file = 'does_not_exist.csv'
-        customer_file = 'also_dne.csv'
-        rentals_file = 'still_also_dne.csv'
+        data = [
+            {
+                "customer_id": "ID001",
+                "name": "Paul",
+                "last_name": "Atreides",
+                "address": "Arrakis",
+                "phone_number": "888-778-6668",
+                "email_address": "thewormisspice@dune.net",
+                "status": "True",
+                "credit_limit": "999999.99",
+            }
+        ]
+        mongo = db.MongoDBConnection()
+        with mongo:
+            test_db = mongo.connection.test_db
 
-        # When 
+            # When
+            (r_cnt, e_cnt) = db.populate_database(test_db, "customers", data)
 
+            customers = test_db.customers.find()
+            cid = test_db.customers.customer_id.find()
+            LOGGER.debug(customers)
+            actual_id = customers[0]["customer_id"]
+            LOGGER.debug(actual_id)
+
+            # Then
+            self.assertEqual(1, r_cnt)
+            self.assertEqual(0, e_cnt)
+            self.assertIn("ID001", actual_id)
+
+            db._drop_collections(test_db)
+
+    def test_import_data(self):
+        """ Test the import_data method"""
+        # Given
+        product_file = 'products'
+        directory_name = './csv_files'
+        customer_file = 'customers'
+        rental_file = 'rentals'
+
+        # When
+        [records, errors] = db.import_data(directory_name, product_file, customer_file, rental_file)
+        
         # Then
-        # with self.assertRaises(IOError):
-        #     db.import_data(directory_name, product_file, customer_file, rentals_file)
+        self.assertEqual(0, errors[0])
+        self.assertEqual(0, errors[1])
+        self.assertEqual(0, errors[2])
+
+        self.assertEqual(4, records[0])
+        self.assertEqual(4, records[1])
+        self.assertEqual(2, records[2])
+# class CallsToDatabase(TestCase):
+#     """ Handles all tests involving calls to DB for data"""
+
+#     def setUp(self):
+#         """ Database Setup """
+#         pass
+
+#     def tearDown(self):
+#         """ Database teardown """
+#         pass
+
+#     def test_show_available_products(self):
+#         """ Test the show_available_product data from database """
+
+#         # Given
+
+#         # When
+
+#         # Then
