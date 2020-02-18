5bdd3135bcd52fcb72f8475a48704e04ca20ec0f
mattcasari <mjcasari@gmail.com>
2020-02-18 13:29:12 -0800

    Adding poor and good files, first commit, not developed

---
+++ students/mattcasari/lesson06/assignment/good_perf.py
@@ -0,0 +1,16 @@
+"""
+well performing, well written module
+"""
+
+import datetime
+import csv
+import timeit
+
+def analyze(filename):
+def main():
+    filename = "data/exercise.csv"
+    analyze(filename)
+
+
+if __name__ == "__main__":
+    main()
