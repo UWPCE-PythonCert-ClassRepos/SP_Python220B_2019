8daa9ffe0b2f410b5bde5fb6fa73839c56847d81
mattcasari <mjcasari@gmail.com>
2020-02-18 13:28:16 -0800

    create_data now linting clean

--- students/mattcasari/lesson06/assignment/create_data.py
+++ students/mattcasari/lesson06/assignment/create_data.py
@@ -1,11 +1,14 @@
-# import csv
-# import datetime
+""" Lesson 06 Data Generator """
+
+#pylint: disable=expression-not-assigned
+
 from datetime import datetime as dt
 from uuid import uuid4
 from random import randrange, choice
 
-DATETIME_MIN = dt.timestamp(dt(2010,1,1))
-DATETIME_MAX = dt.timestamp(dt(2020,12,31))
+DATETIME_MIN = dt.timestamp(dt(2010, 1, 1))
+DATETIME_MAX = dt.timestamp(dt(2020, 12, 31))
+
 
 def create_random_data():
     """
@@ -14,13 +17,16 @@ def create_random_data():
     Generator that creates a string of data in the form:
     GUID, idx, idx+1, idx+2, idx+3, MM/DD/YYYY, [ao or nothing]
     """
-    idx=1
+    idx = 1
     while True:
-        r_date = dt.fromtimestamp(randrange(DATETIME_MIN, DATETIME_MAX)).strftime("%m/%d/%Y")
+        r_date = dt.fromtimestamp(randrange(DATETIME_MIN, DATETIME_MAX)).strftime(
+            "%m/%d/%Y"
+        )
         data = f"{str(uuid4())},{idx},{idx+1},{idx+2},{idx+3},{r_date},{choice(['ao',''])}\n"
         yield data
         idx += 1
-        
+
+
 def main():
     """
     Main run function.
@@ -28,7 +34,7 @@ def main():
     Generates 1 million data lines in a csv titled "exercise.csv"
     """
     data_gen = create_random_data()
-    with open('exercise.csv', 'w+') as csv_file:
+    with open("exercise.csv", "w+") as csv_file:
         [csv_file.write(next(data_gen)) for x in range(1000000)]
 
 
