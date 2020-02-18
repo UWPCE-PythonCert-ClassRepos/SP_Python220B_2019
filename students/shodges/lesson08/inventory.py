26ef5939724548a5b0e71ff18f05d76d9c6923a0
Sean Hodges <sthodges@gmail.com>
2020-02-18 00:27:00 -0800

    Add skeleton structure for the closure

--- students/shodges/lesson08/inventory.py
+++ students/shodges/lesson08/inventory.py
@@ -3,6 +3,7 @@ Functionality to allow inventory management for HP Norton.
 """
 
 import csv
+from functools import partial
 
 def add_furniture(invoice_file, customer_name, item_code, item_description, item_monthly_price):
     """
@@ -15,4 +16,9 @@ def add_furniture(invoice_file, customer_name, item_code, item_description, item
         writer.writerow([customer_name, item_code, item_description, '{:.2f}'.format(item_monthly_price)])
 
 
-#def single_customer(customer_name, invoice_file):
+def single_customer(customer_name, invoice_file):
+    """
+    Returns a curried function that will iterate through a file defined by rental_items and add
+    them to the master inventory file, invoice_file, as rented to customer_name.
+    """
+    def import_from(rental_items):
