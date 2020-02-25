4d5971bf47773c31725eaa5a651bd8f0e35bc059
Sean Hodges <sthodges@gmail.com>
2020-02-25 13:41:29 -0800

    Refactor a bit so that the root dir is only included if .png's are found in it

--- students/shodges/lesson09/pngdiscover.py
+++ students/shodges/lesson09/pngdiscover.py
@@ -12,16 +12,23 @@ def png_discover(directory):
     ['/absolute/path', '/absolute/path/subdir', ['image1.png', 'image2.png'],
      '/absolute/path/subdir2', ['imageA.png'], 'imageZ.png']
     '''
+    png_this_dir = []
     png_files = []
     for file in Path(directory).iterdir():
         if file.is_dir():
-            dir_path = file.absolute().as_posix()
             child_files = png_discover(file.absolute().as_posix())
             if len(child_files) > 0:
-                png_files.append(dir_path)
-                png_files.append(child_files)
+                #If anything was found in child directories, add its dirname followed by the list
+                #of files
+                png_files.append(child_files[0])
+                png_files.append(child_files[1])
         elif file.name.endswith('.png'):
-            png_files.append(file.name)
+            png_this_dir.append(file.name)
+    if len(png_this_dir) > 0:
+        #If we found anything in this dir (excluding subdir's), append it and the dir name to
+        #png_files
+        png_files.insert(0, directory)
+        png_files.insert(1, png_this_dir)
     return png_files
 
 if __name__ == '__main__':
