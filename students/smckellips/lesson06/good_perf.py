ae38bd5f6aed52c72e995dad04eb0cf3c46fc7d3
Sean McKellips <sean.mckellips@gmail.com>
2020-03-08 21:23:08 -0700

    updates to code through Improvement 8

--- students/smckellips/lesson06/good_perf.py
+++ students/smckellips/lesson06/good_perf.py
@@ -1,6 +1,5 @@
 """
 poorly performing, poorly written module
-
 """
 
 import datetime
@@ -8,57 +7,51 @@ import csv
 
 def analyze(filename):
     start = datetime.datetime.now()
+    year_count = {
+        "2013": 0,
+        "2014": 0,
+        "2015": 0,
+        "2016": 0,
+        "2017": 0,
+        "2018": 0
+    }
     with open(filename) as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
         found = 0
-        new_ones = []
         for row in reader:
-            lrow = list(row)
             if "ao" in row[6]:
                 found += 1
-
-            if lrow[5] > '00/00/2012':
-                new_ones.append((lrow[5], lrow[0]))
-
-        year_count = {
-            "2013": 0,
-            "2014": 0,
-            "2015": 0,
-            "2016": 0,
-            "2017": 0,
-            "2018": 0
-        }
-
-        for new in new_ones:
-            if new[0][6:] == '2013':
+                
+            if row[5][6:] < '2013':
+                continue
+            elif row[5][6:] == '2013':
                 year_count["2013"] += 1
-            if new[0][6:] == '2014':
+            elif row[5][6:] == '2014':
                 year_count["2014"] += 1
-            if new[0][6:] == '2015':
+            elif row[5][6:] == '2015':
                 year_count["2015"] += 1
-            if new[0][6:] == '2016':
+            elif row[5][6:] == '2016':
                 year_count["2016"] += 1
-            if new[0][6:] == '2017':
+            elif row[5][6:] == '2017':
                 year_count["2017"] += 1
-            if new[0][6:] == '2018':
+            elif row[5][6:] == '2018':
                 year_count["2018"] += 1
 
-        print(year_count)
-    step1 = datetime.datetime.now()
+    print(year_count)
 
     print(f"'ao' was found {found} times")
     end = datetime.datetime.now()
 
-    print(f'delta1 is {step1 - start}')
-    print(f'delta2 is {end - step1}')
-    print(f'total is {end - start}')
     return (start, end, year_count, found)
 
 def main():
+    '''Main module launch function'''
     filename = "data/exercise.csv"
     analyze(filename)
 
 
 if __name__ == "__main__":
     main()
+    # filename = "data/exercise.csv"
+    # analyze(filename)
