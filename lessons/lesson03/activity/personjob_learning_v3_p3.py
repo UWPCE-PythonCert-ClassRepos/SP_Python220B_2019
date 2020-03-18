ba4f295696fc884e29582b6ce250c474a8aa1d8c
Clemente Fortuna <cjfortu@icloud.com>
2020-03-17 21:07:00 -0400

    my activity files

--- lessons/lesson03/activity/personjob_learning_v3_p3.py
+++ lessons/lesson03/activity/personjob_learning_v3_p3.py
@@ -3,43 +3,53 @@
     delete the database to start over
         (but running this program does not require it)
 
-    Person:
-
-        1. add a new record and delete it
-
-"""
-from personjob_modeli import *
-
+    Person:"""
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from personjob_model import *
 
-PERSON_NAME = 0
-LIVES_IN_TOWN = 1
-NICKNAME = 2
+database = SqliteDatabase('personjob.db')
 
-logger.info('Add and display a Person called Fred; then delete him...')
+def add_and_delete():
 
-logger.info('Add Fred in one step')
 
-new_person = Person.create(
-    person_name = 'Fred',
-    lives_in_town = 'Seattle',
-    nickname = 'Fearless')
-new_person.save()
 
-logger.info('Show Fred')
-aperson = Person.get(Person.person_name == 'Fred')
 
-logger.info(f'We just created {aperson.person_name}, who lives in {aperson.lives_in_town}')
-logger.info('but now we will delete him...')
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
-aperson.delete_instance()
+    database = SqliteDatabase('personjob.db')
+    try:
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        
+        logger.info('Add and display a Person called Fred; then delete him...')
+        logger.info('Add Fred in one step')
 
-logger.info('Reading and print all Person records (but not Fred; he has been deleted)...')
+        new_person = Person.create(
+            person_name = 'Fred',
+            lives_in_town = 'Seattle',
+            nickname = 'Fearless')
+        new_person.save()
 
-for person in Person:
-    logger.info(f'{person.person_name} lives in {person.lives_in_town} and likes to be known as {person.nickname}')
+        logger.info('Show Fred')
+        aperson = Person.get(Person.person_name == 'Fred')
 
-database.close()
+        logger.info(f'We just created {aperson.person_name}, who lives in {aperson.lives_in_town}')
+        logger.info('but now we will delete him...')
+
+        aperson.delete_instance()
+
+        logger.info('Reading and print all Person records (but not Fred; he has been deleted)...')
+
+        for person in Person:
+            logger.info(f'{person.person_name} lives in {person.lives_in_town} and likes to be known as {person.nickname}')
+
+    except Exception as e:
+        logger.info(e)
+
+    finally:
+        database.close()
+
+if __name__ == '__main__':
+    add_and_delete()
--- lessons/lesson03/activity/personjob_learning_v5_p7.py
+++ lessons/lesson03/activity/personjob_learning_v5_p7.py
@@ -3,42 +3,51 @@
     delete the database to start over
         (but running this program does not require it)
 
-
 """
-from personjob_modeli import *
+from personjob_model import *
 
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def cant_change_PK():
 
-logger.info("Back to Person class: try to change Peter's name")
 
-aperson = Person.get(Person.person_name == 'Peter')
-logger.info(f'Current value is {aperson.person_name}')
 
-logger.info('Update Peter to Peta, thereby trying to change the PK...')
 
-try:
-    with database.transaction():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    logger.info("Back to Person class: try to change Peter's name")
+
+    aperson = Person.get(Person.person_name == 'Peter')
+    logger.info(f'Current value is {aperson.person_name}')
+
+    logger.info('Update Peter to Peta, thereby trying to change the PK...')
+
+    try:
+        try:
+            with database.transaction():
+                aperson = Person.get(Person.person_name == 'Peter')
+                aperson.person_name = 'Peta'
+                aperson.save()
+                logger.info(f'Tried to change Peter to {aperson.person_name}')
+
+        except Exception as e:
+            logger.info(f'Cant change a PK and caught you trying') # not caught; no error thrown by Peewee
+            logger.info(e)
+
         aperson = Person.get(Person.person_name == 'Peter')
-        aperson.person_name = 'Peta'
-        aperson.save()
-        logger.info(f'Tried to change Peter to {aperson.person_name}')
+        logger.info(f'Looked for Peter: found! -> {aperson.person_name}')
 
-except Exception as e:
-    logger.info(f'Cant change a PK and caught you trying') # not caught; no error thrown by Peewee
-    logger.info(e)
+        try:
+            aperson = Person.get(Person.person_name == 'Peta')
 
-aperson = Person.get(Person.person_name == 'Peter')
-logger.info(f'Looked for Peter: found! -> {aperson.person_name}')
+        except Exception as e:
+            logger.info(f'Looking for Peta results in zero records. PK changes are ignored and do not throw an error!!!!')
+            logger.info(f'Cant change a PK')
+            logger.info('PK "change" can only be achieved with a delete and new create')
+    
+    finally:
+        database.close()
 
-try:
-    aperson = Person.get(Person.person_name == 'Peta')
-
-except Exception as e:
-    logger.info(f'Looking for Peta results in zero records. PK changes are ignored and do not throw an error!!!!')
-    logger.info(f'Cant change a PK')
-    logger.info('PK "change" can only be achieved with a delete and new create')
-
-database.close()
+if __name__ == '__main__':
+    cant_change_PK()
