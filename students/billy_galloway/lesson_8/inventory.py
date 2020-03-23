9cdf46762a4f828a09355b2623a0cfdfe3d5ba87
brgalloway <digital.locust@gmail.com>
2020-03-22 17:41:21 -0700

    updated single customer with an excpetion

--- students/billy_galloway/lesson_8/inventory.py
+++ students/billy_galloway/lesson_8/inventory.py
@@ -21,12 +21,12 @@ def add_furniture(invoice_file, customer_name,
         for row in csv_reader:
             if customer_name in list(row):
                 with open(invoice_file, 'a') as append_file:
-                    logger.info(f'customer {customer_name} found in file')
+                    logger.info(f'customer {customer_name} invoiced')
                     append_file.write(f'{customer_name},{item_code},{item_description},{item_monthly_cost}\n')
                     break
         else:
             with open(invoice_file, 'a') as append_file:
-                logger.info(f'customer {customer_name} not found in file')
+                logger.info(f'customer {customer_name} invoiced')
                 append_file.write(f'{customer_name},{item_code},{item_description},{item_monthly_cost}\n')
 
     # if invoice file is not found
@@ -38,24 +38,24 @@ def add_furniture(invoice_file, customer_name,
 
 def create_reader(csv_file):
     ''' creates csv reader object '''
-    print(f"trying to find {csv_file}")
+    logger.info(f"trying to find {csv_file}")
     if os.path.isfile(csv_file):
-        try:
-            return csv.reader(open(csv_file), delimiter=',')
-        except FileNotFoundError as e:
-            print(f'{e}')
+        logger.info(f'found file {csv_file}')
+        return csv.reader(open(csv_file), delimiter=',')
 
 
 def single_customer(customer_name, invoice_file):
     ''' outter function that takes a customer and csv file '''
     def invoice(rental_file):
         rental_reader = create_reader(rental_file)
-        for row in rental_reader:
-            add_furniture(invoice_file,customer_name,row[0],row[1],row[2])
-
+        try:
+            for row in rental_reader:
+                add_furniture(invoice_file,customer_name,row[0],row[1],row[2])
+        except TypeError:
+            logger.info(f'provided items file does not exist')
     return invoice
+
 
 create_invoice = single_customer("susan wong", "./lesson_8/data/invoice_file.csv")
 create_invoice("./lesson_8/data/test_items.csv")
 add_furniture('./lesson_8/data/invoice_file.csv', "Dan Wong", "prd001", "seat", 10.00)
-
