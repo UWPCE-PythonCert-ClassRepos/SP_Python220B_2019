b54b7add40960dd15731c8628167481ea1f23306
Benjamin Freeman <bfreema1@mail.sfsu.edu>
2020-04-13 15:30:33 -0700

    lesson5

---
+++ students/Ben_freeman/lesson05/database.py
@@ -0,0 +1,101 @@
+from pymongo import MongoClient
+import pandas as pd
+import logging
+import datetime
+
+"""logging setup"""
+LOG_FORMAT = '%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s'
+FORMATTER = logging.Formatter(LOG_FORMAT)
+
+LOG_FILE = datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setFormatter(FORMATTER)
+FILE_HANDLER.setLevel(logging.INFO)
+
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.INFO)
+CONSOLE_HANDLER.setFormatter(FORMATTER)
+
+LOG = logging.getLogger()
+if not LOG.hasHandlers():
+    LOG.addHandler(FILE_HANDLER)
+    LOG.addHandler(CONSOLE_HANDLER)
+
+
+class MongoDBConnection():
+    """MongoDB Connection"""
+
+    def __init__(self, host='127.0.0.1', port=27017):
+        """ be sure to use the ip address not name for local windows"""
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+def show_available_products():
+    mongo = MongoDBConnection()
+    with mongo:
+        db = mongo.connection.media
+        displayed_items = {}
+        for item in db["products"].find():
+            try:
+                if int(item["quantity_available"])>0:
+                    displayed_items[f"prd{len(displayed_items)+1}"] = item
+                    LOG.info(f"Displaying item {item}")
+            except ValueError:
+                LOG.info("Invalid quantity, use numbers in the future")
+        return displayed_items
+
+
+def show_rentals(product_id):
+    mongo = MongoDBConnection()
+    with mongo:
+        db = mongo.connection.media
+        displayed_items = {}
+        for rentals in db["rentals"].find({"product_id": f" {product_id}"}):
+            rental_people = rentals["customer_id"]
+            for people in db["customers"].find():
+                if people["customer_id"] in rental_people:
+                    displayed_items[f"user{len(displayed_items) + 1}"] = people
+                    no_doubles_counter = False
+                elif rental_people in people["customer_id"] and no_doubles_counter:
+                    displayed_items[f"user{len(displayed_items) + 1}"] = people
+        return displayed_items
+
+
+
+def import_data(directory_name, product_file, customer_file, rentals_file):
+    mongo = MongoDBConnection()
+    with mongo:
+        db = mongo.connection.media
+        databases = (db["products"], db["customers"], db["rentals"])
+        answer = input("Would you like to clear the database?(yes/no): ")
+        if answer.lower() == "yes":
+            for item in databases:
+                item.drop()
+        files = (product_file, customer_file, rentals_file)
+        input_count = []
+        error_count = []
+        for file, data in zip(files, databases):
+            LOG.info(f"starting loading {str(file)}")
+            bad_count = 0
+            count = 0
+            try:
+                new_data = pd.read_csv(f"{directory_name}/{file}.csv")
+                data.insert_many(new_data.to_dict("records"))
+                count += 1
+                LOG.info(f"Database,{data} updated successfully")
+            except FileNotFoundError:
+                bad_count += 1
+                LOG.info(f"Failed to update {data}")
+            input_count.append(count)
+            error_count.append(bad_count)
+        return tuple(input_count), tuple(error_count)
---
+++ students/Ben_freeman/lesson05/test.py
@@ -0,0 +1,30 @@
+# pylint: disable=unused-import, unused-wildcard-import,
+# pylint: disable=too-few-public-methods, too-many-arguments, wildcard-import
+# pylint: disable=logging-format-interpolation, pointless-string-statement
+import sys
+import unittest
+import database as db
+from unittest.mock import patch
+
+
+class TestFunctions(unittest.TestCase):
+
+    def setUP(self):
+        with patch("builtins.input", side_effect="yes"):
+            db.import_data("data_files", "products", "customers", "rentals")
+
+    def test_import_data(self):
+        with patch("builtins.input", side_effect="yes"):
+            a = db.import_data("data_files","products","customers","rentals")
+            self.assertEqual(a, ((1, 1, 1), (0, 0, 0)))
+            b = db.import_data("Willy", "wonka", "and", "the choccolate factory")
+            self.assertEqual(b, ((0, 0, 0), (1, 1, 1)))
+
+    def test_show_available_products(self):
+        a = db.show_available_products()
+        self.assertEqual(a["prd1"][" Description"], ' 60-inch TV stand')
+
+    def test_show_rentals(self):
+        a = db.show_rentals("prd001")
+        b = " Maya Data"
+        self.assertEqual(a["user1"][" Name"], b)
