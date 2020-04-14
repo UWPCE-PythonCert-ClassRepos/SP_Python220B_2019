34f1a0c96a21bddd3f560bb2f26964f381c84662
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-04-14 14:49:12 -0400

    fixing pylint issues

--- students/eric_grandeo/lesson02/charges_calc.py
+++ students/eric_grandeo/lesson02/charges_calc.py
@@ -1,5 +1,5 @@
 '''
-Returns total price paid for individual rentals 
+Returns total price paid for individual rentals
 '''
 import argparse
 import json
@@ -8,35 +8,35 @@ import math
 import logging
 
 
-log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
-formatter = logging.Formatter(log_format)
+LOG_FORMAT = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+FORMATTER = logging.Formatter(LOG_FORMAT)
 
-log_file = datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
-file_handler = logging.FileHandler(log_file)
+LOG_FILE = datetime.datetime.now().strftime('%Y-%m-%d') + '.log'
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
 #file_handler.setLevel(logging.WARNING)
-file_handler.setFormatter(formatter)
+FILE_HANDLER.setFormatter(FORMATTER)
 
-console_handler = logging.StreamHandler()
+CONSOLE_HANDLER = logging.StreamHandler()
 #console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
+CONSOLE_HANDLER.setFormatter(FORMATTER)
 
-logger = logging.getLogger()
+LOGGER = logging.getLogger()
 #logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(CONSOLE_HANDLER)
 
 
 def parse_cmd_arguments():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--input', help='input JSON file', required=True)
     parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
-    parser.add_argument('-d', '--debug', help="enter desired debug level", required=False, default=0)
-    
+    parser.add_argument('-d', '--debug', help="enter desired debug level",
+                        required=False, default=0)
+
 
     return parser.parse_args()
 
-
-def load_rentals_file(filename):    
+def load_rentals_file(filename):
     try:
         with open(filename) as file:
             logging.debug('JSON file: {}'.format(filename))
@@ -51,32 +51,39 @@ def calculate_additional_fields(data):
     for value in data.values():
         try:
             logging.info("Called with value: {}".format(value))
+
+            #logs a warning if no rental end date is available
+            if value['rental_start'] == '':
+                logging.warning("Missing rental start date")
             rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
             #logs the rental start date
             logging.debug('Rental start: {}'.format(value['rental_start']))
+
             #logs a warning if no rental end date is available
             if value['rental_end'] == '':
                 logging.warning("Missing rental end date")
             rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
             #logs the rental end date
             logging.debug('Rental ends: {}'.format(value['rental_end']))
+
             value['total_days'] = (rental_end - rental_start).days
+            #logs total days
             logging.debug('Total days: {:d}'.format(value['total_days']))
-            if value['total_days'] < 0:
-                logging.warning("Total days are negative")
-            #logs a warning if total days is negative; end date is before start date
+
             value['total_price'] = value['total_days'] * value['price_per_day']
-            #logs the toal price
+            #logs the total price
             logging.debug('Total price: ${:,.2f}'.format(value['total_price']))
+
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             #logs square root of total price
             logging.debug('Square root of total price: {:06.2f}'.format(value['sqrt_total_price']))
+
             value['unit_cost'] = value['total_price'] / value['units_rented']
             logging.debug('Unit cost: ${:,.2f}'.format(value['unit_cost']))
         except ValueError:
             #logs an error if total days are negative, resulting in attempting to find square
             #root of a negative number
-            logging.error("Total days must not be negative")
+            logging.error("Total days must not be negative; end date is before start date")
 
     return data
 
@@ -87,24 +94,24 @@ def save_to_json(filename, data):
 if __name__ == "__main__":
     args = parse_cmd_arguments()
     if args.debug == '0':
-        logger.disabled = True
+        LOGGER.disabled = True
     elif args.debug == '1':
-        file_handler.setLevel(logging.ERROR)
-        console_handler.setLevel(logging.ERROR)
-        logger.setLevel(logging.ERROR)
-    elif args.debug == '2': 
-        file_handler.setLevel(logging.WARNING)
-        console_handler.setLevel(logging.WARNING)
-        logger.setLevel(logging.WARNING)
+        FILE_HANDLER.setLevel(logging.ERROR)
+        CONSOLE_HANDLER.setLevel(logging.ERROR)
+        LOGGER.setLevel(logging.ERROR)
+    elif args.debug == '2':
+        FILE_HANDLER.setLevel(logging.WARNING)
+        CONSOLE_HANDLER.setLevel(logging.WARNING)
+        LOGGER.setLevel(logging.WARNING)
     elif args.debug == '3':
         #From assignment: Debug: General comments, indicating where in the script flow we are.
         #Should be shown on screen only (i.e., never saved to logfile).
-        file_handler.setLevel(logging.WARNING)
-        console_handler.setLevel(logging.DEBUG)
-        logger.setLevel(logging.DEBUG)
-    
-            
+        FILE_HANDLER.setLevel(logging.WARNING)
+        CONSOLE_HANDLER.setLevel(logging.DEBUG)
+        LOGGER.setLevel(logging.DEBUG)
+
+
     data = load_rentals_file(args.input)
     data = calculate_additional_fields(data)
     save_to_json(args.output, data)
-
+    
