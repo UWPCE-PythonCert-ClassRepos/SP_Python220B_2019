2b74138cd695ffde5d6e04a76b8e9f3adb834671
Benjamin Freeman <bfreema1@mail.sfsu.edu>
2020-04-16 12:48:39 -0700

    lesson5

--- students/Ben_freeman/lesson05/test.py
+++ students/Ben_freeman/lesson05/test.py
@@ -8,23 +8,21 @@ from unittest.mock import patch
 
 
 class TestFunctions(unittest.TestCase):
-
-    def setUP(self):
-        with patch("builtins.input", side_effect="yes"):
-            db.import_data("data_files", "products", "customers", "rentals")
-
     def test_import_data(self):
-        with patch("builtins.input", side_effect="yes"):
+        with patch("builtins.input", side_effect=("yes", "yes")):
             a = db.import_data("data_files","products","customers","rentals")
             self.assertEqual(a, ((1, 1, 1), (0, 0, 0)))
             b = db.import_data("Willy", "wonka", "and", "the choccolate factory")
             self.assertEqual(b, ((0, 0, 0), (1, 1, 1)))
 
     def test_show_available_products(self):
+        with patch("builtins.input", side_effect=("yes")):
+            db.import_data("data_files", "products", "customers", "rentals")
         a = db.show_available_products()
         self.assertEqual(a["prd1"][" Description"], ' 60-inch TV stand')
 
     def test_show_rentals(self):
-        a = db.show_rentals("prd001")
+        with patch("builtins.input", side_effect=("yes")):
+            a = db.show_rentals("prd001")
         b = " Maya Data"
         self.assertEqual(a["user1"][" Name"], b)
