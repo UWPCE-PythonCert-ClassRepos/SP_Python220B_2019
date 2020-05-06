f7420745766bde33cfaa46d1e5bac84250d3a7c9
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-05-05 21:09:13 -0400

    fixed pylint errors

--- students/eric_grandeo/lesson03/basic_operations.py
+++ students/eric_grandeo/lesson03/basic_operations.py
@@ -1,86 +1,84 @@
 '''
-Basic opertions
-
+Basic opertions: add, search, and delete a customer. Update credit
+limit and provide total number of active customers
 '''
 
-from customers_model import *
-from peewee import *
+# pylint: disable=R0913
+# pylint: disable=W
+
 import logging
+from peewee import *
+from customers_model import *
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 #database = SqliteDatabase('customers.db')
 
 
 def add_customer(customer_id, name, lastname, home_address,
                  phone_number, email_address, status, credit_limit):
+    '''Add customer to DB'''
+
     try:
-        #database.connect()
-        #database.execute_sql('PRAGMA foreign_keys = On;')
         with database.transaction():
             new_customer = Customers.create(
-                customer_id = customer_id,
-                name = name,
-                lastname = lastname,
-                home_address = home_address,
-                phone_number = phone_number,
-                email_address = email_address,
-                status = status,
-                credit_limit = credit_limit
+                customer_id=customer_id,
+                name=name,
+                lastname=lastname,
+                home_address=home_address,
+                phone_number=phone_number,
+                email_address=email_address,
+                status=status,
+                credit_limit=credit_limit
             )
             new_customer.save()
-            logger.info('Database add successful')
-    except Exception as e:
-        logger.info('Error creating = {}'.format(name))
-        logger.info(e)
+            LOGGER.info('Database add successful')
+    except Exception as error:
+        LOGGER.info('Error creating = {}'.format(name))
+        LOGGER.info(error)
     finally:
         database.close()
-        logger.info('Database closed')
-        
+        LOGGER.info('Database closed')
 
 def search_customer(customer_id):
     '''Insert docstring'''
     try:
         customer = Customers.get(Customers.customer_id == customer_id)
-        #name, lastname, email address and phone number
-        logger.info('Customer found')
+        LOGGER.info('Customer found')
         return {'name':customer.name, 'lastname':customer.lastname,
                 'email_address':customer.email_address, 'phone_number':customer.phone_number}
-    except DoesNotExist as e:
-        logger.info(e)
-        return {}   
+    except DoesNotExist as error:
+        LOGGER.info(error)
+        return {}
 
 def delete_customer(customer_id):
     '''Insert docstring'''
     try:
         customer = Customers.get(Customers.customer_id == customer_id)
         customer.delete_instance()
-        logger.info('Customer deleted')
+        LOGGER.info('Customer deleted')
     except:
-        logger.info("Customer not found for delete")
+        LOGGER.info("Customer not found for delete")
         raise ValueError('Customer not found')
-    
 
 def update_customer_credit(customer_id, credit_limit):
+    '''Updates the customer credit limit with a new number'''
     try:
         customer = Customers.get(Customers.customer_id == customer_id)
         customer.credit_limit = credit_limit
         customer.save()
-        logger.info('Customer credit limit updated')
+        LOGGER.info('Customer credit limit updated')
     except:
-        logger.info("Customer not found to update credit limit")
+        LOGGER.info("Customer not found to update credit limit")
         raise ValueError('Customer not found')
-    
 
 def list_active_customers():
-    query = Customers.select().where(Customers.status == True).count()
-    logger.info("Number of active users: {}".format(query))
+    '''Count of all customers where active == True'''
+    query = Customers.select().where(Customers.status).count()
+    LOGGER.info("Number of active users: {}".format(query))
     return query
 
 if __name__ == "__main__":
     database.create_tables([Customers])
     database.close()
-
-
-
