23ba319187f6314f5530305800e2eac371f7a3e5
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-05-06 09:28:45 -0400

    fixed pylint errors

--- students/eric_grandeo/lesson03/unit_test.py
+++ students/eric_grandeo/lesson03/unit_test.py
@@ -2,37 +2,31 @@
 Unit tests
 '''
 
-'''
-import sys
-# Add path to files
-sys.path.append('/home/ejgrandeo/uwpython/SP_Python220B_2019/students/eric_grandeo/lesson03')
-'''
+# pylint: disable=W
 
-
+import logging
 from unittest import TestCase
+import peewee
 from customers_model import *
 from basic_operations import *
-import peewee
-import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 class TestBasicOperations(TestCase):
-    '''insert docstring'''    
-    
+    '''Test all Database functions'''
     def setUp(self):
-        '''insert docstring''' 
+        '''Create the tables'''
         database.create_tables([Customers])
-        logger.info('Create table successful')
-        
+        LOGGER.info('Create table successful')
+
     def tearDown(self):
-        '''insert docstring''' 
+        '''Delete the tables'''
         database.drop_tables([Customers])
-        logger.info('Database tables dropped')
-    
+        LOGGER.info('Database tables dropped')
+
     def test_add_customer(self):
-        '''insert docstring''' 
+        '''test add a customer to the database'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -45,11 +39,11 @@ class TestBasicOperations(TestCase):
         }
         add_customer(**test_customer)
         record = Customers.get(Customers.customer_id == test_customer['customer_id'])
-        logger.info("New customer: {}".format(record.name))
+        LOGGER.info("New customer: {}".format(record.name))
         self.assertEqual(record.customer_id, test_customer['customer_id'])
-        
+
     def test_search_customer(self):
-        '''insert docstring''' 
+        '''test searching for a customer by ID'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -60,16 +54,17 @@ class TestBasicOperations(TestCase):
             'status': True,
             'credit_limit': 25000
         }
-        
+
         add_customer(**test_customer)
         customer_record = {'name':'Eric Grandeo', 'lastname':'Grandeo',
-            'email_address':'email@email.com', 'phone_number':'1-212-555-1234'}
+                           'email_address':'email@email.com',
+                           'phone_number':'1-212-555-1234'}
         result = search_customer('12345')
         self.assertEqual(result, customer_record)
-     
-        
+
+
     def test_search_customer_fail(self):
-        '''insert docstring''' 
+        '''test a failed search, user does not exist'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -80,15 +75,14 @@ class TestBasicOperations(TestCase):
             'status': True,
             'credit_limit': 25000
         }
-        
+
         add_customer(**test_customer)
         fail_customer = {}
         result = search_customer('12346')
-        self.assertEqual(result, fail_customer) 
-        
-        
+        self.assertEqual(result, fail_customer)
+
     def test_delete_customer(self):
-        '''insert docstring''' 
+        '''test deleting a customer'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -99,19 +93,16 @@ class TestBasicOperations(TestCase):
             'status': True,
             'credit_limit': 25000
         }
-        
         add_customer(**test_customer)
         self.assertEqual(delete_customer(test_customer['customer_id']), None)
-    
-    
+
     def test_delete_customer_fail(self):
-        '''insert docstring''' 
+        '''test deleting a customer that does not exist'''
         with self.assertRaises(ValueError):
             delete_customer('2468')
-     
-             
+
     def test_update_customer_credit(self):
-        '''insert docstring'''
+        '''test updating a users credit limit by id'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -125,12 +116,12 @@ class TestBasicOperations(TestCase):
         add_customer(**test_customer)
         update_customer_credit(test_customer['customer_id'], 100000)
         get_customer = Customers.get(Customers.customer_id == test_customer['customer_id'])
-        logger.info("New credit limit: {}".format(get_customer.credit_limit))
+        LOGGER.info("New credit limit: {}".format(get_customer.credit_limit))
         self.assertEqual(get_customer.credit_limit, 100000)
 
 
     def test_update_customer_credit_fail(self):
-        '''insert docstring'''
+        '''test updating the credit limit of a user that does not exist'''
         test_customer = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -144,10 +135,9 @@ class TestBasicOperations(TestCase):
         add_customer(**test_customer)
         with self.assertRaises(ValueError):
             update_customer_credit('2468', 100000)
-        
-        
+
     def test_list_active_customers(self):
-        '''insert docstring'''
+        '''test getting the number of active customers where active == True'''
         test_customer_1 = {
             'customer_id': '12345',
             'name': 'Eric Grandeo',
@@ -182,6 +172,3 @@ class TestBasicOperations(TestCase):
         add_customer(**test_customer_2)
         add_customer(**test_customer_3)
         self.assertEqual(list_active_customers(), 2)
-        
-#This function will return an integer with the number
-#  of customers whose status is currently active        
