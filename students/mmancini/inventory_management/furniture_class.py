ec7b00feb4f616e8d7cad72c393fefb31ac1c9b6
mmanc125uw <mmanc125@uw.edu>
2020-05-15 14:39:42 -0400

    sp_py220 lesson01, pylint config, initial refactor

--- students/mmancini/inventory_management/furniture_class.py
+++ students/mmancini/inventory_management/furniture_class.py
@@ -1,21 +1,28 @@
-# Furniture class
-from inventoryClass import inventory
+"""
+Furniture
+"""
 
-class furniture(inventory):
+# .pylintrc disable = duplicate-code, too-many-arguments, too-few-public-methods
 
-    def __init__(self, productCode, description, marketPrice, rentalPrice, material, size):
-        inventory.__init__(self,productCode,description,marketPrice,rentalPrice) # Creates common instance variables from the parent class
+from inventory_management.inventory_class import Inventory
+
+class Furniture(Inventory):
+    '''the furniture class'''
+    def __init__(self, product_code, description, market_price, rental_price, material, size):
+        Inventory.__init__(self, product_code, description, market_price, rental_price)
+        # Creates common instance variables from the parent class
 
         self.material = material
         self.size = size
 
-    def returnAsDictionary(self):
-        outputDict = {}
-        outputDict['productCode'] = self.productCode
-        outputDict['description'] = self.description
-        outputDict['marketPrice'] = self.marketPrice
-        outputDict['rentalPrice'] = self.rentalPrice
-        outputDict['material'] = self.material
-        outputDict['size'] = self.size
+    def return_as_dictionary(self):
+        '''returns the class inventory as a dict'''
+        output_dict = {}
+        output_dict['product_code'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['market_price'] = self.market_price
+        output_dict['rental_price'] = self.rental_price
+        output_dict['material'] = self.material
+        output_dict['size'] = self.size
 
-        return outputDict
+        return output_dict
--- students/mmancini/inventory_management/inventory_class.py
+++ students/mmancini/inventory_management/inventory_class.py
@@ -1,17 +1,24 @@
-# Inventory class
-class inventory:
+"""
+Inventory
+"""
 
-    def __init__(self, productCode, description, marketPrice, rentalPrice):
-        self.productCode = productCode
+# pylint: disable=too-few-public-methods
+# pylint: disable=too-many-arguments
+
+class Inventory:
+    '''inventory class'''
+    def __init__(self, product_code, description, market_price, rental_price):
+        self.product_code = product_code
         self.description = description
-        self.marketPrice = marketPrice
-        self.rentalPrice = rentalPrice
+        self.market_price = market_price
+        self.rental_price = rental_price
 
-    def returnAsDictionary(self):
-        outputDict = {}
-        outputDict['productCode'] = self.productCode
-        outputDict['description'] = self.description
-        outputDict['marketPrice'] = self.marketPrice
-        outputDict['rentalPrice'] = self.rentalPrice
+    def return_as_dictionary(self):
+        '''returns the class inventory as a dict'''
+        output_dict = {}
+        output_dict['product_code'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['market_price'] = self.market_price
+        output_dict['rental_price'] = self.rental_price
 
-        return outputDict
+        return output_dict
