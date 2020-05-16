4833de3e0b37b3dd644c3225dabd4c14f2f2c68c
mmanc125uw <mmanc125@uw.edu>
2020-05-16 07:43:47 -0400

    sp_py220 lesson01 using mock

--- students/mmancini/test_unit.py
+++ students/mmancini/test_unit.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 from unittest.mock import patch
 from unittest.mock import MagicMock
 
+import inventory_management.market_prices as market_prices
 import inventory_management.main as main
 
 
@@ -16,6 +17,8 @@ class MainTests(TestCase):
 
     def setUp(self):
         """ setup """
+        self.main = main
+        self.mp = market_prices
 		
 		
     def test_main_menu(self):
@@ -23,7 +26,23 @@ class MainTests(TestCase):
 
         with patch('builtins.input', side_effect='1'):
             self.assertEqual(main.main_menu(), main.add_new_item)
-        
+
+        with patch('builtins.input', side_effect='2'):
+            self.assertEqual(main.main_menu(), main.item_info)
+            
         with patch('builtins.input', side_effect='q'):
             self.assertEqual(main.main_menu(), main.exit_program)
 
+            
+    def test_get_price_1(self):
+        """Test get_price"""
+
+        self.assertEqual(main.get_price('test'), 24)
+
+
+    def test_get_price_2(self):
+        """ Test get_price mocked return """
+
+        self.mp.get_latest_price = MagicMock(return_value=50)
+        self.assertEqual(main.get_price('test'), 50)
+    
