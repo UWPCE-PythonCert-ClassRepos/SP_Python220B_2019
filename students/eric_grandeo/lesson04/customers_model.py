81f7ffc2d8bfa4f96d51752a27b9b653200056b0
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-05-18 08:04:57 -0400

    moving files from previous lesson

---
+++ students/eric_grandeo/lesson04/customers_model.py
@@ -0,0 +1,25 @@
+"""
+Create Customers database
+"""
+
+from peewee import *
+
+database = SqliteDatabase('customers.db')
+database.connect()
+database.execute_sql('PRAGMA foreign_keys = ON;')
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Customers(BaseModel):
+    customer_id = CharField(primary_key = True, max_length = 5)
+    name = CharField(max_length = 30)
+    lastname = CharField(max_length = 30)
+    home_address = CharField(max_length = 50)
+    phone_number = CharField(max_length = 10)
+    email_address = CharField(max_length = 30)
+    status = BooleanField()
+    credit_limit = DecimalField(max_digits = 7, decimal_places = 2)
+
