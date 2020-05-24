864ae09e7794c3b0267f03014f27ffc3b6020019
mmanc125uw <mmanc125@uw.edu>
2020-05-24 18:09:05 -0400

    sp_py220 lesson2, initial logging

--- students/mmancini/lesson02/charges_calc.py
+++ students/mmancini/lesson02/charges_calc.py
@@ -6,12 +6,53 @@ import json
 import datetime
 import math
 import traceback
+import logging
+import sys
+
+
+def init_logger(level):
+    """ init logger """
+    log_format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+    log_file = datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
+
+    level_opts = {0: logging.CRITICAL, 1: logging.ERROR,
+                  2: logging.WARNING, 3: logging.DEBUG}
+
+    try:
+        log_level = level_opts.get(int(level))
+    except KeyError:
+        print('Debug level not valid.')
+        log_level = logging.CRITICAL
+
+    # Create a formatter using the format string.
+    formatter = logging.Formatter(log_format)
+
+    # Create a log message handler that sends output to a time-stamped log file.
+    file_handler = logging.FileHandler(log_file)
+    # Sets the level of log messages to be displayed in the file.
+    file_handler.setLevel(logging.WARNING)
+    # Sets the formatter for this handler to the formatter created above.
+    file_handler.setFormatter(formatter)
+
+    # Create a console log message handler.
+    console_handler = logging.StreamHandler()
+    # Set the level of messages to be displayed in the console window.
+    console_handler.setLevel(log_level)
+    # Set the formatter for the handler to the formatter created above.
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 def parse_cmd_arguments():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--input', help='input JSON file', required=True)
     parser.add_argument('-o', '--output', help='ouput JSON file', required=True)
+    parser.add_argument('-d', '--debug', help='specify debugger level [0-3]',
+                        required=False, default='0')
 
     return parser.parse_args()
 
@@ -53,7 +94,19 @@ def save_to_json(filename, data):
         json.dump(data, file)
 
 if __name__ == "__main__":
+
     args = parse_cmd_arguments()
+    init_logger(args.debug)
+
+
+    logging.debug("test the logger")
+    logging.critical("test the logger")
+    logging.error("test the logger")
+    logging.warning("test the logger")
+    sys.exit()
+
+
+
     data = load_rentals_file(args.input)
     data = calculate_additional_fields(data)
     save_to_json(args.output, data)
