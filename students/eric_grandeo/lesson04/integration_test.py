953f737f7b3eda20810bcdf86540e2fba6cfc933
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-05-25 22:19:49 -0400

    updated integration test

--- students/eric_grandeo/lesson04/integration_test.py
+++ students/eric_grandeo/lesson04/integration_test.py
@@ -28,8 +28,8 @@ class TestIntegration(TestCase):
 
     def test_integration(self):
         '''add a customer, delete customer, count number of active'''
-        test_customer_1 = {
-            'customer_id': '12345',
+        test_customers = [{
+            'customer_id': '54321',
             'name': 'Eric Grandeo',
             'lastname': 'Grandeo',
             'home_address': '123 Fake Street',
@@ -37,30 +37,28 @@ class TestIntegration(TestCase):
             'email_address': 'email@email.com',
             'status': True,
             'credit_limit': 25000
-        }
-        test_customer_2 = {
-            'customer_id': '45678',
-            'name': 'Jack Grandeo',
-            'lastname': 'Grandeo',
+        },
+        {
+            'customer_id': '98765',
+            'name': 'Jack Charles',
+            'lastname': 'Charles',
             'home_address': '123 Fake Street',
             'phone_number': '1-212-555-1234',
             'email_address': 'email@email.com',
             'status': False,
             'credit_limit': 25000
         }
-        test_customer_3 = {
-            'customer_id': '54321',
-            'name': 'Vivie Grandeo',
-            'lastname': 'Grandeo',
+        ,{
+            'customer_id': '10846',
+            'name': 'Vivie Harper',
+            'lastname': 'Harper',
             'home_address': '123 Fake Street',
             'phone_number': '1-212-555-1234',
             'email_address': 'email@email.com',
             'status': True,
             'credit_limit': 25000
-        }
-        add_customer(**test_customer_1)
-        add_customer(**test_customer_2)
-        add_customer(**test_customer_3)
-        delete_customer(test_customer_1['customer_id'])
+        }]
+        add_multiple_customers(test_customers)
+        delete_customer(test_customers[0]['customer_id'])
         self.assertEqual(list_active_customers(), 1)
-        
+    
