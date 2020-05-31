f28e112e35c2d6ea5c2996473fb57e153fc1b8e2
matt.gummel <matt.gummel@sap.com>
2020-05-31 12:51:11 -0700

    Finished Lesson03 tests

--- students/mgummel/lesson03/assignment/text.py
+++ students/mgummel/lesson03/assignment/text.py
@@ -2,7 +2,7 @@ delete_customer('12345')
 delete_customer('123456')
 delete_customer('1234s1234564564332486736')
 
-add_customer('12345', 'Max2', 'Tucker2', '40312 22nd ave SW', '2062-519-2405', 'mgummel225@gmail.com', True, 45600)
+
 add_customer('123456', 'Max', 'Tucker', '4031 22nd ave SW', '206-519-2405', 'mgummel225@gmail.com', False, 45600)
 add_customer('1234s1234564564332486736', 'Max', 'Tucker', '4031324 22nd ave SW', '206-519-2405', 'mgummel225@gmail.com',
              True, 45600)
