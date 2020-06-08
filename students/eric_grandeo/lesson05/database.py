b45b897bd50e65701bd87e67064ba0e99e63bb43
Eric74nyc <10297305+Eric74nyc@users.noreply.github.com>
2020-06-07 21:54:25 -0400

    fixed pylint errors

--- students/eric_grandeo/lesson05/database.py
+++ students/eric_grandeo/lesson05/database.py
@@ -2,11 +2,18 @@
 Create Mongo Database
 """
 
+# pylint: disable=R0914
+# pylint: disable=C0301
+# pylint: disable=C0103
+# pylint: disable=W
+
+
 import logging
-from pymongo import MongoClient
-from pymongo import errors
 import csv
 import os
+from pymongo import MongoClient
+from pymongo import errors
+
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
@@ -39,59 +46,58 @@ def import_data(directory_name, product_file, customer_file, rentals_file):
        added (in that order), the second with a count of any errors
        that occurred, in the same order."""
     mongo = MongoDBConnection()
-    
+
     try:
         with mongo:
             db = mongo.connection.hp_norton
-            
             products = db["products"]
             customers = db["customers"]
             rentals = db["rentals"]
-            
+
             error_dict = {product_file: 0, customer_file: 0, rentals_file: 0}
-            
+
             coll_list = [products, customers, rentals]
             file_list = [product_file, customer_file, rentals_file]
-            #error_list = [prod_errors, cust_errors, rent_errors]
-            
+
+
             merged_list = tuple(zip(coll_list, file_list))
 
             for item in merged_list:
                 with open(os.path.join(directory_name, item[1])) as file:
                     result = item[0].insert_many(csv.DictReader(file))
                     for doc in item[0].find():
-                        LOGGER.info("Added record: {} to collection {}".format(doc, item[0]))   
+                        LOGGER.info("Added record: {} to collection {}".format(doc, item[0]))
 
-    except errors.PyMongoError as error:    
+    except errors.PyMongoError as error:
         LOGGER.error("Error creating record: {}". format(error))
         error_dict[item[1]] += 1
 
     prod_num = products.find().count()
     cust_num = customers.find().count()
     rent_num = rentals.find().count()
-        
-    return ((prod_num, cust_num, rent_num), (error_dict[product_file], error_dict[customer_file], error_dict[rentals_file]))
-    
-        
+
+    return ((prod_num, cust_num, rent_num),
+            (error_dict[product_file], error_dict[customer_file], error_dict[rentals_file]))
+
+
 def show_available_products():
     """Returns a Python dictionary of products listed as available"""
     mongo = MongoDBConnection()
     avail_dict = {}
-    
+
     with mongo:
         db = mongo.connection.hp_norton
         products = db["products"]
-        
+
         myquery = {"quantity_available": {"$gt": "0"}}
-        
+
         mydoc = products.find(myquery)
-        
-        for x in mydoc:
-            LOGGER.info("Available Product: {}".format(x))
-            avail_dict[x["product_id"]] = {k: v for k,v in x.items() if k not in  ("_id","product_id")}
+
+        for prod in mydoc:
+            LOGGER.info("Available Product: {}".format(prod))
+            avail_dict[prod["product_id"]] = {k: v for k, v in prod.items() if k not in ("_id", "product_id")}
     LOGGER.info("All Available Prods: {}".format(avail_dict))
-    return avail_dict        
-        
+    return avail_dict
 
 
 def show_rentals(product_id):
@@ -99,16 +105,15 @@ def show_rentals(product_id):
        from users that have rented products matching product_id"""
     mongo = MongoDBConnection()
     result_dict = {}
-    
+
     with mongo:
         db = mongo.connection.hp_norton
         rentals = db["rentals"]
         customers = db["customers"]
-        
+
         for doc in rentals.find({"product_id": product_id}):
             for customer in customers.find({"user_id": doc["user_id"]}):
                 result_dict[customer["user_id"]] = {k: v for k, v in customer.items() if k not in ("_id", "user_id")}
                 LOGGER.info("Product id: {}, Renter: {}".format(product_id, customer))
-                
-    return result_dict        
-    
+
+    return result_dict
