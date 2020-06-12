f99e07978e8b89ae249c13913562dfa987d70ad5
matt.gummel <matt.gummel@sap.com>
2020-06-12 15:29:02 -0700

    finished jpgdiscovery

--- students/mgummel/lesson09/assignment/jpgdiscover.py
+++ students/mgummel/lesson09/assignment/jpgdiscover.py
@@ -1,25 +1,58 @@
-from pathlib import Path
+"""Module jpgdiscover.py to discover all png files"""
+
+from os.path import exists, join, abspath, isdir
 from os import listdir
+from pathlib import Path
 
-file_list = list()
+def get_dir():
+    """
+    Get user input for the directory to use for searching directories
+    for png files
+    :return: Absolute path of the directory the user chooses
+    """
+    dir_path = input("Enter parent directory to search from\n>>>")
+    while not exists(dir_path):
+        dir_path = input("Not valid path, please try again\n>>>")
+    full_path = abspath(dir_path)
+    return full_path
 
-def get_dir(default_path="./data"):
-    directory_path = input("Enter parent directory to search from\n>>>")
-    if directory_path:
-        return directory_path
-    return default_path
+def dir_recursion(path, items, stack_level=0):
+    """
+    Searches all directories recursively for an .png files ands them to
+    a list right after their respective file path.
 
-def get_pictures(working_directory):
-    test = listdir(working_directory)
-    print(test)
+    :param path: current path to search
+    :param items: the full_list of items to capture through a call stack
+    :param stack_level: integer that determines location in the call stack
+    :return: list of full paths followed by png files in that directory
+    """
+    full_list = items
+    photo_list = list()
 
-    """d = '.'
-    e = [os.path.join(d, o) for o in os.listdir(d)
-     if os.path.isdir(os.path.join(d, o))]"""
+    subitems = listdir(path)
+
+    for item in subitems:
+        full_path = join(path, item)
+        if isdir(full_path):
+            stack_level += 1
+            dir_recursion(full_path, full_list, stack_level)
+            stack_level -= 1
+            full_list.insert(0, full_path)
+
+        else:
+            if Path(item).suffix == '.png':
+                print(item)
+                photo_list.append(item)
+
+    # Needed to prevent empty list inserted at the beginning
+    if stack_level > 0:
+        full_list.insert(0, photo_list)
+
+    return full_list
 
 
 
 if __name__ == '__main__':
-    path = get_dir()
-    #list_recursively([2,23,33,2,23,40])
-    get_pictures(path)
+    directory = get_dir()
+    png_files = dir_recursion(directory, [])
+    print(f"List of PNG's: {png_files}")
