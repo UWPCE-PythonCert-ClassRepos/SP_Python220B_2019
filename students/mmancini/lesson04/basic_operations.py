5ec8f23f832a493c351c3da124ade5a117dbaa57
mmanc125uw <mmanc125@uw.edu>
2020-06-16 07:44:52 -0400

    sp_py220 lesson4 iteration, pythonic refact

---
+++ students/mmancini/lesson04/basic_operations.py
@@ -0,0 +1,143 @@
+'''
+    Basic operations for Customers Database
+'''
+
+# pylint: disable=too-few-public-methods
+# pylint: disable=unused-wildcard-import
+# pylint: disable=wildcard-import
+# pylint: disable=unused-import
+# pylint: disable=invalid-name
+# pylint: disable=unused-argument
+# pylint: disable=too-many-arguments
+# pylint: disable=unnecessary-pass
+# pylint: disable=no-self-use
+
+
+import logging
+from customers_model import *
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+LOGGER.info('Logger initialized')
+
+CUSTOMER_ID = 0
+FIRST_NAME = 1
+LAST_NAME = 2
+HOME_ADDRESS = 3
+PHONE_NUMBER = 4
+EMAIL_ADDRESS = 5
+ACTIVITY_STATUS = 6
+CREDIT_LIMIT = 7
+
+database.init('customers.db')
+LOGGER.info('customers db database initialized')
+database.create_tables([Customer])
+LOGGER.info('Customer tables created in database')
+
+
+def add_customer(customer_id, first_name, last_name, home_address, phone_number,
+                 email_address, activity_status, credit_limit):
+    '''adds new customer to the database'''
+    pass
+    customer = (customer_id, first_name, last_name, home_address, phone_number,
+                email_address, activity_status, credit_limit)
+    try:
+        with database.transaction():
+            new_customer = Customer.create(
+                customer_id=customer[CUSTOMER_ID],
+                first_name=customer[FIRST_NAME],
+                last_name=customer[LAST_NAME],
+                home_address=customer[HOME_ADDRESS],
+                phone_number=customer[PHONE_NUMBER],
+                email_address=customer[EMAIL_ADDRESS],
+                activity_status=customer[ACTIVITY_STATUS],
+                credit_limit=customer[CREDIT_LIMIT])
+            new_customer.save()
+            LOGGER.info('added customer %s %s to db', first_name, last_name)
+    except IntegrityError as exc:
+        LOGGER.error(exc)
+        LOGGER.info('Was not able to add customer to the database')
+        raise IntegrityError
+
+def search_customer(customers_id):
+    '''
+        search customer in db
+        in: customer id
+        out: dict of fount customer
+    '''
+    pass
+    customer_found = "no"
+
+    try:
+        customer = Customer.get(Customer.customer_id == customers_id)
+        customer_dict = {'first_name': customer.first_name,
+                         'last_name': customer.last_name,
+                         'email_address': customer.email_address,
+                         'phone_number': customer.phone_number}
+        customer_found = "yes"
+    except DoesNotExist:
+        raise ValueError('Customer id is not in the database')
+
+    LOGGER.info('search of customer found == %s', customer_found)
+    return customer_dict
+
+def delete_customer(customers_id):
+    '''deletes customer from database'''
+    pass
+    try:
+        customer = Customer.get(Customer.customer_id == customers_id)
+        customer.delete_instance()
+        LOGGER.info('deleted customer %s %s from the database',
+                    customer.first_name, customer.last_name)
+
+    except DoesNotExist:
+        raise ValueError('Customer not deleted id is not found in the database')
+
+def update_customer_credit(customers_id, new_credit_limit):
+    '''
+        update customer in db
+    '''
+    pass
+    try:
+        customer = Customer.get(Customer.customer_id == customers_id)
+        current_credit_limit = customer.credit_limit
+        customer.credit_limit = new_credit_limit
+        customer.save()
+        LOGGER.info('updated customer %s %s credit limit from %s to %s',
+                    customer.first_name, customer.last_name,
+                    current_credit_limit, customer.credit_limit)
+    except DoesNotExist:
+        raise ValueError('update failed, customer id is not found in the database')
+
+def list_active_customers():
+    '''
+        list active customers
+        out:  number of active customers
+    '''
+    pass
+    active_count = 0
+#    for customer in Customer.select():
+#        if customer.activity_status is True:
+#            active_count += 1
+    # refactor for Pythonic
+    active_count = len([customer.customer_id for customer in Customer.select().where(Customer.activity_status == 'Active')])
+    LOGGER.info('Returning %s active customers', active_count)
+    return active_count
+
+
+def show_customers():
+    '''
+        show list of customers via more pythonic comprehensions
+    '''
+    LOGGER.info('Show names of all the customers in the database.')
+    gener = ((f'{customer.first_name}'+' '+f'{customer.last_name}') for customer in Customer.select())
+    cust_list = []
+    for name in gener:
+        cust_list.append(name)
+        LOGGER.info(name)
+    return cust_list
+    
+def close_database():
+    '''Closes the customers.db database'''
+    database.close()
+    LOGGER.info('database closed')
---
+++ students/mmancini/lesson04/test_basic_operations.py
@@ -0,0 +1,148 @@
+'''
+Test basic_operations
+'''
+
+# pylint: disable=too-few-public-methods
+# pylint: disable=unused-wildcard-import
+# pylint: disable=wildcard-import
+# pylint: disable=unused-import
+# pylint: disable=invalid-name
+# pylint: disable=unused-argument
+# pylint: disable=too-many-arguments
+# pylint: disable=unnecessary-pass
+# pylint: disable=no-self-use
+
+
+import logging
+from unittest import TestCase
+from peewee import *
+from customers_model import *
+from basic_operations import add_customer, search_customer, delete_customer
+from basic_operations import update_customer_credit, list_active_customers, show_customers
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+LOGGER.info('Logger initialized')
+
+LOGGER.info('creating test db')
+database.init('test.db')
+
+class SuiteOfTests(TestCase):
+    '''testing basic operation'''
+    customer_111 = ('111', 'John', 'Smith', '111 Main St', 1112223333,
+                    'johnsmith@gmail.com', True, 1500)
+    customer_222 = ('222', 'Mary', 'Jones', '222 Central Ave', 444-555-6666,
+                    'maryjones@gmail.com', True, 2500)
+
+    def setUp(self):
+        '''sets up the database'''
+
+        database.drop_tables([Customer])
+        database.create_tables([Customer])
+
+        LOGGER.info('test setup complete')
+
+    def test_add_customer(self):
+        '''test add customer'''
+        pass
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+        customer = Customer.get(Customer.customer_id == self.customer_111[0])
+        self.assertEqual(customer.customer_id, self.customer_111[0])
+        self.assertEqual(customer.first_name, self.customer_111[1])
+        self.assertEqual(customer.last_name, self.customer_111[2])
+        self.assertEqual(customer.home_address, self.customer_111[3])
+        self.assertEqual(customer.phone_number, self.customer_111[4])
+        self.assertEqual(customer.email_address, self.customer_111[5])
+        self.assertEqual(customer.activity_status, self.customer_111[6])
+        self.assertEqual(customer.credit_limit, self.customer_111[7])
+        LOGGER.info('test add customer completed')
+
+        # test we are unable to add same customer
+        with self.assertRaises(IntegrityError):
+            add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                         self.customer_111[3], self.customer_111[4],
+                         self.customer_111[5], self.customer_111[6], self.customer_111[7])
+
+    def test_search_customer(self):
+        '''test search customer'''
+        pass
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+
+        customer_dict = search_customer(self.customer_111[0])
+        expected_dict = {'first_name': self.customer_111[1],
+                         'last_name': self.customer_111[2],
+                         'email_address': self.customer_111[5],
+                         'phone_number': self.customer_111[4]}
+        self.assertEqual(customer_dict, expected_dict)
+
+        # Test customer not in db
+        with self.assertRaises(ValueError):
+            search_customer('222')
+
+        LOGGER.info('test search customer completed')
+
+    def test_delete_customer(self):
+        '''test delete customer'''
+        pass
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+        delete_customer(self.customer_111[0])
+        LOGGER.info('customer deleted')
+        LOGGER.info('customer %s %s deleted', self.customer_111[1], self.customer_111[2])
+
+        # ensure its deleted
+        with self.assertRaises(ValueError):
+            delete_customer(self.customer_111[0])
+
+        LOGGER.info('test delete customer completed')
+
+    def test_update_customer_credit(self):
+        '''test update customer'''
+        pass
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+        customer = Customer.get(Customer.customer_id == self.customer_111[0])
+        LOGGER.info("test update customer credit limit before update is %s", customer.credit_limit)
+        update_customer_credit(self.customer_111[0], 3000)
+        customer = Customer.get(Customer.customer_id == self.customer_111[0])
+        LOGGER.info("test update customer credit limit after update is %s", customer.credit_limit)
+        self.assertEqual(customer.credit_limit, 3000)
+        LOGGER.info('test update customer completed')
+
+        # Test update of invalid customer id
+        with self.assertRaises(ValueError):
+            update_customer_credit('222', 7500)
+
+    def test_list_active_customers(self):
+        '''test list active customers'''
+        pass
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+        active_count = list_active_customers()
+        self.assertEqual(1, active_count)
+        customer = Customer.get(Customer.customer_id == self.customer_111[0])
+        customer.delete_instance()
+        active_count = list_active_customers()
+        self.assertEqual(0, active_count)
+
+        LOGGER.info('test list active customers completed')
+
+    def test_show_customers(self):
+        '''test more pythonic show customers'''
+        add_customer(self.customer_111[0], self.customer_111[1], self.customer_111[2],
+                     self.customer_111[3], self.customer_111[4],
+                     self.customer_111[5], self.customer_111[6], self.customer_111[7])
+        add_customer(self.customer_222[0], self.customer_222[1], self.customer_222[2],
+                     self.customer_222[3], self.customer_222[4],
+                     self.customer_222[5], self.customer_222[6], self.customer_222[7])
+        customers = show_customers()
+        LOGGER.info("test more pythonic show customers %s", customers)
+        expected = ['John Smith', 'Mary Jones']
+        self.assertEqual(expected, customers)
