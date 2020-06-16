c996e7968da74cddf4289ad84e42f0f1b4d579b9
tianxie812 <xietian628@yahoo.com>
2020-06-15 20:25:32 -0700

    updating a bunch of stuff

--- students/Tianx/Lesson3/src/customer_model.py
+++ students/Tianx/Lesson3/src/customer_model.py
@@ -12,18 +12,18 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Customer(BaseModel):
     """
     This class defines Customer, which maintains details of customer info.
     """
-
     customer_id = CharField(primary_key=True, max_length=30)
     first_name = CharField(max_length=30)
     last_name = CharField(max_length=30)
     home_address = CharField(max_length=200)
-    phone_number = IntegerField(max_length=10)
+    phone_number = CharField(max_length=10)
     email_address = CharField(max_length=50)
-    status = BooleanField(default=True)
+    active_status = BooleanField(default=True)
     credit_limit = DecimalField(max_digits=10, decimal_places=2, default=0)
 
 
