d9f88fa1a6f663792ad25c71fd13a82e3509a55a
stellie <kims82@gmail.com>
2020-06-18 14:37:37 -0700

    update code and fix pylint errors so that pylint passes

---
+++ students/stellie/lesson01/assignment/inventory_management/market_prices.py
@@ -0,0 +1,7 @@
+"""This module retrieves the latest price"""
+
+
+def get_latest_price(item_code):
+    """Method to return 24 as the latest market price"""
+    return 24
+    # Raise an exception to force the user to Mock its output
