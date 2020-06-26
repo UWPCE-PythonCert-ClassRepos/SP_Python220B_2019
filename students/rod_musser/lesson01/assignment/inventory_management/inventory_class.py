fcdccc107f1d64b21df4f8891b665f2da92d0b1f
Rod Musser <rmusser@yahoo.com>
2020-06-25 21:36:58 -0700

    Lesson 1 Assignment

---
+++ students/rod_musser/lesson01/assignment/inventory_management/inventory_class.py
@@ -0,0 +1,19 @@
+"""Inventory class"""
+class Inventory:
+
+    """Represents an item in the inventory"""
+    def __init__(self, product_code, description, market_price, rental_price):
+        self.product_code = product_code
+        self.description = description
+        self.market_price = market_price
+        self.rental_price = rental_price
+
+    def return_as_dictionary(self):
+        """Return Inventory attributes as a dict"""
+        output_dict = {}
+        output_dict['product_code'] = self.product_code
+        output_dict['description'] = self.description
+        output_dict['market_price'] = self.market_price
+        output_dict['rental_price'] = self.rental_price
+
+        return output_dict
