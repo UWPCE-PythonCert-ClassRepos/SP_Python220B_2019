6add70222ffe955809f490c78f168fc14172e9d9
stellie <kims82@gmail.com>
2020-07-06 14:55:18 -0700

    setup files for assignment

---
+++ students/stellie/lesson04/assignment/basic_operations.py
@@ -0,0 +1,118 @@
+# Stella Kim
+# Assignment 4: Iterables, Iterators & Generators
+
+"""Create customer database with Peewee ORM, SQLite and Python"""
+
+
+import logging
+from peewee import IntegrityError, DoesNotExist
+from customer_model import CUSTOMER_DB, Customer
+
+# Format logs
+LOG_FORMAT = '%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s'
+
+# Setup logging format
+FORMATTER = logging.Formatter(LOG_FORMAT)
+
+# Setup file handler
+FILE_HANDLER = logging.FileHandler('db.log')
+FILE_HANDLER.setLevel(logging.WARNING)
+FILE_HANDLER.setFormatter(FORMATTER)
+
+# Setup console handler
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.DEBUG)
+CONSOLE_HANDLER.setFormatter(FORMATTER)
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(CONSOLE_HANDLER)
+LOGGER.info('Logger has initiated.')
+
+CUSTOMER_DB.create_tables([Customer])
+LOGGER.info('Customer table created.')
+
+
+def add_customer(customer_id, first_name, last_name, home_address,
+                 phone, email, status, credit_limit):
+    """Create a new customer.  Raises error if ID in use."""
+    try:
+        with CUSTOMER_DB.transaction():
+            new_customer = Customer.create(
+                customer_id=customer_id,
+                first_name=first_name,
+                last_name=last_name,
+                home_address=home_address,
+                phone=phone,
+                email=email,
+                status=status,
+                credit_limit=credit_limit
+            )
+        new_customer.save()
+        LOGGER.debug('New customer %s %s (ID: %s) has been added to DB',
+                     first_name, last_name, customer_id)
+
+    except IntegrityError as error:
+        LOGGER.error('Failure: ID %s already in use.', customer_id)
+        LOGGER.info(error)
+        raise ValueError
+
+
+def search_customer(customer_id):
+    """Search for a customer in the database by ID."""
+    try:
+        search = Customer.get(Customer.customer_id == customer_id)
+        LOGGER.debug('Searching for customer %s...', customer_id)
+        return {'First Name': search.first_name,
+                'Last Name': search.last_name,
+                'Email Address': search.email,
+                'Phone Number': search.phone}
+
+    except DoesNotExist:
+        LOGGER.error('Customer %s does not exist.', customer_id)
+        return {}
+
+
+def delete_customer(customer_id):
+    """Delete a customer from the database by ID."""
+    try:
+        search = Customer.get(Customer.customer_id == customer_id)
+        LOGGER.debug('Searching for customer %s...', customer_id)
+        search.delete_instance()
+        LOGGER.debug('Customer %s has been deleted from database.',
+                     customer_id)
+        return search
+
+    except DoesNotExist:
+        LOGGER.error('Customer %s does not exist.', customer_id)
+        return None
+
+
+def update_customer_credit(customer_id, credit_limit):
+    """Update a customer's credit limit by ID."""
+    try:
+        search = Customer.get(Customer.customer_id == customer_id)
+        LOGGER.debug('Searching for customer %s...', customer_id)
+        with CUSTOMER_DB.transaction():
+            search.credit_limit = credit_limit
+            search.save()
+        LOGGER.debug('Credit limit for %s has been updated.', customer_id)
+        return search.credit_limit
+
+    except DoesNotExist:
+        LOGGER.error('Customer %s does not exist.', customer_id)
+        return None
+
+
+def list_active_customers():
+    """Display a count of active customers in the database."""
+    active_customers = Customer.select().where(Customer.status ==
+                                               'Active').count()
+    LOGGER.info('Number of customers whose status is currently active: %s.',
+                active_customers)
+    return active_customers
+
+
+CUSTOMER_DB.close()
---
+++ students/stellie/lesson04/assignment/tests/test_integration.py
@@ -0,0 +1,66 @@
+# Stella Kim
+# Assignment 4: Iterables, Iterators & Generators
+
+"""Integration tests for storing customer data"""
+
+from unittest import TestCase
+from peewee import SqliteDatabase
+from customer_model import Customer
+import basic_operations as main
+
+
+def reset_database():
+    """Clears table data in database"""
+    main.CUSTOMER_DB.drop_tables([Customer])
+    main.CUSTOMER_DB.create_tables([Customer])
+
+
+class ModuleTests(TestCase):
+    """Class for customer storage integration testing"""
+
+    def test_integration(self):
+        """Test functionality of entire application."""
+        self.assertIsInstance(main.CUSTOMER_DB, SqliteDatabase)
+        reset_database()
+        test_db = [(1, 'John', 'Smith', '123 Main Street', '2065551234',
+                    'smith.john@example.com', 'Active', 10000),
+                   (2, 'Jane', 'Doe', '1000 Market Street', '2065555678',
+                    'doe.jane@example.com', 'Active', 5000),
+                   (3, 'Alice', 'Wonderland', '200 Cherry Street',
+                    '2065551357', 'wonderland.alice@example.com',
+                    'Active', 1000),
+                   (3, 'Mark', 'Allen', '555 Columbia Avenue', '2065552468',
+                    'allen.mark@example.com', 'Active', 8500)]
+
+        main.add_customer(*test_db[0])
+        main.add_customer(*test_db[1])
+        main.add_customer(*test_db[2])
+
+        customer_1 = Customer.get(Customer.customer_id == 1)
+        customer_2 = Customer.get(Customer.customer_id == 2)
+        customer_3 = Customer.get(Customer.customer_id == 3)
+
+        self.assertEqual(test_db[0][1], customer_1.first_name)
+        self.assertEqual(test_db[1][2], customer_2.last_name)
+        self.assertEqual(test_db[2][3], customer_3.home_address)
+
+        with self.assertRaises(ValueError):
+            main.add_customer(*test_db[3])
+
+        search = main.search_customer(1)
+        expected = {'First Name': 'John',
+                    'Last Name': 'Smith',
+                    'Email Address': 'smith.john@example.com',
+                    'Phone Number': '2065551234'}
+        self.assertEqual(search, expected)
+        self.assertEqual(main.search_customer(4), {})
+
+        main.delete_customer(2)
+        search = main.delete_customer(2)
+        self.assertEqual(search, None)
+
+        main.update_customer_credit(1, 12000)
+        customer = Customer.get(Customer.customer_id == 1)
+        self.assertEqual(12000, customer.credit_limit)
+        update = main.update_customer_credit(4, 2000)
+        self.assertEqual(update, None)
