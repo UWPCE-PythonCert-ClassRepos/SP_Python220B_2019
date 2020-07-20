0581db51660c1d7f6d41500c653ec0422882d35c
Nichole Godwin <headiehg@mail.com>
2020-07-20 15:49:47 -0700

    pre-logging

--- students/headieh_godwin/lesson02/assignment/charges_calc.py
+++ students/headieh_godwin/lesson02/assignment/charges_calc.py
@@ -1,5 +1,5 @@
 '''
-Returns total price paid for individual rentals 
+Returns total price paid for individual rentals
 '''
 import argparse
 import json
@@ -25,11 +25,19 @@ def load_rentals_file(filename):
 def calculate_additional_fields(data):
     for value in data.values():
         try:
-            rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
-            rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
-            value['total_days'] = (rental_end - rental_start).days
+#changed the code assuming days with no rental start or rental end should be given a value of 0 total_days
+            if (value['rental_start'] == '' or value['rental_end'] == ''):
+                rental_start = datetime.datetime(2016, 6, 19, 0, 0)
+                rental_end = datetime.datetime(2016, 6, 19, 0, 0)
+            else:
+                rental_start = datetime.datetime.strptime(value['rental_start'], '%m/%d/%y')
+                rental_end = datetime.datetime.strptime(value['rental_end'], '%m/%d/%y')
+#changed code to take abs value assuming start and end were just backwards for some
+            value['total_days'] = abs((rental_end - rental_start).days)
             value['total_price'] = value['total_days'] * value['price_per_day']
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
+            if value['units_rented'] == 0:
+                value['units_rented'] = 1
             value['unit_cost'] = value['total_price'] / value['units_rented']
         except:
             exit(0)
