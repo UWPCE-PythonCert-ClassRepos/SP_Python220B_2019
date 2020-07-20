a25fa40c6bc73fe88b8357a11083d09cb3eb4779
mmanc125uw <mmanc125@uw.edu>
2020-07-20 18:57:59 -0400

    sp_py220 lesson8, build out partial func

--- students/mmancini/lesson08/inventory.py
+++ students/mmancini/lesson08/inventory.py
@@ -11,9 +11,14 @@ def add_furniture(invoice_file, customer_name, item_code, item_description, item
     with open(invoice_file, "a+") as invoice_csv:
         csv_writer = csv.writer(invoice_csv, delimiter=',', lineterminator='\n')
         csv_writer.writerow([customer_name, item_code, item_description, item_monthly_price])
-        
+
+
 def single_customer(customer_name, invoice_file):
     """ define function which curries customer_name and invoice_file for add_furniture"""
     def customer_rental(rental_items):
-        pass
+        """ process rented items """
+        customer = partial(add_furniture, invoice_file=invoice_file, customer_name=customer_name)
+        with open(rental_items, "r") as rental_csv:
+            for row in csv.reader(rental_csv):
+                customer(item_code=row[0], item_description=row[1], item_monthly_price=row[2])
     return customer_rental
