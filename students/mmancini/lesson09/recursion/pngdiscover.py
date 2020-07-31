1f2c097888458cb647b43439739995b2e2bf60bb
mmanc125uw <mmanc125@uw.edu>
2020-07-31 10:59:25 -0400

    sp_py220 lesson9, recursion updates

--- students/mmancini/lesson09/recursion/pngdiscover.py
+++ students/mmancini/lesson09/recursion/pngdiscover.py
@@ -11,7 +11,20 @@ def png_discovery(directory):
         in: folder spec to traverse from
         out: list of list format([subfoldername1/[filename1.png,filename2.png],...],[subfoldername2/[filename1.png,filename2.png,...], etc]
     '''
-    pass
+    
+    sublist = []
+    count = 0
+    for file in os.listdir(directory):
+        if file.endswith('.png'):
+            count += 1
+            sublist.append(file)
+        elif os.path.isdir(os.path.join(directory, file)):
+            png_discovery(os.path.join(directory, file))
+        else:
+            continue
+    if count >= 1:
+        DATA_LIST.append(os.path.join(os.getcwd(), directory))
+        DATA_LIST.append(sublist)
 
 
 if __name__ == '__main__':
