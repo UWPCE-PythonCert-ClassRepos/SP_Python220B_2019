f45147de33239b8009337bae1fbc847e33453ac8
geekwriter2 <geekwriter@hotmail.com>
2020-08-26 21:38:55 -0700

    changes to lesson 2. corrected exception catching for math error and added debug levels def

--- students/michael_mcdonald/lesson2/charges_calc.py
+++ students/michael_mcdonald/lesson2/charges_calc.py
@@ -1,4 +1,4 @@
-"""Returns total price paid for individual rentals """
+"""Returns total price paid for individual rentals v3"""
 
 import argparse
 import json
@@ -15,10 +15,11 @@ FORMATTER = logging.Formatter(LOG_FORMAT)
 # python charges_calc.py -i source_orig.json -o out.json
 
 
+# returning error here to handle json loading errors
 def load_rentals_file(filename):
     """load rental JSON file"""
 
-    logging.info('load_rentals_file: filename =\t %s', filename)
+    logging.debug('load_rentals_file: filename =\t %s', filename)
     with open(filename) as file:
         try:
             # renamed data as it shadows top level data variable
@@ -27,21 +28,22 @@ def load_rentals_file(filename):
         # added specific exception clause for IO
         except (FileNotFoundError, IOError):
             logging.error('JSON file error %s %s', FileNotFoundError.filename,
-                          IOError, exc_info=True)
+                          IOError)
             sys.exit(0)
 
         # added specific exception clause for JSONDecodeError
         except ValueError:
-            logging.error("JSON read error %s", ValueError, exc_info=True)
+            logging.error("JSON read error %s", ValueError)
             sys.exit(0)
     return my_data
 
 
 # renamed data as it shadows top level data variable
+# used warnings to report individual record errors
 def calculate_additional_fields(my_data):
     """calculate additional JSON fields"""
 
-    logging.info('calculate_additional_fields: my_data =\t%s', 'data')  # data placeholder
+    logging.debug('calculate_additional_fields: my_data =\t%s', 'data')  # data placeholder
     for value in my_data.values():
         # add product_code to logging to quickly find which ones are failing
         product_code = value['product_code']
@@ -52,10 +54,10 @@ def calculate_additional_fields(my_data):
             logging.warning('date parsing error %s %s', 'rental_start', 'rental_end')
         value['total_days'] = (rental_end - rental_start).days
         value['total_price'] = value['total_days'] * value['price_per_day']
-        logging.info('sqrt_total_price = (%s - %s) * %s (%s) %s', rental_end, rental_start,
-                     value['price_per_day'],
-                     value['total_days'],
-                     product_code)
+        logging.debug('sqrt_total_price = (%s - %s) * %s (%s) %s', rental_end, rental_start,
+                      value['price_per_day'],
+                      value['total_days'],
+                      product_code)
         try:
             value['sqrt_total_price'] = math.sqrt(value['total_price'])
             value['unit_cost'] = value['total_price'] / value['units_rented']
@@ -71,52 +73,51 @@ def calculate_additional_fields(my_data):
 def save_to_json(filename, my_data):
     """save results to a JSON file"""
 
-    logging.info('save_to_json: filename = \t%s, data = \t%s', filename, 'data')
+    logging.debug('save_to_json: filename = \t%s, data = \t%s', filename, 'data')
     with open(filename, 'w') as file:
         json.dump(my_data, file)
 
 
-# python charges_calc.py -i source_orig.json -o out.json -d [1|2|3|4]
-def process_debug_level(level):
+# python charges_calc.py -i source_orig.json -o out.json -d [1|2|3|]
+def process_debug_level(level=0):
     """set a debug level"""
 
     file_handler = logging.FileHandler(LOG_FILE)
     file_handler.setFormatter(FORMATTER)
+
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(FORMATTER)
+
     logger = logging.getLogger()
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    if level == '1':
-        debug_level = 'Debugger will handle DEBUG and higher'
-        logger.setLevel(logging.DEBUG)
-        console_handler.setLevel(logging.DEBUG)
-        file_handler.setLevel(logging.DEBUG)
-    elif level == '2':
-        debug_level = 'Debugger will handle INFO and higher'
-        logger.setLevel(logging.INFO)
-        console_handler.setLevel(logging.INFO)
-        file_handler.setLevel(logging.INFO)
-    elif level == '3':
-        debug_level = 'Debugger will handle WARNING and higher'
-        logger.setLevel(logging.WARNING)
-        console_handler.setLevel(logging.WARNING)
-        file_handler.setLevel(logging.WARNING)
-    elif level == '4':
-        debug_level = 'Debugger will handle ERROR and CRITICAL'
-        logger.setLevel(logging.ERROR)
+    if level == '0':
+        debug_level = 'Debugger turned off'
+        logging.disable(logging.INFO)
+        logging.disable(logging.ERROR)
+        logging.disable(logging.DEBUG)
+        logging.disable(logging.WARNING)
+        logging.disable(logging.CRITICAL)
+    elif level == '1':
+        debug_level = 'Debugger will handle ERROR'
         console_handler.setLevel(logging.ERROR)
         file_handler.setLevel(logging.ERROR)
-    elif level == '4':
-        debug_level = 'Debugger will handle CRITICAL'
-        logger.setLevel(logging.CRITICAL)
-        console_handler.setLevel(logging.CRITICAL)
-        file_handler.setLevel(logging.CRITICAL)
+
+    elif level == '2':
+        debug_level = 'Debugger will handle INFO and WARNINGS'
+        console_handler.setLevel(logging.DEBUG)
+        file_handler.setLevel(logging.WARNING)
+        logger.setLevel(logging.DEBUG)
+
+    # debug to screen only, set file logger above DEBUG
+    elif level == '3':
+        debug_level = 'Debugger will handle ERROR, WARNINGS'
+        console_handler.setLevel(logging.WARNING)
+        file_handler.setLevel(logging.WARNING)
+
     else:
-        debug_level = 'Default debugger set to NOTSET. Enter 1-3 to set a level'
-        logger.setLevel(logging.NOTSET)
-        console_handler.setLevel(logging.NOTSET)
-        file_handler.setLevel(logging.NOTSET)
+        debug_level = 'Debugger out of bounds. Please choose -d [1|2|3]'
+        logging.disable(logging.CRITICAL)
     return debug_level
 
 
@@ -134,8 +135,8 @@ def parse_cmd_arguments():
 
 if __name__ == "__main__":
     args = parse_cmd_arguments()
-    debug_lvl = process_debug_level(args.debug)
-    print(debug_lvl)
+    DEBUG_LVL = process_debug_level(args.debug)
+    print(DEBUG_LVL)
     data = load_rentals_file(args.input)
     data = calculate_additional_fields(data)
     save_to_json(args.output, data)
